b1d53f3aca15b685c9a688dfc1f63fc69d5f1919
Akuli <akuviljanen17@gmail.com>
2017-03-19 09:09:43 +0200

    use correct colors in line numbers

--- porcupine/linenumbers.py
+++ porcupine/linenumbers.py
@@ -5,6 +5,8 @@ This doesn't handle scrolling in any way. See multiscrollbar.py.
 
 import tkinter as tk
 
+from porcupine.settings import config, color_themes
+
 
 class LineNumbers(tk.Text):
 
@@ -16,6 +18,13 @@ class LineNumbers(tk.Text):
         self.insert('1.0', " 1")    # this is always there
         self['state'] = 'disabled'  # must be after the insert
         self._linecount = 1
+
+        config.connect('editing:color_theme', self._on_theme_changed)
+        self._on_theme_changed(None, config['editing:color_theme'])
+
+    def _on_theme_changed(self, junk, value):
+        self['fg'] = color_themes[value]['foreground']
+        self['bg'] = color_themes[value]['background']
 
     def do_update(self):
         """This should be ran when the line count changes."""
@@ -35,9 +44,13 @@ class LineNumbers(tk.Text):
 
 
 if __name__ == '__main__':
-    # simple test/demo
+    import porcupine.settings
+
     root = tk.Tk()
-    text = tk.Text(root)
+    porcupine.settings.load()
+
+    theme = color_themes[config['editing:color_theme']]
+    text = tk.Text(root, fg=theme['foreground'], bg=theme['background'])
     linenumbers = LineNumbers(root, text)
     linenumbers.pack(side='left', fill='y')
     text.pack(side='left', fill='both', expand=True)
