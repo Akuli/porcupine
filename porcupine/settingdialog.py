7ac7bf0cf2e296d8616a4b6ae7cd7f4a3ebbdb5e
Akuli <akuviljanen17@gmail.com>
2017-03-19 21:46:10 +0200

    more setting stuff

--- porcupine/settingdialog.py
+++ porcupine/settingdialog.py
@@ -5,13 +5,28 @@ import codecs
 import os
 import pkgutil
 import re
+import sys
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import ttk, messagebox
 import tkinter.font as tkfont
 
 from porcupine.settings import config
 
 _PADDING = {'padx': 5, 'pady': 2}
+
+
+try:
+    # There's no ttk.Spinbox for some reason. Maybe this will be fixed
+    # later?
+    TtkSpinbox = ttk.Spinbox
+except AttributeError:
+    # At least not yet, but implementing this is easy to do by reading
+    # the code of ttk.Combobox.
+    class TtkSpinbox(ttk.Entry):
+        def __init__(self, master=None, *, from_=None, **kwargs):
+            if from_ is not None:
+                kwargs['from'] = from_  # this actually works
+            super().__init__(master, 'ttk::spinbox', **kwargs)
 
 
 def _create_checkbox(parent, *, text='', **kwargs):
@@ -47,6 +62,39 @@ def _create_checkbox(parent, *, text='', **kwargs):
     return frame
 
 
+def _list_families():
+    result = {'TkFixedFont'}   # a set to delete duplicates
+    for family in tkfont.families():
+        # There are some weird fonts starting with @ on Windows for some
+        # reason.
+        if not family.startswith('@'):
+            result.add(family)
+    return sorted(result, key=str.casefold)
+
+
+class _WarningTriangle(tk.Label):
+
+    _image_cache = {}
+
+    def __init__(self, *args, **kwargs):
+        if not self._image_cache:
+            data = pkgutil.get_data('porcupine', 'images/triangle.png')
+            triangle = tk.PhotoImage(data=base64.b64encode(data))
+            empty = tk.PhotoImage(width=triangle.width(),
+                                  height=triangle.height())
+            self._image_cache['triangle'] = triangle
+            self._image_cache['empty'] = empty
+
+        super().__init__(*args, **kwargs)
+        self.hide()
+
+    def show(self):
+        self['image'] = self._image_cache['triangle']
+
+    def hide(self):
+        self['image'] = self._image_cache['empty']
+
+
 class _Section(tk.LabelFrame):
 
     def __init__(self, *args, **kwargs):
@@ -54,14 +102,16 @@ class _Section(tk.LabelFrame):
         self._row = 0
         self.grid_columnconfigure(0, weight=1)
 
-    def add_widget(self, widget, text=None):
+    def add_widget(self, widget, text=None, triangle=None):
         if text is None:
             widget.grid(row=self._row, column=0, columnspan=2,
                         sticky='w', **_PADDING)
         else:
             label = tk.Label(self, text=text)
             label.grid(row=self._row, column=0, sticky='w', **_PADDING)
-            widget.grid(row=self._row, column=1, sticky='w', **_PADDING)
+            widget.grid(row=self._row, column=1, sticky='e', **_PADDING)
+        if triangle is not None:
+            triangle.grid(row=self._row, column=2)
         self._row += 1
 
     def add_checkbox(self, key, **kwargs):
@@ -69,112 +119,125 @@ class _Section(tk.LabelFrame):
             self, variable=config.variables[key], **kwargs)
         self.add_widget(checkbox)
 
-    def add_entry(self, key, *, text, validator=None, **kwargs):
-        if validator is None:
-            entry = tk.Entry(self, textvariable=config.variables[key])
-            self.add_widget(entry, text)
-        else:
-            self._add_validating_entry(key, text, validator, **kwargs)
-
-    def _add_validating_entry(self, key, text, validator, *,
-                              _image_cache=[], **kwargs):
+    def add_entry(self, key, *, text, **kwargs):
         var = tk.StringVar()
         var.set(config[key])
-
-        if _image_cache:
-            triangle_image, empty_image = _image_cache
-        else:
-            data = pkgutil.get_data('porcupine', 'images/triangle.png')
-            triangle_image = tk.PhotoImage(data=base64.b64encode(data))
-            empty_image = tk.PhotoImage(
-                width=triangle_image.width(),
-                height=triangle_image.height())
-            _image_cache[:] = [triangle_image, empty_image]
-
-        frame = tk.Frame(self)
-        entry = tk.Entry(frame, textvariable=var, **kwargs)
-        entry.pack(side='left', fill='both', expand=True)
-        trianglelabel = tk.Label(frame, width=20, image=empty_image)
-        trianglelabel.pack(side='right')
+        entry = tk.Entry(self, textvariable=var, **kwargs)
+        triangle = _WarningTriangle(self)
 
         def to_config(*junk):
             value = var.get()
-            if validator(value):
-                trianglelabel['image'] = empty_image
+            if config.validate(key, value):
+                triangle.hide()
                 config[key] = value
             else:
-                trianglelabel['image'] = triangle_image
+                triangle.show()
 
         def from_config(key, value):
             var.set(value)
 
         var.trace('w', to_config)
         config.connect(key, from_config)
-        self.add_widget(frame, text)
+        self.add_widget(entry, text, triangle)
 
     def add_spinbox(self, key, *, text, **kwargs):
         var = tk.StringVar()
         var.set(str(config[key]))
         spinbox = tk.Spinbox(self, textvariable=var, **kwargs)
+        triangle = _WarningTriangle(self)
 
         def to_config(*junk):
             try:
                 value = int(var.get())
+                if not config.validate(key, value):
+                    raise ValueError
             except ValueError:
+                triangle.show()
                 return
-            if spinbox['from'] <= value <= spinbox['to']:
-                config[key] = value
+            triangle.hide()
+            config[key] = value
 
         def from_config(key, value):
             var.set(str(value))
 
         var.trace('w', to_config)
         config.connect(key, from_config)
-        self.add_widget(spinbox, text)
+        self.add_widget(spinbox, text, triangle)
 
+    def add_font_selector(self, key, *, text, **kwargs):
+        familyvar = tk.StringVar()
+        sizevar = tk.StringVar()
 
-def _validate_font(string):
-    # the editor crashes if the font is set to ''
-    if not string:
-        return False
+        frame = tk.Frame(self)
+        family_combobox = ttk.Combobox(
+            frame, textvariable=familyvar, values=_list_families())
+        family_combobox['width'] -= 4  # about same size as other widgets
+        family_combobox.pack(side='left')
+        size_spinbox = TtkSpinbox(
+            frame, textvariable=sizevar, from_=1, to=100, width=4)
+        size_spinbox.pack(side='left')
 
-    # This returns True for strings like 'asdfasdfasdf', but font
-    # strings like that actually work so it's not a problem. tkfont.Font
-    # also takes an exists keyword argument, but it makes it raise
-    # errors way too often for this.
-    try:
-        tkfont.Font(font=string)
-        return True
-    except tk.TclError:
-        return False
+        triangle = _WarningTriangle(self)
 
+        # not all values that the user types are valid, so we need to
+        # cache the previous correct family and size
+        cache = []   # ['family', size]
 
-def _validate_encoding(name):
-    try:
-        codecs.lookup(name)
-        return True
-    except LookupError:
-        return False
+        def from_config(key, value):
+            # the fonts are stored as "{family} size" strings because
+            # tkinter widgets can use strings like that
+            if value.lower() == 'tkfixedfont':
+                # Special case: tkinter's default font. The cache needs
+                # to be set before setting the variables because
+                # to_config() needs the cache.
+                cache[:] = ['TkFixedFont', None]
+                familyvar.set('TkFixedFont')
+                sizevar.set('N/A')
+            else:
+                print(repr(value))
+                match = re.search(r'^\{(.+)\} (\d+)$', value)
+                cache[:] = [match.group(1), int(match.group(2))]
+                familyvar.set(match.group(1))
+                sizevar.set(match.group(2))
+ 
+        def to_config(*junk):
+            family = familyvar.get()
+            ok = True
 
+            if family.lower() == 'tkfixedfont':
+                # special family
+                cache[:] = ['TkFixedFont', None]
+                sizevar.set('N/A')
 
-def _validate_geometry(geometry):
-    """Check if a tkinter geometry is valid.
+            elif family.casefold() in map(str.casefold, _list_families()):
+                # family is ok, how about size?
+                cache[0] = family
+                try:
+                    size = int(sizevar.get())
+                    if size <= 0:
+                        raise ValueError
+                    cache[1] = size
+                except ValueError:   # int() failed
+                    ok = False
 
-    >>> _validate_geometry('100x200+300+400')
-    True
-    >>> _validate_geometry('100x200')
-    True
-    >>> _validate_geometry('+300+400')
-    True
-    >>> _validate_geometry('asdf')
-    False
-    >>> # tkinter actually allows '', but it does nothing
-    >>> _validate_geometry('')
-    False
-    """
-    if not geometry:
-        return False
-    return re.search(r'^(\d+x\d+)?(\+\d+\+\d+)?$', geometry) is not None
+            else:
+                # family is not ok
+                ok = False
+
+            if ok:
+                triangle.hide()
+                if cache == ['TkFixedFont', None]:
+                    config[key] = 'TkFixedFont'
+                else:
+                    config[key] = '{%s} %d' % tuple(cache)
+            else:
+                triangle.show()
+
+        familyvar.trace('w', to_config)
+        sizevar.trace('w', to_config)
+        config.connect(key, from_config)
+        from_config(key, config[key])
+        self.add_widget(frame, text, triangle)
 
 
 class SettingDialog(tk.Toplevel):
@@ -206,8 +269,7 @@ class SettingDialog(tk.Toplevel):
     def _create_filesection(self):
         section = _Section(self, text="Files")
         section.add_entry(
-            'files:encoding', validator=_validate_encoding,
-            text="Encoding of opened and saved files:")
+            'files:encoding', text="Encoding of opened and saved files:")
         section.add_checkbox(
             'files:add_trailing_newline',
             text="Make sure that files end with an empty line when saving")
@@ -215,10 +277,8 @@ class SettingDialog(tk.Toplevel):
 
     def _create_editingsection(self):
         section = _Section(self, text="Editing")
-        section.add_entry(
-            'editing:font', validator=_validate_font,
-            text=('The font as a Tkinter font string ' +
-                  '(e.g. "{Some Font} 12"):'))
+        section.add_font_selector(
+            'editing:font', text="Font family and size:")
         section.add_spinbox(
             'editing:indent', from_=1, to=100, text="Indent width:")
         section.add_checkbox(
@@ -234,7 +294,7 @@ class SettingDialog(tk.Toplevel):
         section.add_checkbox(
             'gui:statusbar', text="Display a statusbar at bottom")
         section.add_entry(
-            'gui:default_geometry', validator=_validate_geometry,
+            'gui:default_geometry',
             text=("Default window size as a Tkinter geometry " +
                   "(e.g. 650x500):"))
         return section
--- porcupine/settings.py
+++ porcupine/settings.py
@@ -1,8 +1,12 @@
+import codecs
 import configparser
 import glob
 import os
 import pkgutil
+import re
+import sys
 import tkinter as tk
+import tkinter.font as tkfont
 import traceback
 
 
@@ -13,20 +17,22 @@ import traceback
 class _Config:
 
     def __init__(self, *, dont_reset=()):
-        self.variables = {}   # see setup()
-        self._callbacks = {}  # see setup() and _on_var_changed()
+        self.variables = {}
+        self._callbacks = {}
+        self._validators = {}
         self._dont_reset = dont_reset
         self.default_values = None
         self.original_values = None
 
     def setup(self, contentlist):
-        for string, vartype in contentlist:
-            # The variables are named like the strings to make them easy
+        for key, vartype, validator in contentlist:
+            # The variables are named like the keys to make them easy
             # to recognize in _on_var_changed().
-            var = vartype(name=string)
+            var = vartype(name=key)
             var.trace('w', self._on_var_changed)
-            self.variables[string] = var
-            self._callbacks[string] = []
+            self.variables[key] = var
+            self._callbacks[key] = []
+            self._validators[key] = validator
 
     def _on_var_changed(self, key, *junk):
         try:
@@ -47,29 +53,39 @@ class _Config:
             self._configparser_load(parser, allow_missing=True)
         self.original_values = dict(self)
 
+        for key in self.keys():
+            if not self.validate(key, self[key]):
+                print("%s: invalid %r value %r, using %r instead"
+                      % (__name__, key, self[key], self.default_values[key]),
+                      file=sys.stderr)
+                self[key] = self.default_values[key]
+
     def _configparser_load(self, parser, allow_missing=False):
-        for string, var in self.variables.items():
-            sectionname, key = string.split(':')
+        for key, var in self.variables.items():
+            sectionname, configkey = key.split(':')
             try:
                 section = parser[sectionname]
-                section[key]
+                section[configkey]
             except KeyError as e:
                 if allow_missing:
                     continue
                 raise e
 
             if isinstance(var, tk.BooleanVar):
-                var.set(section.getboolean(key))
+                var.set(section.getboolean(configkey))
             elif isinstance(var, tk.IntVar):
-                var.set(section.getint(key))
+                var.set(section.getint(configkey))
             elif isinstance(var, tk.StringVar):
-                var.set(section[key])
+                var.set(section[configkey])
             else:
                 raise TypeError(type(var).__name__)
 
     def dump(self, file):
         parser = configparser.ConfigParser()
         for string, var in self.variables.items():
+            if var.get() == self.default_values[string]:
+                continue
+
             if isinstance(var, tk.StringVar):
                 value = var.get()
             elif isinstance(var, tk.BooleanVar):
@@ -93,9 +109,13 @@ class _Config:
             if name not in self._dont_reset:
                 self[name] = self.default_values[name]
 
-    # rest of this is convenience stuff
+    # rest of this is mostly convenience stuff
     def connect(self, string, callback):
         self._callbacks[string].append(callback)
+
+    def validate(self, string, value):
+        validator = self._validators[string]
+        return validator is None or validator(value)
 
     def __setitem__(self, string, value):
         self.variables[string].set(value)
@@ -113,32 +133,76 @@ config = _Config(dont_reset=['editing:color_theme'])
 _user_config_dir = os.path.join(os.path.expanduser('~'), '.porcupine')
 
 
+def _validate_encoding(name):
+    try:
+        codecs.lookup(name)
+        return True
+    except LookupError:
+        return False
+
+
+def _validate_geometry(geometry):
+    """Check if a tkinter geometry is valid.
+
+    >>> _validate_geometry('100x200+300+400')
+    True
+    >>> _validate_geometry('100x200')
+    True
+    >>> _validate_geometry('+300+400')
+    True
+    >>> _validate_geometry('asdf')
+    False
+    >>> # tkinter actually allows '', but it does nothing
+    >>> _validate_geometry('')
+    False
+    """
+    if not geometry:
+        return False
+    return re.search(r'^(\d+x\d+)?(\+\d+\+\d+)?$', geometry) is not None
+
+
+def _validate_fontstring(string):
+    if string == 'TkFixedFont':
+        # special case, but this is case-sensitive here because tkinter
+        # wants it CapsWordy
+        return True
+
+    match = re.search(r'^\{(.+)\} (\d+)$', string)
+    if match is None or int(match.group(2)) <= 0:
+        return False
+    return match.group(1).casefold() in map(str.casefold, tkfont.families())
+
+
 def load():
     os.makedirs(os.path.join(_user_config_dir, 'themes'), exist_ok=True)
 
+    # color themes must be read first because the editing:color_theme
+    # validator needs it
+    default_themes = pkgutil.get_data('porcupine', 'default_themes.ini')
+    color_themes.read_string(default_themes.decode('utf-8'))
+    escaped_path = glob.escape(os.path.join(_user_config_dir, 'themes'))
+    color_themes.read(glob.glob(os.path.join(escaped_path, '*.ini')))
+
     # we can't create StringVar etc. earlier because they need a root window
+    # TODO: allow tabs? (ew)
     config.setup([
-        # (string, vartype)
-        ('files:encoding', tk.StringVar),
-        ('files:add_trailing_newline', tk.BooleanVar),
-        ('editing:font', tk.StringVar),
-        ('editing:indent', tk.IntVar),     # TODO: allow tabs? (ew)
-        ('editing:undo', tk.BooleanVar),
-        ('editing:autocomplete', tk.BooleanVar),
-        ('editing:color_theme', tk.StringVar),
-        ('gui:linenumbers', tk.BooleanVar),
-        ('gui:statusbar', tk.BooleanVar),
-        ('gui:default_geometry', tk.StringVar),
+        # (string, vartype, validator)
+        ('files:encoding', tk.StringVar, _validate_encoding),
+        ('files:add_trailing_newline', tk.BooleanVar, None),
+        ('editing:font', tk.StringVar, _validate_fontstring),
+        ('editing:indent', tk.IntVar, (lambda value: value > 0)),
+        ('editing:undo', tk.BooleanVar, None),
+        ('editing:autocomplete', tk.BooleanVar, None),
+        ('editing:color_theme', tk.StringVar,
+         (lambda name: name in color_themes)),
+        ('gui:linenumbers', tk.BooleanVar, None),
+        ('gui:statusbar', tk.BooleanVar, None),
+        ('gui:default_geometry', tk.StringVar, _validate_geometry),
     ])
 
     default_config = pkgutil.get_data('porcupine', 'default_settings.ini')
     config.load(default_config.decode('utf-8'),
                 os.path.join(_user_config_dir, 'settings.ini'))
-
-    default_themes = pkgutil.get_data('porcupine', 'default_themes.ini')
-    color_themes.read_string(default_themes.decode('utf-8'))
-    escaped_path = glob.escape(os.path.join(_user_config_dir, 'themes'))
-    color_themes.read(glob.glob(os.path.join(escaped_path, '*.ini')))
 
 
 _COMMENTS = """\
