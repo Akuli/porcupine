5dff05336ca2ec753359309e60d1716f083e15df
Akuli <akuviljanen17@gmail.com>
2017-03-25 15:43:48 +0200

    interrupting issues

--- porcupine/scripts/sh_run.sh
+++ porcupine/scripts/sh_run.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # run a python file
 # usage: /path/to/sh_run.sh /PATH/TO/PYTHON DIRNAME BASENAME.py
+# the shebang says bash instead of sh because bash seems to handle
+# SIGINT better than Debian's /bin/sh (which is a symlink to /bin/dash)
 
 cd "$2"
 "$1" "$3"
