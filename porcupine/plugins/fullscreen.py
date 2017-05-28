0195e69e94bf88fc311dfefbb2d5fa9cae6ed7f5
Akuli <akuviljanen17@gmail.com>
2017-05-28 12:31:14 +0300

    now the full screen thing is a checkbutton

--- porcupine/plugins/fullscreen.py
+++ porcupine/plugins/fullscreen.py
@@ -1,13 +1,25 @@
 """Simple fullscreen mode button."""
 
+import tkinter as tk
+
 from porcupine import utils
 
 
-def toggle_fullscreen():
+def on_var_changed(variable_name, junk, trace_mode):
     root = utils.get_root()
-    new_value = (not root.attributes('-fullscreen'))
-    root.attributes('-fullscreen', new_value)
+    root.attributes('-fullscreen', root.getvar(variable_name))
 
 
 def setup(editor):
-    editor.add_action(toggle_fullscreen, "View/Full Screen", 'F11', '<F11>')
+    var = tk.BooleanVar()
+    var.trace('w', on_var_changed)
+
+    def toggle_var():
+        var.set(not var.get())
+
+    # add_action only supports command items, but that's why the menus
+    # are also exposed :)
+    editor.add_action(toggle_var, binding='<F11>')
+    menu = editor.get_menu("View")
+    menu.add_checkbutton(label="Full Screen", accelerator='F11',
+                         variable=var)
