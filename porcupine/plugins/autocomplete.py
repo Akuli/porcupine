806d537ee97003c0477f6a6b8db019b5f4e84f1f
Akuli <akuviljanen17@gmail.com>
2017-05-28 18:03:13 +0300

    fix autocomplete bug

--- porcupine/plugins/autocomplete.py
+++ porcupine/plugins/autocomplete.py
@@ -14,11 +14,8 @@ class AutoCompleter:
         self._completing = False
 
     def _find_suffixes(self):
-        # TODO: what if there's text on both sides of the cursor? currently
-        # this is treated as one long word that the cursor is a part of, and
-        # it's kind of confusing
-        lineno = int(self.textwidget.index('insert').split('.')[0])
-        before_cursor = self.textwidget.get('%d.0' % lineno, 'insert')
+        before_cursor = self.textwidget.get('insert linestart', 'insert')
+        after_cursor = self.textwidget.get('insert', 'insert lineend')
 
         match = re.search('\w+$', before_cursor)
         if match is None:
@@ -27,12 +24,18 @@ class AutoCompleter:
         prefix = match.group(0)
 
         # Tcl's regexes don't support \b or a sane way of grouping so
-        # they are kind of useless for this
-        # TODO: ignore the word that the cursor is on top of if any
+        # they are kind of useless for this. I guess I should implement
+        # this with Tcl regexes too and check which is faster :)
         result = set()
         for chunk in self.textwidget.iter_chunks():
             for match in re.finditer(r'\b' + prefix + r'(\w+)', chunk):
                 result.add(match.group(1))
+
+        # if the cursor is in the middle of a word, that word must not
+        # be completed, e.g. if the user types abcdef and moves the
+        # cursor between c and d, we must not autocomplete to abcdefdef
+        result.discard(re.search('^\w*', after_cursor).group(0))
+
         return collections.deque(sorted(result, key=str.casefold))
 
     def complete(self, prev_or_next):
@@ -73,52 +76,12 @@ def tab_callback(tab):
     def do_reset(*junk):
         completer.reset()
 
-    text = tab.textwidget
-    text.complete_hook.connect(completer.complete)
-    text.cursor_move_hook.connect(do_reset)
+    tab.textwidget.complete_hook.connect(completer.complete)
+    tab.textwidget.cursor_move_hook.connect(do_reset)
     yield
-    text.complete_hook.disconnect(completer.complete)
-    text.cursor_move_hook.disconnect(do_reset)
+    tab.textwidget.complete_hook.disconnect(completer.complete)
+    tab.textwidget.cursor_move_hook.disconnect(do_reset)
 
 
 def setup(editor):
     editor.new_tab_hook.connect(tab_callback)
-
-
-if __name__ == '__main__':
-    # simple test
-    from porcupine import textwidget
-    from porcupine.settings import load as load_settings
-
-    def on_tab(event):
-        completer.complete('next')
-        return 'break'
-
-    def on_shift_tab(event):
-        completer.complete('previous')
-        return 'break'
-
-    # <<Modified>> is not really correct, the real editor resets on
-    # cursor movement instead
-    def on_modified(event):
-        text.unbind('<<Modified>>')
-        text.edit_modified(False)
-        text.bind('<<Modified>>', on_modified)
-        completer.reset()
-
-    root = tk.Tk()
-    load_settings()
-
-    text = textwidget.MainText(root)
-    text.iter_chunks = lambda: [text.get('1.0', 'end - 1 char')]    # lol
-    text.pack()
-
-    completer = AutoCompleter(text)
-    text.bind('<<Modified>>', on_modified)
-    text.bind('<Tab>', on_tab)
-    if root.tk.call('tk', 'windowingsystem') == 'x11':
-        text.bind('<ISO_Left_Tab>', on_shift_tab)
-    else:
-        text.bind('<Shift-Tab>', on_shift_tab)
-
-    root.mainloop()
