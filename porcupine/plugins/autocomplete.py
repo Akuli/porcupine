974b9cfcde25ce1cd5b32cf108575016d5303475
Akuli <akuviljanen17@gmail.com>
2017-08-10 22:44:42 +0300

    fix autocompleter stuff

--- porcupine/plugins/autocomplete.py
+++ porcupine/plugins/autocomplete.py
@@ -1,3 +1,4 @@
+# TODO: document this module's simple register_completer() api
 import collections
 import re
 
@@ -6,20 +7,46 @@ import jedi
 import porcupine
 from porcupine import tabs, utils
 
+__all__ = ['register_completer']
 setup_before = ['tabs2spaces']      # see tabs2spaces.py
-
-# Some jedi settings.
 jedi.settings.add_bracket_after_function = True
 
+_suffix_finders = {}
 
-def _jedi_suffix_finder(source, line, col):
+
+def register_completer(filetype_name, function):
+    """Add a syntax completer for a specific filetype.
+
+    Use like this::
+
+        from porcupine.plugins import autocomplete
+
+        def java_completer(script, cursor_lineno, cursor_column):
+            # script is the full content of the file
+            # this should return an iterable of things that can be inserted
+            # after the current cursor position, or an empty iterable
+            # (e.g. []) for no completions
+
+        autocomplete.register_completer("Java", java_completer)
+
+    The *filetype_name* should be a key of
+    :data:`porcupine.filetypes.filetypes`. Using this decorator multiple
+    times with the same *filetype_name* overrides previous decoratings.
+    """
+    def inner(func):
+        _AutoCompleter._suffix_finders[filetype_name] = func
+        return func
+
+    return inner
+
+
+def _jedi_suffix_finder(source, line, column):
     # TODO: Make this into its own module.
-    script = jedi.Script(source, line, col)
+    script = jedi.Script(source, line, column)
     return (c.complete for c in script.completions())
 
 
-class AutoCompleter:
-    suffix_finders = {"Python": _jedi_suffix_finder}
+class _AutoCompleter:
 
     def __init__(self, tab):
         self.tab = tab
@@ -27,7 +54,8 @@ class AutoCompleter:
         self._suffixes = None
         self._completing = False    # avoid recursion
 
-    def _generic_suffix_finder(self):
+    # find all words in file, sort by frequency
+    def _fallback_suffix_finder(self):
         before_cursor = self.tab.textwidget.get('insert linestart', 'insert')
         after_cursor = self.tab.textwidget.get('insert', 'insert lineend')
 
@@ -37,40 +65,44 @@ class AutoCompleter:
             return None
         prefix = match.group(0)
 
+        # find unique words starting with the prefix
         # Tcl's regexes don't support \b or a sane way of grouping so
         # they are kind of useless for this. I guess I should implement
         # this with Tcl regexes too and check which is faster :)
-        result = set()
+        result = collections.defaultdict(int)
         for chunk in self.tab.textwidget.iter_chunks():
             for match in re.finditer(r'\b' + prefix + r'(\w+)', chunk):
-                result.add(match.group(1))
+                result[match.group(1)] += 1
 
         # if the cursor is in the middle of a word, that word must not
         # be completed, e.g. if the user types abcdef and moves the
         # cursor between c and d, we must not autocomplete to abcdefdef
-        result.discard(re.search(r'^\w*', after_cursor).group(0))
+        try:
+            del result[re.search(r'^\w*', after_cursor).group(0)]
+        except KeyError:
+            pass
 
-        return collections.deque(sorted(result, key=str.casefold))
+        return sorted(result, key=result.get, reverse=True)
 
     def _find_suffixes(self):
         before_cursor = self.tab.textwidget.get('insert linestart', 'insert')
         after_cursor = self.tab.textwidget.get('insert', 'insert lineend')
 
-        if re.match(r"^\s*$", before_cursor) is not None:
-            # If the line is just whitespace, or empty, we want to indent.
+        if before_cursor.isspace() or not before_cursor:
+            # let other plugins handle this however they want to
             return None
-        elif re.match(r"\w+", after_cursor) is None:
-            finder = self.suffix_finders.get(self.tab.filetype.name)
+        if re.search(r"^\w", after_cursor) is not None:
+            # don't complete in the middle of a word
+            return collections.deque()
 
-            if finder is None:
-                return self._generic_suffix_finder()
+        finder = _suffix_finders.get(self.tab.filetype.name)
+        if finder is None:
+            return self._generic_suffix_finder()
 
-            line, col = map(int, self.tab.textwidget.index("insert").split("."))
-            source = self.tab.textwidget.get("1.0", "end - 1 char")
-            return collections.deque(finder(source, line, col))
-
-        # We don't want to complete in the middle of a word.
-        return collections.deque()
+        source = self.tab.textwidget.get("1.0", "end - 1 char")
+        cursor_pos = self.tab.textwidget.index("insert")
+        line, col = map(int, cursor_pos.split("."))
+        return collections.deque(finder(source, line, col))
 
     def _complete(self, rotation):
         self._completing = True
@@ -97,11 +129,7 @@ class AutoCompleter:
             # something's selected, autocompleting is probably not the
             # right thing to do
             return None
-
-        if shifted:       # display the previous completion
-            return self._complete(1)
-        else:
-            return self._complete(-1)
+        return self._complete(1 if shifted else -1)
 
     def reset(self, *junk):
         # deleting and inserting from _complete() runs this, so this
@@ -114,22 +142,10 @@ def on_new_tab(event):
     # TODO: autocomplete in other kinds of tabs too?
     tab = event.widget.tabs[-1]
     if isinstance(tab, tabs.FileTab):
-        completer = AutoCompleter(tab)
+        completer = _AutoCompleter(tab)
         utils.bind_tab_key(tab.textwidget, completer.on_tab, add=True)
         tab.textwidget.bind('<<CursorMoved>>', completer.reset, add=True)
 
 
 def setup():
     porcupine.get_tab_manager().bind('<<NewTab>>', on_new_tab, add=True)
-
-
-def completer(filetype):
-    """
-    Decorator that allows you to add a syntax completer for a specific filetype
-    """
-
-    def _inner(func):
-        AutoCompleter.suffix_finders[filetype] = func
-        return func
-
-    return _inner
---
+++ porcupine/plugins/jedi.py
@@ -0,0 +1,29 @@
+"""Python autocompletion with Jedi.
+
+You need jedi if you want to use this module. Install it like this on
+Windows::
+
+    py -m pip install --user jedi
+
+Or like this on other operating systems::
+
+    python3 -m pip install --user jedi
+"""
+
+import logging
+from porcupine.plugins import autocomplete
+
+try:
+    import jedi
+except ImportError:
+    logging.getLogger(__name__).exception("jedi is not installed")
+    jedi = None
+
+
+def jedi_completer(source, line, column):
+    script = jedi.Script(source, line, column)
+    return (c.complete for c in script.completions())
+
+
+def setup():
+    autocomplete.register_completer("Python", jedi_completer)
