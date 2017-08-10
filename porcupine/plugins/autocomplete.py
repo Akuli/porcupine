393e82b001ef5b7ae2ac0a98104938e8e09a9186
Purple Myst <PurpleMyst@users.noreply.github.com>
2017-08-10 18:17:15 +0200

    Use old autocompleter where Jedi is not applicable
    Further changes to the code allow for more specific auto-completion
    engines by adding to the `AutoCompleter.suffix_finders` dictionary.
    
    You can see an example of this in the code directly, seeing as the Jedi
    autocompleter is implemented using that same dictionary.

--- porcupine/plugins/autocomplete.py
+++ porcupine/plugins/autocomplete.py
@@ -14,22 +14,22 @@ jedi.settings.add_bracket_after_function = True
 
 class AutoCompleter:
 
-    def __init__(self, textwidget):
-        self.textwidget = textwidget
+    def __init__(self, tab):
+        self.tab = tab
         self._startpos = None
         self._suffixes = None
         self._completing = False    # avoid recursion
 
-    def _find_suffixes(self):
-        before_cursor = self.textwidget.get('insert linestart', 'insert')
-        after_cursor = self.textwidget.get('insert', 'insert lineend')
+    def _jedi_suffix_finder(self):
+        before_cursor = self.tab.textwidget.get('insert linestart', 'insert')
+        after_cursor = self.tab.textwidget.get('insert', 'insert lineend')
 
         if re.match(r"^\s*$", before_cursor) is not None:
             # If the line is just whitespace, or empty, we want to indent.
             return None
         elif re.match(r"\w+", after_cursor) is None:
-            line, col = map(int, self.textwidget.index("insert").split("."))
-            source = self.textwidget.get("1.0", "end - 1 char")
+            line, col = map(int, self.tab.textwidget.index("insert").split("."))
+            source = self.tab.textwidget.get("1.0", "end - 1 char")
 
             script = jedi.Script(source, line, col)
             completions = (c.complete for c in script.completions())
@@ -40,11 +40,52 @@ class AutoCompleter:
         # We don't want to complete in the middle of a word.
         return collections.deque()
 
+    suffix_finders = {"Python": _jedi_suffix_finder}
+
+    def _generic_suffix_finder(self):
+        before_cursor = self.tab.textwidget.get('insert linestart', 'insert')
+        after_cursor = self.tab.textwidget.get('insert', 'insert lineend')
+
+        match = re.search(r'\w+$', before_cursor)
+        if match is None:
+            # can't autocomplete based on this
+            return None
+        prefix = match.group(0)
+
+        # Tcl's regexes don't support \b or a sane way of grouping so
+        # they are kind of useless for this. I guess I should implement
+        # this with Tcl regexes too and check which is faster :)
+        result = set()
+        for chunk in self.tab.textwidget.iter_chunks():
+            for match in re.finditer(r'\b' + prefix + r'(\w+)', chunk):
+                result.add(match.group(1))
+
+        # if the cursor is in the middle of a word, that word must not
+        # be completed, e.g. if the user types abcdef and moves the
+        # cursor between c and d, we must not autocomplete to abcdefdef
+        result.discard(re.search(r'^\w*', after_cursor).group(0))
+
+        return collections.deque(sorted(result, key=str.casefold))
+
+    def _find_suffixes(self):
+        # We don't pass `self._generic_suffix_finder` as the default argument
+        # here because the values of the suffix_finders dictionary are unbound
+        # methods, while `self._generic_suffix_finder` is a bound method.
+        # Basically this means that we need to pass self when we get something
+        # from the dictionary, and we don't need to pass self when calling a
+        # method directly.
+        finder = self.suffix_finders.get(self.tab.filetype.name)
+
+        if finder is None:
+            return self._generic_suffix_finder()
+        else:
+            return finder(self)
+
     def _complete(self, rotation):
         self._completing = True
 
         if self._suffixes is None:
-            self._startpos = self.textwidget.index('insert')
+            self._startpos = self.tab.textwidget.index('insert')
             self._suffixes = self._find_suffixes()
             if self._suffixes is None:
                 # no completable characters before the cursor, just give
@@ -53,9 +94,9 @@ class AutoCompleter:
             self._suffixes.appendleft('')  # end of completions
 
         self._suffixes.rotate(rotation)
-        self.textwidget.delete(self._startpos, 'insert')
-        self.textwidget.mark_set('insert', self._startpos)
-        self.textwidget.insert(self._startpos, self._suffixes[0])
+        self.tab.textwidget.delete(self._startpos, 'insert')
+        self.tab.textwidget.mark_set('insert', self._startpos)
+        self.tab.textwidget.insert(self._startpos, self._suffixes[0])
 
         self._completing = False
         return 'break'
@@ -81,8 +122,8 @@ class AutoCompleter:
 def on_new_tab(event):
     # TODO: autocomplete in other kinds of tabs too?
     tab = event.widget.tabs[-1]
-    if isinstance(tab, tabs.FileTab) and tab.filetype.name == "Python":
-        completer = AutoCompleter(tab.textwidget)
+    if isinstance(tab, tabs.FileTab):
+        completer = AutoCompleter(tab)
         utils.bind_tab_key(tab.textwidget, completer.on_tab, add=True)
         tab.textwidget.bind('<<CursorMoved>>', completer.reset, add=True)
 
