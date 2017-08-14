5da1f35d367684ef2d4dfa735a1294163cb1c87f
Akuli <akuviljanen17@gmail.com>
2017-08-14 12:55:44 +0300

    autocomplete stuff

--- porcupine/plugins/autocomplete.py
+++ porcupine/plugins/autocomplete.py
@@ -2,16 +2,13 @@
 import collections
 import re
 
-import jedi
-
 import porcupine
 from porcupine import tabs, utils
 
 __all__ = ['register_completer']
 setup_before = ['tabs2spaces']      # see tabs2spaces.py
-jedi.settings.add_bracket_after_function = True
 
-_suffix_finders = {}
+_completers = {}
 
 
 def register_completer(filetype_name, function):
@@ -21,25 +18,56 @@ def register_completer(filetype_name, function):
 
         from porcupine.plugins import autocomplete
 
-        def java_completer(script, cursor_lineno, cursor_column):
-            # script is the full content of the file
-            # this should return an iterable of things that can be inserted
-            # after the current cursor position, or an empty iterable
-            # (e.g. []) for no completions
+        def java_completer(tab):
+            # Do whatever you need to do with the text widget. For example:
+            full_content = tab.textwidget.get('1.0', 'end - 1 char')
+            cursor_line, cursor_col = map(int, tab.textwidget.index('insert')\
+.split('.'))
 
-        autocomplete.register_completer("Java", java_completer)
+            # This should return an iterable of things that can be inserted
+            # after the current cursor position, or e.g. [] for no completions.
+
+        def setup():
+            autocomplete.register_completer("Java", java_completer)
+
+    The ``tab`` argument to *function* is a :class:`porcupine.tabs.Filetab`.
 
     The *filetype_name* should be a key of
-    :data:`porcupine.filetypes.filetypes`. Using this decorator multiple
-    times with the same *filetype_name* overrides previous decoratings.
+    :data:`porcupine.filetypes.filetypes`. Registering multiple completers
+    for the same *filetype_name* overrides previous registrations.
     """
-    _suffix_finders[filetype_name] = function
+    _completers[filetype_name] = function
 
 
-def _jedi_suffix_finder(source, line, column):
-    # TODO: Make this into its own module.
-    script = jedi.Script(source, line, column)
-    return (c.complete for c in script.completions())
+def _fallback_completer(tab):
+    """Find all words in file, sorted by frequency."""
+    before_cursor = tab.textwidget.get('insert linestart', 'insert')
+    after_cursor = tab.textwidget.get('insert', 'insert lineend')
+
+    match = re.search(r'\w+$', before_cursor)
+    if match is None:
+        # can't autocomplete based on this
+        return None
+    prefix = match.group(0)
+
+    # find unique words starting with the prefix
+    # Tcl's regexes don't support \b or a sane way of grouping so
+    # they are kind of useless for this. I guess I should implement
+    # this with Tcl regexes too and check which is faster :)
+    result = collections.Counter()
+    for chunk in tab.textwidget.iter_chunks():
+        print([prefix])
+        result.update(re.findall(r'\b' + prefix + r'(\w+)', chunk))
+
+    # if the cursor is in the middle of a word, that word must not
+    # be completed, e.g. if the user types abcdef and moves the
+    # cursor between c and d, we must not autocomplete to abcdefdef
+    try:
+        del result[re.search(r'^\w*', after_cursor).group(0)]
+    except KeyError:
+        pass
+
+    return sorted(result, key=result.get, reverse=True)
 
 
 class _AutoCompleter:
@@ -50,68 +78,34 @@ class _AutoCompleter:
         self._suffixes = None
         self._completing = False    # avoid recursion
 
-    # find all words in file, sort by frequency
-    def _fallback_suffix_finder(self):
-        before_cursor = self.tab.textwidget.get('insert linestart', 'insert')
-        after_cursor = self.tab.textwidget.get('insert', 'insert lineend')
-
-        match = re.search(r'\w+$', before_cursor)
-        if match is None:
-            # can't autocomplete based on this
-            return None
-        prefix = match.group(0)
-
-        # find unique words starting with the prefix
-        # Tcl's regexes don't support \b or a sane way of grouping so
-        # they are kind of useless for this. I guess I should implement
-        # this with Tcl regexes too and check which is faster :)
-        result = collections.defaultdict(int)
-        for chunk in self.tab.textwidget.iter_chunks():
-            for match in re.finditer(r'\b' + prefix + r'(\w+)', chunk):
-                result[match.group(1)] += 1
-
-        # if the cursor is in the middle of a word, that word must not
-        # be completed, e.g. if the user types abcdef and moves the
-        # cursor between c and d, we must not autocomplete to abcdefdef
-        try:
-            del result[re.search(r'^\w*', after_cursor).group(0)]
-        except KeyError:
-            pass
-
-        return sorted(result, key=result.get, reverse=True)
-
     def _find_suffixes(self):
         before_cursor = self.tab.textwidget.get('insert linestart', 'insert')
         after_cursor = self.tab.textwidget.get('insert', 'insert lineend')
 
-        if before_cursor.isspace() or not before_cursor:
+        print([before_cursor])
+        if re.search(r'\S$', before_cursor) is None:
             # let other plugins handle this however they want to
+            print("give up")
             return None
-        if re.search(r"^\w", after_cursor) is not None:
+        if re.search(r'^\w', after_cursor) is not None:
             # don't complete in the middle of a word
-            return collections.deque()
+            return []
 
-        finder = _suffix_finders.get(self.tab.filetype.name)
-        if finder is None:
-            return self._fallback_suffix_finder()
-
-        source = self.tab.textwidget.get("1.0", "end - 1 char")
-        cursor_pos = self.tab.textwidget.index("insert")
-        line, col = map(int, cursor_pos.split("."))
-        return finder(source, line, col)
+        completer = _completers.get(self.tab.filetype.name,
+                                    _fallback_completer)
+        return completer(self.tab)
 
     def _complete(self, rotation):
         self._completing = True
 
         if self._suffixes is None:
-            self._startpos = self.tab.textwidget.index('insert')
-
             suffixes = self._find_suffixes()
             if suffixes is None:
                 # no completable characters before the cursor, just give
                 # up and allow doing something else on this tab press
                 return None
 
+            self._startpos = self.tab.textwidget.index('insert')
             self._suffixes = collections.deque(suffixes)
             self._suffixes.appendleft('')  # end of completions
 
--- porcupine/plugins/jedi.py
+++ porcupine/plugins/jedi.py
@@ -11,20 +11,33 @@ Or like this on other operating systems::
 """
 
 import logging
+import os
+from porcupine import dirs, utils
 from porcupine.plugins import autocomplete
 
 try:
     import jedi
 except ImportError:
-    logging.getLogger(__name__).exception("jedi is not installed")
+    # the space after the last \n is intentional, logging strips off
+    # trailing newlines
+    logging.getLogger(__name__).exception(
+        "Jedi is not installed. You can install it like this:\n\n" +
+        "    %s -m pip install --user jedi\n ", utils.short_python_command)
     jedi = None
 
 
-def jedi_completer(source, line, column):
-    script = jedi.Script(source, line, column)
+def jedi_completer(tab):
+    source = tab.textwidget.get("1.0", "end - 1 char")
+    cursor_pos = tab.textwidget.index("insert")
+    line, column = map(int, cursor_pos.split("."))
+
+    # the source is already unicode, so jedi doesn't need an encoding
+    script = jedi.Script(source, line, column, path=tab.path)
     return (c.complete for c in script.completions())
 
 
 def setup():
     if jedi is not None:
+        jedi.settings.cache_directory = os.path.join(dirs.cachedir, 'jedi')
+        jedi.settings.case_insensitive_completion = False
         autocomplete.register_completer("Python", jedi_completer)
