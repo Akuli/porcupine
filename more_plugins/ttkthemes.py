e9369eba8f6279c132a77d9d21f0b63080f7e0f0
Akuli <akuviljanen17@gmail.com>
2017-08-24 09:44:30 +0300

    see https://github.com/RedFantom/ttkthemes/issues/5

--- more_plugins/ttkthemes.py
+++ more_plugins/ttkthemes.py
@@ -6,14 +6,7 @@ import ttkthemes
 
 
 def setup():
-    # yes, this is needed
-    ttkthemes.ThemedStyle()
-
-    # ttkthemes wants me to create a ttkthemes.ThemedTk instead of a sane
-    # tkinter.Tk, but i don't like that at all so i need these weird poops
-    root = porcupine.get_main_window().nametowidget('.')
-    root.img_support = False
-    for theme in sorted(ttkthemes.ThemedTk.get_themes(root)):
-        callback = functools.partial(ttkthemes.ThemedTk.set_theme, root, theme)
+    style = ttkthemes.ThemedStyle()
+    for theme in sorted(style.get_themes()):
+        callback = functools.partial(style.set_theme, theme)
         porcupine.add_action(callback, 'Ttk Themes/%s' % theme)
-    del root.img_support
