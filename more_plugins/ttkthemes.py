72c3fc0cdc5f880d5d757a9eda5cd3c19a915682
Akuli <akuviljanen17@gmail.com>
2017-08-30 22:11:08 +0300

    use porcupine.settings

--- more_plugins/ttkthemes.py
+++ more_plugins/ttkthemes.py
@@ -2,25 +2,33 @@
 import functools
 import tkinter
 
-from porcupine import menubar
+from porcupine import menubar, settings
 import ttkthemes
 
+config = settings.get_section('General')
 
-def on_theme_changed(style, var, *stupid_junk):
-    style.set_theme(var.get())
+
+def on_theme_changed(style, theme_name):
+    try:
+        style.set_theme(theme_name)
+    except tkinter.TclError as e:
+        raise settings.InvalidValue(str(e)) from None
 
 
 def setup():
     style = ttkthemes.ThemedStyle()
-    theme_var = tkinter.StringVar()
+
+    # https://github.com/RedFantom/ttkthemes/issues/6
+    # this does what style.theme_use() should do
+    default_theme = style.tk.eval('return $ttk::currentTheme')
+
+    config.add_option('ttk_theme', default_theme, reset=False)
+    config.connect('ttk_theme', functools.partial(on_theme_changed, style),
+                   run_now=True)
 
     menu = menubar.get_menu('Ttk Themes')
     for theme in sorted(style.get_themes()):
         menu.add_radiobutton(label=theme.title(), value=theme,
-                             variable=theme_var)
+                             variable=config.get_var('ttk_theme'))
 
     theme_var.trace('w', functools.partial(on_theme_changed, style, theme_var))
-
-    # https://github.com/RedFantom/ttkthemes/issues/6
-    # this does what theme_var.set(style.theme_use()) should do
-    style.tk.eval('set %s $ttk::currentTheme' % theme_var)
