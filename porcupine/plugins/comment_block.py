43a137ff80d1487eada114b455a35a28a3d9e94a
Akuli <akuviljanen17@gmail.com>
2018-01-05 12:28:26 +0200

    some stuffs

--- porcupine/plugins/comment_block.py
+++ porcupine/plugins/comment_block.py
@@ -2,19 +2,20 @@
 
 import functools
 
-from porcupine import get_tab_manager, tabs, utils
+from porcupine import actions, get_tab_manager, tabs, utils
 
 # update the code if you add a filetype that doesn't use # as comment prefix
 filetype_names = ['Python', 'Makefile', 'Shell', 'Tcl']
 
 
-def comment_or_uncomment(filetab, event):
-    if filetab.filetype.name not in filetype_names:
+def comment_or_uncomment():
+    tab = get_tab_manager().current_tab
+    if tab.filetype.name not in filetype_names:
         # add '#' normally
         return None
 
     try:
-        start_index, end_index = map(str, event.widget.tag_ranges('sel'))
+        start_index, end_index = map(str, tab.textwidget.tag_ranges('sel'))
     except ValueError as e:
         # nothing selected, add '#' normally
         return None
@@ -26,18 +27,18 @@ def comment_or_uncomment(filetab, event):
         end += 1
 
     gonna_uncomment = all(
-        event.widget.get('%d.0' % lineno, '%d.1' % lineno) == '#'
+        tab.textwidget.get('%d.0' % lineno, '%d.1' % lineno) == '#'
         for lineno in range(start, end))
 
     for lineno in range(start, end):
         if gonna_uncomment:
-            event.widget.delete('%d.0' % lineno, '%d.1' % lineno)
+            tab.textwidget.delete('%d.0' % lineno, '%d.1' % lineno)
         else:
-            event.widget.insert('%d.0' % lineno, '#')
+            tab.textwidget.insert('%d.0' % lineno, '#')
 
     # select everything on the (un)commented lines
-    event.widget.tag_remove('sel', '1.0', 'end')
-    event.widget.tag_add('sel', '%d.0' % start, '%d.0' % end)
+    tab.textwidget.tag_remove('sel', '1.0', 'end')
+    tab.textwidget.tag_add('sel', '%d.0' % start, '%d.0' % end)
     return 'break'
 
 
@@ -46,10 +47,12 @@ def on_new_tab(event):
     if isinstance(tab, tabs.FileTab):
         # the '#' character seems to be a 'numbersign' in tk
         tab.textwidget.bind(
-            '<numbersign>', functools.partial(comment_or_uncomment, tab),
-            add=True)
+            '<numbersign>', (lambda event: comment_or_uncomment()), add=True)
 
 
 def setup():
-    # TODO: add an action
+    # the action's binding feature cannot be used because then typing
+    # a '#' outside the main text widget inserts a # to the main widget
+    actions.add_command("Edit/Comment Block", comment_or_uncomment,
+                        filetype_names=filetype_names)
     utils.bind_with_data(get_tab_manager(), '<<NewTab>>', on_new_tab, add=True)
