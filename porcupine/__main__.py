900b709394c618ed62b912a891870d15e293c3c4
Akuli <akuviljanen17@gmail.com>
2018-01-20 23:23:48 +0200

    moar logging

--- porcupine/__main__.py
+++ porcupine/__main__.py
@@ -3,14 +3,13 @@
 import argparse
 import logging
 import os
-import platform
 from queue import Empty         # queue is a handy variable name
 import sys
 import tkinter
 
 import porcupine
-from porcupine import (_ipc, _logs, _pluginloader, _session,
-                       dirs, filetypes, settings, tabs, utils)
+from porcupine import (_ipc, _logs, _pluginloader, _session, dirs, filetypes,
+                       settings, tabs)
 
 log = logging.getLogger(__name__)
 
@@ -100,10 +99,7 @@ Examples:
 
 
 def main():
-    _logs.setup()
-
     parser = argparse.ArgumentParser(
-        prog=('%s -m porcupine' % utils.short_python_command),
         epilog=_EPILOG, formatter_class=argparse.RawDescriptionHelpFormatter)
 
     parser.add_argument(
@@ -138,14 +134,14 @@ def main():
 
     loggroup = parser.add_mutually_exclusive_group()
     loggroup.add_argument(
-        '--logfile', type=argparse.FileType('w'),
-        help=("write information about what Porcupine is doing to this file, "
-              "defaults to a .txt file in %s" % dirs.cachedir))
-    loggroup.add_argument(
-        '--verbose', dest='logfile', action='store_const', const=sys.stderr,
-        help="use stderr as the log file")
+        '--verbose', action='store_true',
+        help=("print all logging messages to stderr, only warnings and errors "
+              "are printed by default"))
 
     args = parser.parse_args()
+
+    dirs.makedirs()
+    _logs.setup(args.verbose)
 
     filelist = []
     for file in args.files:
@@ -179,12 +175,6 @@ def main():
         # not running yet, become the Porcupine that other Porcupines
         # connect to
         pass
-
-    dirs.makedirs()
-    log.info("starting Porcupine %s on %s", porcupine.__version__,
-             platform.platform().replace('-', ' '))
-    log.info("running on Python %d.%d.%d from %s",
-             *(list(sys.version_info[:3]) + [sys.executable]))
 
     root = tkinter.Tk()
     root.title("Porcupine")
--- porcupine/_logs.py
+++ porcupine/_logs.py
@@ -1,12 +1,79 @@
+import glob
+import itertools
 import logging
 import os
+import platform
+import shutil
+import subprocess
 import sys
+import threading
+import time
+
+import porcupine
+from porcupine import dirs
+
+log = logging.getLogger(__name__)
+DAYS = 60*60*24       # seconds in 1 day
 
 
-def setup():
-    handler = logging.StreamHandler(sys.stderr)
-    handler.setLevel(logging.DEBUG)
-    handler.setFormatter(logging.Formatter(
-        "[PID {} %(levelname)s] %(name)s: %(message)s".format(os.getpid())
-    ))
-    logging.basicConfig(level=logging.DEBUG, handlers=[handler])
+def _remove_old_logs():
+    pattern = os.path.join(glob.escape(dirs.cachedir), 'log*.txt')
+    for filename in glob.glob(pattern):
+        path = os.path.join(dirs.cachedir, filename)
+        if time.time() - os.path.getmtime(path) > 3*DAYS:
+            log.info("'%s' is more than 3 days old, removing it")
+            os.remove(path)
+
+
+# running lsb_release takes about 0.12 seconds on this system => thread it :DDD
+# no really i want things to start up as quickly as possible and logging
+# is thread-safe so why not
+def _run_lsb_release():
+    try:
+        output = subprocess.check_output(['lsb_release', '-a'],
+                                         stderr=subprocess.STDOUT)
+        log.info("output from 'lsb_release -a':\n%s",
+                 output.decode('utf-8', errors='replace'))
+    except (subprocess.CalledProcessError, OSError):
+        log.warning("unexpected error when calling lsb_release", exc_info=True)
+
+
+def setup(verbose):
+    print_handler = logging.StreamHandler(sys.stderr)
+    print_handler.setLevel(logging.DEBUG if verbose else logging.WARNING)
+
+    # an iterator 'log.txt', 'log2.txt', 'log3.txt', ...
+    filenames = itertools.chain(
+        ['log.txt'],
+        map('log{}.txt'.format, itertools.count(start=2)),
+    )
+    for filename in filenames:
+        logfile_path = os.path.join(dirs.cachedir, filename)
+        try:
+            # x means create without overwriting
+            file_handler = logging.FileHandler(logfile_path, 'x')
+        except FileExistsError:
+            continue
+        file_handler.setLevel(logging.DEBUG)
+        break
+
+    logging.basicConfig(level=logging.DEBUG,  # no idea why this is needed
+                        handlers=[print_handler, file_handler],
+                        format="[%(levelname)s] %(name)s: %(message)s")
+
+    log.info("starting Porcupine %s from '%s'", porcupine.__version__,
+             porcupine.__path__[0])
+    log.info("PID %d, log file '%s'", os.getpid(), logfile_path)
+    log.info("running on Python %d.%d.%d from '%s'",
+             *(list(sys.version_info[:3]) + [sys.executable]))
+    log.info("platform.system() returned %r", platform.system())
+    if shutil.which('lsb_release') is not None:
+        threading.Thread(target=_run_lsb_release).start()
+    else:   # e.g. windows
+        log.info("platform.platform() returned %r", platform.platform())
+
+    # don't fail to run if old logs can't be deleted for some reason
+    try:
+        _remove_old_logs()
+    except OSError:
+        log.exception("unexpected problem with removing old log files")
