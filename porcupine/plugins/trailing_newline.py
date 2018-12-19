928b1d0a107c2d8e713bccd5d27b935960341de6
Akuli <akuviljanen17@gmail.com>
2018-12-19 21:29:05 +0200

    trailing_newline plugin pythotk

--- porcupine/plugins/trailing_newline.py
+++ porcupine/plugins/trailing_newline.py
@@ -1,22 +1,23 @@
 """Add a trailing newline character to files when saving."""
 
-from porcupine import get_tab_manager, tabs, utils
+import functools
+
+from porcupine import get_tab_manager, tabs
 
 
-def on_save(event):
-    textwidget = event.widget.textwidget
-    if textwidget.get('end - 2 chars', 'end - 1 char') != '\n':
+def on_save(textwidget):
+    if textwidget.get(textwidget.end.back(chars=1), textwidget.end) != '\n':
         # doesn't end with a \n yet, be sure not to annoyingly move the
         # cursor like IDLE does
-        cursor = textwidget.index('insert')
-        textwidget.insert('end - 1 char', '\n')
-        textwidget.mark_set('insert', cursor)
+        cursor = textwidget.marks['insert']
+        textwidget.insert(textwidget.end, '\n')
+        textwidget.marks['insert'] = cursor
 
 
-def on_new_tab(event):
-    if isinstance(event.data_widget, tabs.FileTab):
-        event.data_widget.bind('<<Save>>', on_save, add=True)
+def on_new_tab(tab):
+    if isinstance(tab, tabs.FileTab):
+        tab.on_save.connect(functools.partial(on_save, tab.textwidget))
 
 
 def setup():
-    utils.bind_with_data(get_tab_manager(), '<<NewTab>>', on_new_tab, add=True)
+    get_tab_manager().on_new_tab.connect(on_new_tab)
