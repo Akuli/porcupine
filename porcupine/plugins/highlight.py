104c1912c859a9f2411d720f6f06db0ac2cb16c4
Akuli <akuviljanen17@gmail.com>
2018-12-19 12:19:17 +0200

    highlight plugin pythotk

--- porcupine/plugins/highlight.py
+++ porcupine/plugins/highlight.py
@@ -8,11 +8,11 @@
 
 import multiprocessing
 import queue
-import tkinter.font as tkfont
 
 import pygments.styles
 import pygments.token
 import pygments.util   # only for ClassNotFound, the docs say that it's here
+import pythotk as tk
 
 from porcupine import filetypes, get_tab_manager, settings, tabs, utils
 
@@ -23,8 +23,6 @@ def _list_all_token_types(tokentype):
     yield tokentype
     for sub in map(_list_all_token_types, tokentype.subtypes):
         yield from sub
-
-_ALL_TAGS = set(map(str, _list_all_token_types(pygments.token.Token)))  # noqa
 
 
 # tokenizing with pygments is the bottleneck of this thing (at least on
@@ -37,7 +35,7 @@ class PygmentizerProcess:
         self.process = multiprocessing.Process(target=self._run)
         self.process.start()
 
-    # returns {str(tokentype): [start1, end1, start2, end2, ...]}
+    # returns {str(tokentype): [(start1, end1), (start2, end2), ...]}
     def _pygmentize(self, filetype, code):
         # pygments doesn't include any info about where the tokens are
         # so we need to do it manually :(
@@ -47,14 +45,14 @@ class PygmentizerProcess:
 
         result = {}
         for tokentype, string in lexer.get_tokens(code):
-            start = '%d.%d' % (lineno, column)
+            start = (lineno, column)
             if '\n' in string:
                 lineno += string.count('\n')
                 column = len(string.rsplit('\n', 1)[1])
             else:
                 column += len(string)
-            end = '%d.%d' % (lineno, column)
-            result.setdefault(str(tokentype), []).extend([start, end])
+            end = (lineno, column)
+            result.setdefault(str(tokentype), []).append((start, end))
 
         return result
 
@@ -81,12 +79,15 @@ class Highlighter:
         self._get_filetype = filetype_getter
         self.pygmentizer = PygmentizerProcess()
 
+        tag_names = set(map(str, _list_all_token_types(pygments.token.Token)))
+        self.all_tags = [self.textwidget.get_tag(name) for name in tag_names]
+
         # the tags use fonts from here
         self._fonts = {}
         for bold in (True, False):
             for italic in (True, False):
                 # the fonts will be updated later, see _on_config_changed()
-                self._fonts[(bold, italic)] = tkfont.Font(
+                self._fonts[(bold, italic)] = tk.NamedFont(
                     weight=('bold' if bold else 'normal'),
                     slant=('italic' if italic else 'roman'))
 
@@ -95,9 +96,9 @@ class Highlighter:
         config.connect('font_family', self._on_config_changed, run_now=False)
         config.connect('font_size', self._on_config_changed, run_now=False)
         self._on_config_changed()
-        self.textwidget.after(50, self._do_highlights)
+        tk.after(50, self._do_highlights)
 
-    def on_destroy(self, junk=None):
+    def on_destroy(self):
         config.disconnect('pygments_style', self._on_config_changed)
         config.disconnect('font_family', self._on_config_changed)
         config.disconnect('font_size', self._on_config_changed)
@@ -106,18 +107,11 @@ class Highlighter:
         self.pygmentizer.process.terminate()
         #print("terminated", repr(self.pygmentizer.process))
 
-    def _on_config_changed(self, junk=None):
-        # when the font family or size changes, self.textwidget['font']
-        # also changes because it's a porcupine.textwiddet.ThemedText widget
-        fontobject = tkfont.Font(name=self.textwidget['font'], exists=True)
-        font_updates = fontobject.actual()
-        del font_updates['weight']     # ignore boldness
-        del font_updates['slant']      # ignore italicness
-
+    def _on_config_changed(self, junk_config_value=None):
+        # the text widget uses TkFixedFont
         for (bold, italic), font in self._fonts.items():
-            # fonts don't have an update() method
-            for key, value in font_updates.items():
-                font[key] = value
+            font.family = tk.NamedFont('TkFixedFont').family
+            font.size = tk.NamedFont('TkFixedFont').size
 
         # http://pygments.org/docs/formatterdevelopment/#styles
         # all styles seem to yield all token types when iterated over,
@@ -128,32 +122,35 @@ class Highlighter:
             # i don't like random underlines in my code and i don't know
             # how to implement the border with tkinter
             key = (infodict['bold'], infodict['italic'])   # pep8 line length
-            kwargs = {'font': self._fonts[key]}
+            configs = {'font': self._fonts[key]}
             if infodict['color'] is None:
-                kwargs['foreground'] = ''    # reset it
+                configs['foreground'] = ''    # reset it
             else:
-                kwargs['foreground'] = '#' + infodict['color']
+                configs['foreground'] = '#' + infodict['color']
             if infodict['bgcolor'] is None:
-                kwargs['background'] = ''
+                configs['background'] = ''
             else:
-                kwargs['background'] = '#' + infodict['bgcolor']
+                configs['background'] = '#' + infodict['bgcolor']
 
-            self.textwidget.tag_config(str(tokentype), **kwargs)
+            self.textwidget.get_tag(str(tokentype)).update(configs)
 
             # make sure that the selection tag takes precedence over our
             # token tag
-            self.textwidget.tag_lower(str(tokentype), 'sel')
+            # TODO: add 'tag lower' to pythotk
+            tk.tcl_call(None, self.textwidget, 'tag', 'lower',
+                        str(tokentype), 'sel')
 
     # handle things from the highlighting process
+    # TODO: use pythotk.init_threads() stuff here?
     def _do_highlights(self):
-        # this check is actually unnecessary; turns out that destroying
-        # the text widget stops this timeout because the text widget's
-        # after method was used, but i don't feel like relying on it
+        # this check is kinda not necessary; turns out that quitting porcupine
+        # stops this timeout, but i don't feel like relying on it
         if not self.pygmentizer.process.is_alive():
             return
 
         # if the pygmentizer process has put multiple result dicts to
-        # the queue, only use the last one
+        # the queue, only use the last one, this prevents slowness when typing
+        # fast
         tags2add = None
         try:
             while True:
@@ -163,65 +160,32 @@ class Highlighter:
 
         if tags2add is not None:
             # print("_do_highlights: got something")
-            for tag in _ALL_TAGS:
-                self.textwidget.tag_remove(tag, '0.0', 'end')
-            for tag, places in tags2add.items():
-                self.textwidget.tag_add(tag, *places)
+            for tag in self.all_tags:
+                tag.remove()
+            for tagname, places in tags2add.items():
+                tag = self.textwidget.get_tag(tagname)
+                for start, end in places:
+                    tag.add(start, end)
 
         # 50 milliseconds doesn't seem too bad, bigger timeouts tend to
         # make things laggy
-        self.textwidget.after(50, self._do_highlights)
+        tk.after(50, self._do_highlights)
 
-    def highlight_all(self, junk=None):
-        code = self.textwidget.get('1.0', 'end - 1 char')
-        self.pygmentizer.in_queue.put([self._get_filetype(), code])
+    def highlight_all(self):
+        code = self.textwidget.get()
+        self.pygmentizer.in_queue.put((self._get_filetype(), code))
 
 
-def on_new_tab(event):
-    tab = event.data_widget
+def on_new_tab(tab):
     if not isinstance(tab, tabs.FileTab):
         return
 
     highlighter = Highlighter(tab.textwidget, (lambda: tab.filetype))
-    tab.bind('<<FiletypeChanged>>', highlighter.highlight_all, add=True)
-    tab.textwidget.bind('<<ContentChanged>>', highlighter.highlight_all,
-                        add=True)
-    tab.bind('<Destroy>', highlighter.on_destroy, add=True)
+    tab.on_filetype_changed.connect(highlighter.highlight_all)
+    tab.textwidget.bind('<<ContentChanged>>', highlighter.highlight_all)
+    tab.textwidget.bind('<Destroy>', highlighter.on_destroy)
     highlighter.highlight_all()
 
 
 def setup():
-    utils.bind_with_data(get_tab_manager(), '<<NewTab>>', on_new_tab, add=True)
-
-
-if __name__ == '__main__':
-    # simple test
-    # FIXME: doesnt work
-    import tkinter
-    from porcupine.settings import load as load_settings
-
-    def on_modified(event):
-        text.unbind('<<Modified>>')
-        text.edit_modified(False)
-        text.bind('<<Modified>>', on_modified)
-        text.after_idle(highlighter.highlight_all)
-
-    root = tkinter.Tk()
-    load_settings()     # must be after creating root window
-    text = tkinter.Text(root, insertbackground='red')
-    text.pack(fill='both', expand=True)
-    text.bind('<<Modified>>', on_modified)
-
-    # The theme doesn't display perfectly here because the highlighter
-    # only does tags, not foreground, background etc. See textwidget.py.
-    highlighter = Highlighter(
-        text, (lambda: filetypes.get_filetype_by_name('Python')))
-
-    with open(__file__, 'r') as f:
-        text.insert('1.0', f.read())
-    text.see('end')
-
-    try:
-        root.mainloop()
-    finally:
-        highlighter.on_destroy()
+    get_tab_manager().on_new_tab.connect(on_new_tab)
