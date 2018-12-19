a4cf8f76a6098b23a6758ec560f5b0821e16e130
Akuli <akuviljanen17@gmail.com>
2018-12-19 21:17:25 +0200

    rstrip plugin pythotk

--- porcupine/plugins/rstrip.py
+++ porcupine/plugins/rstrip.py
@@ -1,26 +1,26 @@
 """Remove trailing whitespace when enter is pressed."""
 
-from porcupine import get_tab_manager, tabs, utils
+import pythotk as tk
+
+from porcupine import get_tab_manager, tabs
 
 
 def after_enter(textwidget):
     """Strip trailing whitespace at the end of a line."""
-    lineno = int(textwidget.index('insert').split('.')[0]) - 1
-    line = textwidget.get('%d.0' % lineno, '%d.0 lineend' % lineno)
+    line_start = textwidget.marks['insert'].linestart().back(lines=1)
+    line = textwidget.get(line_start, line_start.lineend())
     if len(line) != len(line.rstrip()):
-        textwidget.delete('%d.%d' % (lineno, len(line.rstrip())),
-                          '%d.0 lineend' % lineno)
+        textwidget.delete(
+            (line_start.line, len(line.rstrip())), line_start.lineend())
 
 
-def on_new_tab(event):
-    if isinstance(event.data_widget, tabs.FileTab):
-        textwidget = event.data_widget.textwidget
+def on_new_tab(tab):
+    if isinstance(tab, tabs.FileTab):
+        def bind_callback():
+            tk.after_idle(after_enter, args=[tab.textwidget])
 
-        def bind_callback(event):
-            textwidget.after_idle(after_enter, textwidget)
-
-        textwidget.bind('<Return>', bind_callback, add=True)
+        tab.textwidget.bind('<Return>', bind_callback)
 
 
 def setup():
-    utils.bind_with_data(get_tab_manager(), '<<NewTab>>', on_new_tab, add=True)
+    get_tab_manager().on_new_tab.connect(on_new_tab)
