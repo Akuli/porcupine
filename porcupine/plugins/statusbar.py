f2d7ca8f630b7d0cbd34bd8e9873c0c2412f6eae
Akuli <akuviljanen17@gmail.com>
2018-12-19 21:26:04 +0200

    statusbar plugin pythotk

--- porcupine/plugins/statusbar.py
+++ porcupine/plugins/statusbar.py
@@ -1,4 +1,4 @@
-from tkinter import ttk
+import pythotk as tk
 
 from porcupine import get_tab_manager, utils
 
@@ -8,50 +8,51 @@ from porcupine import get_tab_manager, utils
 
 
 # this widget is kind of weird
-class LabelWithEmptySpaceAtLeft(ttk.Label):
+class LabelWithEmptySpaceAtLeft(tk.Label):
 
     def __init__(self, master):
-        self._spacer = ttk.Frame(master)
+        self._spacer = tk.Frame(master)
         self._spacer.pack(side='left', expand=True)
         super().__init__(master)
         self.pack(side='left')
 
-    def destroy(self):
+    # i don't want to override destroy() because pythotk can call it too
+    def destroy_me(self):
         self._spacer.destroy()
-        super().destroy()
+        self.destroy()
 
 
-class StatusBar(ttk.Frame):
+class StatusBar(tk.Frame):
 
     def __init__(self, master, tab):
         super().__init__(master)
         self.tab = tab
         # one label for each tab-separated thing
-        self.labels = [ttk.Label(self)]
+        self.labels = [tk.Label(self)]
         self.labels[0].pack(side='left')
 
-        tab.bind('<<StatusChanged>>', self.do_update, add=True)
-        self.do_update()
+        tab.on_status_changed.connect(self.update)
+        self.update()
 
-    # this is do_update() because tkinter has a method called update()
-    def do_update(self, junk=None):
+    # don't do this in tkinter, it has its own method called update
+    def update(self):
         parts = self.tab.status.split('\t')
+        assert len(parts) != 0
 
         # there's always at least one part, the label added in
         # __init__ is not destroyed here
         while len(self.labels) > len(parts):
-            self.labels.pop().destroy()
+            self.labels.pop().destroy_me()
         while len(self.labels) < len(parts):
             self.labels.append(LabelWithEmptySpaceAtLeft(self))
 
         for label, text in zip(self.labels, parts):
-            label['text'] = text
+            label.config['text'] = text
 
 
-def on_new_tab(event):
-    tab = event.data_widget
+def on_new_tab(tab):
     StatusBar(tab.bottom_frame, tab).pack(side='bottom', fill='x')
 
 
 def setup():
-    utils.bind_with_data(get_tab_manager(), '<<NewTab>>', on_new_tab, add=True)
+    get_tab_manager().on_new_tab.connect(on_new_tab)
