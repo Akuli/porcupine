84c3e80034f6671f5d723f8bcc6dee3d4a536e7a
Akuli <akuviljanen17@gmail.com>
2018-12-19 11:09:23 +0200

    fullscreen plugin pythotk

--- porcupine/plugins/fullscreen.py
+++ porcupine/plugins/fullscreen.py
@@ -1,11 +1,14 @@
+import pythotk as tk
+
 from porcupine import actions, get_main_window
 
 
-def on_var_changed(variable_name, junk, trace_mode):
-    window = get_main_window()
-    window.attributes('-fullscreen', window.getvar(variable_name))
+def on_var_changed(var):
+    # TODO: add 'wm attributes' to pythotk
+    tk.tcl_call(None, 'wm', 'attributes', get_main_window().toplevel,
+                '-fullscreen', var.get())
 
 
 def setup():
     action = actions.add_yesno("View/Full Screen", False, '<F11>')
-    action.var.trace('w', on_var_changed)
+    action.var.write_trace.connect(on_var_changed)
