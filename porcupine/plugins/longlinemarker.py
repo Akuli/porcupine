f26533c7b0d5419a3df405c9ff99ab7f2237f9b6
Akuli <akuviljanen17@gmail.com>
2018-12-19 17:42:00 +0200

    longlinemarker plugin pythotk

--- porcupine/plugins/longlinemarker.py
+++ porcupine/plugins/longlinemarker.py
@@ -1,12 +1,11 @@
 """Maximum line length marker for Tkinter's text widget."""
-
-import tkinter
-import tkinter.font as tkfont
+# TODO: support horizontal scrolling
 
 import pygments.styles
 import pygments.token
+import pythotk as tk
 
-from porcupine import get_tab_manager, settings, tabs, utils
+from porcupine import get_tab_manager, settings, tabs
 
 config = settings.get_section('General')
 
@@ -18,32 +17,34 @@ class LongLineMarker:
 
         # this must not be a ttk frame because the background color
         # comes from the pygments style, not from the ttk theme
-        self.frame = tkinter.Frame(filetab.textwidget, width=1)
+        self.frame_command = filetab.textwidget.to_tcl() + '.longlinemarker'
+        tk.tcl_call(None, 'frame', self.frame_command, '-width', '1')
         self._height = 0        # on_configure() will run later
 
     def setup(self):
         config.connect('font_family', self.do_update, run_now=False)
         config.connect('font_size', self.do_update, run_now=False)
         config.connect('pygments_style', self.on_style_changed)
-        self.tab.textwidget.bind('<Configure>', self.on_configure, add=True)
-        self.tab.bind('<<FiletypeChanged>>', self.do_update, add=True)
-        self.tab.bind('<Destroy>', self.on_destroy, add=True)
+        self.tab.textwidget.bind('<Configure>', self.on_configure, event=True)
+        self.tab.on_filetype_changed.connect(self.do_update)
+        self.tab.textwidget.bind('<Destroy>', self.on_destroy)
         self.do_update()
 
-    def on_destroy(self, event):
+    def on_destroy(self):
         config.disconnect('font_family', self.do_update)
         config.disconnect('font_size', self.do_update)
         config.disconnect('pygments_style', self.on_style_changed)
 
-    def do_update(self, junk=None):
+    def do_update(self):
         column = self.tab.filetype.max_line_length
         if column == 0:
             # maximum line length is disabled, see filetypes.ini docs
-            return
-
-        font = tkfont.Font(name=self.tab.textwidget['font'], exists=True)
-        where = font.measure(' ' * self.tab.filetype.max_line_length)
-        self.frame.place(x=where, height=self._height)
+            tk.tcl_call(None, 'place', 'forget', self.frame_command)
+        else:
+            font = tk.NamedFont('TkFixedFont')
+            where = font.measure(' ' * column)
+            tk.tcl_call(None, 'place', self.frame_command,
+                        '-x', where, '-height', self._height)
 
     def on_style_changed(self, name):
         # do the same thing as porcupine's color theme menu does
@@ -52,21 +53,22 @@ class LongLineMarker:
             if tokentype in infos:
                 for key in ['bgcolor', 'color', 'border']:
                     if infos[tokentype][key] is not None:
-                        self.frame['bg'] = '#' + infos[tokentype][key]
+                        tk.tcl_call(None, self.frame_command, 'configure',
+                                    '-bg', '#' + infos[tokentype][key])
                         return
 
         # stupid fallback
-        self.frame['bg'] = 'red'
+        tk.tcl_call(None, self.frame_command, 'configure', '-bg', 'red')
 
     def on_configure(self, event):
         self._height = event.height
         self.do_update()
 
 
-def on_new_tab(event):
-    if isinstance(event.data_widget, tabs.FileTab):
-        LongLineMarker(event.data_widget).setup()
+def on_new_tab(tab):
+    if isinstance(tab, tabs.FileTab):
+        LongLineMarker(tab).setup()
 
 
 def setup():
-    utils.bind_with_data(get_tab_manager(), '<<NewTab>>', on_new_tab, add=True)
+    get_tab_manager().on_new_tab.connect(on_new_tab)
