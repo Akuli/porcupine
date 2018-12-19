be1b243b0d3721a751f49cb2ddb87fc684ab90cf
Akuli <akuviljanen17@gmail.com>
2018-12-19 11:06:12 +0200

    find plugin pythotk

--- porcupine/plugins/find.py
+++ porcupine/plugins/find.py
@@ -4,9 +4,9 @@
 
 import re
 import sys
-import tkinter as tk
-from tkinter import ttk
 import weakref
+
+import pythotk as tk
 
 from porcupine import actions, get_tab_manager, images, tabs
 
@@ -15,13 +15,13 @@ from porcupine import actions, get_tab_manager, images, tabs
 finders = weakref.WeakKeyDictionary()
 
 
-class Finder(ttk.Frame):
+class Finder(tk.Frame):
     """A widget for finding and replacing text.
 
     Use the pack geometry manager with this widget.
     """
 
-    def __init__(self, parent, textwidget, **kwargs):
+    def __init__(self, parent, textwidget: tk.Text, **kwargs):
         super().__init__(parent, **kwargs)
         self._textwidget = textwidget
 
@@ -41,17 +41,15 @@ class Finder(ttk.Frame):
         #
         # note that column 2 is used just for spacing, the separator helps
         # distinguish this from e.g. status bar below this
-        self.grid_columnconfigure(2, minsize=30)
-        self.grid_columnconfigure(3, weight=1)
 
         # TODO: use the pygments theme somehow?
-        textwidget.tag_config('find_highlight',
-                              foreground='black', background='yellow')
+        self._highlight_tag = textwidget.get_tag('find_highlight')
+        self._highlight_tag['foreground'] = 'black'
+        self._highlight_tag['background'] = 'yellow'
 
         self.find_entry = self._add_entry(0, "Find:")
-        find_var = self.find_entry['textvariable'] = tk.StringVar()
-        find_var.trace('w', self.highlight_all_matches)
-        self.find_entry.lol = find_var     # because cpython gc
+        find_var = self.find_entry.config['textvariable'] = tk.StringVar()
+        find_var.write_trace.connect(self.highlight_all_matches)
 
         self.replace_entry = self._add_entry(1, "Replace with:")
 
@@ -60,19 +58,20 @@ class Finder(ttk.Frame):
 
         # commented out because pressing tab in self.find_entry unselects the
         # text in textwidget for some reason
+        # FIXME
         #self.replace_entry.bind('<Return>', self._replace_this)
 
-        buttonframe = ttk.Frame(self)
+        buttonframe = tk.Frame(self)
         buttonframe.grid(row=2, column=0, columnspan=4, sticky='we')
 
-        self.previous_button = ttk.Button(buttonframe, text="Previous match",
-                                          command=self._go_to_previous_match)
-        self.next_button = ttk.Button(buttonframe, text="Next match",
-                                      command=self._go_to_next_match)
-        self.replace_this_button = ttk.Button(
+        self.previous_button = tk.Button(buttonframe, text="Previous match",
+                                         command=self._go_to_previous_match)
+        self.next_button = tk.Button(buttonframe, text="Next match",
+                                     command=self._go_to_next_match)
+        self.replace_this_button = tk.Button(
             buttonframe, text="Replace this match",
             command=self._replace_this)
-        self.replace_all_button = ttk.Button(
+        self.replace_all_button = tk.Button(
             buttonframe, text="Replace all",
             command=self._replace_all)
 
@@ -83,90 +82,87 @@ class Finder(ttk.Frame):
         self._update_buttons()
 
         self.full_words_var = tk.BooleanVar()
-        self.full_words_var.trace('w', self.highlight_all_matches)
+        self.full_words_var.set(False)
+        self.full_words_var.write_trace.connect(self.highlight_all_matches)
         self.ignore_case_var = tk.BooleanVar()
-        self.ignore_case_var.trace('w', self.highlight_all_matches)
+        self.ignore_case_var.set(False)
+        self.ignore_case_var.write_trace.connect(self.highlight_all_matches)
 
-        ttk.Checkbutton(
+        tk.Checkbutton(
             self, text="Full words only", variable=self.full_words_var).grid(
                 row=0, column=3, sticky='w')
-        ttk.Checkbutton(
+        tk.Checkbutton(
             self, text="Ignore case", variable=self.ignore_case_var).grid(
                 row=1, column=3, sticky='w')
 
-        self.statuslabel = ttk.Label(self)
+        self.statuslabel = tk.Label(self)
         self.statuslabel.grid(row=3, column=0, columnspan=4, sticky='we')
 
-        ttk.Separator(self, orient='horizontal').grid(
+        tk.Separator(self, orient='horizontal').grid(
             row=4, column=0, columnspan=4, sticky='we')
 
-        closebutton = ttk.Label(self, cursor='hand2')
+        closebutton = tk.Label(self, cursor='hand2',
+                               image=images.get('closebutton'))
         closebutton.place(relx=1, rely=0, anchor='ne')
         closebutton.bind('<Button-1>', self.hide)
 
-        # TODO: figure out why images don't work in tests
-        if 'pytest' not in sys.modules:     # pragma: no cover
-            closebutton['image'] = images.get('closebutton')
+        self.grid_columns[2].config['minsize'] = 30
+        self.grid_columns[3].config['weight'] = 1
 
         # explained in test_find_plugin.py
-        textwidget.bind('<<Selection>>', self._update_buttons, add=True)
+        textwidget.bind('<<Selection>>', self._update_buttons)
 
     def _add_entry(self, row, text):
-        ttk.Label(self, text=text).grid(row=row, column=0, sticky='w')
-        entry = ttk.Entry(self, width=35, font='TkFixedFont')
+        tk.Label(self, text=text).grid(row=row, column=0, sticky='w')
+        entry = tk.Entry(self, width=35, font='TkFixedFont')
         entry.bind('<Escape>', self.hide)
         entry.grid(row=row, column=1, sticky='we')
         return entry
 
     def show(self):
         self.pack(fill='x')
-        self.find_entry.focus_set()
+        self.find_entry.focus()
         self.highlight_all_matches()
 
-    def hide(self, junk_event=None):
+    def hide(self):
         # remove previous highlights from highlight_all_matches
-        self._textwidget.tag_remove('find_highlight', '1.0', 'end')
+        self._highlight_tag.remove()
 
         self.pack_forget()
-        self._textwidget.focus_set()
-
-    # tag_ranges returns (start1, end1, start2, end2, ...), and this thing
-    # gives a list of (start, end) pairs
-    def get_match_ranges(self):
-        starts_and_ends = list(
-            map(str, self._textwidget.tag_ranges('find_highlight')))
-        assert len(starts_and_ends) % 2 == 0
-        pairs = list(zip(starts_and_ends[0::2], starts_and_ends[1::2]))
-        return pairs
+        self._textwidget.focus()
 
     # must be called when going to another match or replacing becomes possible
-    # or impossible, i.e. when find_highlight areas or the selection changes
-    def _update_buttons(self, junk_event=None):
+    # or impossible, i.e. when _highlight_tag areas or the selection changes
+    def _update_buttons(self):
         matches_something_state = (
-            'normal' if self.get_match_ranges() else 'disabled')
+            'normal' if self._highlight_tag.ranges() else 'disabled')
 
         try:
-            start, end = map(str, self._textwidget.tag_ranges('sel'))
+            [(start, end)] = self._textwidget.get_tag('sel').ranges()
         except ValueError:
             replace_this_state = 'disabled'
         else:   # no, elif doesn't work here
-            if (start, end) in self.get_match_ranges():
+            if (start, end) in self._highlight_tag.ranges():
                 replace_this_state = 'normal'
             else:
                 replace_this_state = 'disabled'
 
-        self.previous_button['state'] = matches_something_state
-        self.next_button['state'] = matches_something_state
-        self.replace_this_button['state'] = replace_this_state
-        self.replace_all_button['state'] = matches_something_state
+        self.previous_button.config['state'] = matches_something_state
+        self.next_button.config['state'] = matches_something_state
+        self.replace_this_button.config['state'] = replace_this_state
+        self.replace_all_button.config['state'] = matches_something_state
 
     def _get_matches_to_highlight(self, looking4):
-        search_opts = {'nocase': self.ignore_case_var.get()}
+        search_opts = []
+
+        if self.ignore_case_var.get():
+            search_opts.append('-nocase')
+
         if self.full_words_var.get():
             # tk doesn't have python-style \b, but it has \m and \M that match
             # the beginning and end of word, see re_syntax(3tcl)
             search_arg = r'\m' + looking4 + r'\M'
-            search_opts['regexp'] = True
+            search_opts.append('-regexp')
         else:
             search_arg = looking4
 
@@ -183,141 +179,146 @@ class Finder(ttk.Frame):
                 start_index_for_search = start_index
                 first_time = False
             else:
-                start_index_for_search = '%s + 1 char' % start_index
+                start_index_for_search = start_index.forward(chars=1)
 
-            start_index = self._textwidget.search(
-                search_arg, start_index_for_search, 'end', **search_opts)
-            if not start_index:
+            # TODO: add search to pythotk
+            args = search_opts + ['--', search_arg, start_index_for_search,
+                                  self._textwidget.end]
+            start_index = tk.tcl_call(
+                self._textwidget.TextIndex, self._textwidget, 'search', *args)
+            if start_index is None:
                 # no more matches
                 break
             yield start_index
 
-    def highlight_all_matches(self, *junk):
+    def highlight_all_matches(self, junk_var=None):
         # clear previous highlights
-        self._textwidget.tag_remove('find_highlight', '1.0', 'end')
+        self._highlight_tag.remove()
 
-        looking4 = self.find_entry.get()
+        looking4 = self.find_entry.text
         if not looking4:    # don't search for empty string
             self._update_buttons()
-            self.statuslabel['text'] = "Type something to find."
+            self.statuslabel.config['text'] = "Type something to find."
             return
         if self.full_words_var.get():
             # check for non-wordy characters
             match = re.search(r'\W', looking4)
             if match is not None:
                 self._update_buttons()
-                self.statuslabel['text'] = ('The search string can\'t contain '
-                                            '"%s" when "Full words only" is '
-                                            'checked.' % match.group(0))
+                self.statuslabel.config['text'] = (
+                    "The search string can't contain "
+                    '"%s" when "Full words only" is checked.' % match.group(0))
                 return
 
         count = 0
         for start_index in self._get_matches_to_highlight(looking4):
-            self._textwidget.tag_add(
-                'find_highlight', start_index,
-                '%s + %d chars' % (start_index, len(looking4)))
+            self._highlight_tag.add(start_index,
+                                    start_index.forward(chars=len(looking4)))
             count += 1
 
         self._update_buttons()
         if count == 0:
-            self.statuslabel['text'] = "Found no matches :("
+            self.statuslabel.config['text'] = "Found no matches :("
         elif count == 1:
-            self.statuslabel['text'] = "Found 1 match."
+            self.statuslabel.config['text'] = "Found 1 match."
         else:
-            self.statuslabel['text'] = "Found %d matches." % count
+            self.statuslabel.config['text'] = "Found %d matches." % count
 
     def _select_range(self, start, end):
-        # the tag_lower makes sure sel shows up, hiding find_highlight under it
-        self._textwidget.tag_lower('find_highlight', 'sel')
-        self._textwidget.tag_remove('sel', '1.0', 'end')
-        self._textwidget.tag_add('sel', start, end)
-        self._textwidget.mark_set('insert', start)
-        self._textwidget.see(start)
+        # the tag_lower makes sure sel shows up, hiding _highlight_tag under it
+        # TODO: add tag lower to pythotk
+        tk.tcl_call(None, self._textwidget, 'tag', 'lower',
+                    self._highlight_tag, 'sel')
+        self._textwidget.get_tag('sel').remove()
+        self._textwidget.get_tag('sel').add(start, end)
+        self._textwidget.marks['insert'] = start
+        # TODO: add see to pythotk
+        tk.tcl_call(None, self._textwidget, 'see', start)
 
     # TODO: adjust scrolling accordingly
-    def _go_to_next_match(self, junk_event=None):
-        pairs = self.get_match_ranges()
+    def _go_to_next_match(self):
+        pairs = self._highlight_tag.ranges()
         if not pairs:
             # the "Next match" button is disabled in this case, but the key
             # binding of the find entry is not
-            self.statuslabel['text'] = "No matches found!"
+            self.statuslabel.config['text'] = "No matches found!"
             return
 
         # find first pair that starts after the cursor
         for start, end in pairs:
-            if self._textwidget.compare(start, '>', 'insert'):
+            if start > self._textwidget.marks['insert']:
                 self._select_range(start, end)
                 break
         else:
             # reached end of file, use the first match
             self._select_range(*pairs[0])
 
-        self.statuslabel['text'] = ""
+        self.statuslabel.config['text'] = ""
         self._update_buttons()
 
     # see _go_to_next_match for comments
-    def _go_to_previous_match(self, junk_event=None):
-        pairs = self.get_match_ranges()
+    def _go_to_previous_match(self):
+        pairs = self._highlight_tag.ranges()
         if not pairs:
-            self.statuslabel['text'] = "No matches found!"
+            self.statuslabel.config['text'] = "No matches found!"
             return
 
         for start, end in reversed(pairs):
-            if self._textwidget.compare(start, '<', 'insert'):
+            if start < self._textwidget.marks['insert']:
                 self._select_range(start, end)
                 break
         else:
             self._select_range(*pairs[-1])
 
-        self.statuslabel['text'] = ""
+        self.statuslabel.config['text'] = ""
         self._update_buttons()
         return
 
-    def _replace_this(self, junk_event=None):
-        if str(self.replace_this_button['state']) == 'disabled':
-            self.statuslabel['text'] = (
+    def _replace_this(self):
+        if self.replace_this_button.config['state'] == 'disabled':
+            self.statuslabel.config['text'] = (
                 'Click "Previous match" or "Next match" first.')
             return
 
         # highlighted areas must not be moved after .replace, think about what
         # happens when you replace 'asd' with 'asd'
-        start, end = self._textwidget.tag_ranges('sel')
-        self._textwidget.tag_remove('find_highlight', start, end)
+        start, end = self._textwidget.get_tag('sel').ranges()
+        self._highlight_tag.remove(start, end)
         self._update_buttons()
-        self._textwidget.replace(start, end, self.replace_entry.get())
+        self._textwidget.replace(start, end, self.replace_entry.text)
 
-        self._textwidget.mark_set('insert', start)
+        self._textwidget.marks['insert'] = start
         self._go_to_next_match()
 
-        left = len(self.get_match_ranges())
+        left = len(self._highlight_tag.ranges())
         if left == 0:
-            self.statuslabel['text'] = "Replaced the last match."
+            self.statuslabel.config['text'] = "Replaced the last match."
         elif left == 1:
-            self.statuslabel['text'] = (
+            self.statuslabel.config['text'] = (
                 "Replaced a match. There is 1 more match.")
         else:
-            self.statuslabel['text'] = (
+            self.statuslabel.config['text'] = (
                 "Replaced a match. There are %d more matches." % left)
 
     def _replace_all(self):
-        match_ranges = self.get_match_ranges()
+        match_ranges = self._highlight_tag.ranges()
 
         # must do this backwards because replacing may screw up indexes AFTER
         # the replaced place
         for start, end in reversed(match_ranges):
-            self._textwidget.replace(start, end, self.replace_entry.get())
-        self._textwidget.tag_remove('find_highlight', '1.0', 'end')
+            self._textwidget.replace(start, end, self.replace_entry.text)
+        self._highlight_tag.remove()
         self._update_buttons()
 
         if len(match_ranges) == 1:
-            self.statuslabel['text'] = "Replaced 1 match."
+            self.statuslabel.config['text'] = "Replaced 1 match."
         else:
-            self.statuslabel['text'] = ("Replaced %d matches." %
-                                        len(match_ranges))
+            self.statuslabel.config['text'] = ("Replaced %d matches." %
+                                               len(match_ranges))
 
 
 def find():
-    tab = get_tab_manager().select()
+    tab = get_tab_manager().selected_tab
     assert isinstance(tab, tabs.FileTab)
     if tab not in finders:
         finders[tab] = Finder(tab.bottom_frame, tab.textwidget)
