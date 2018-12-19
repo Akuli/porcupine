0db641219f12a170325dbfb0e97e7e7d58ff992a
Akuli <akuviljanen17@gmail.com>
2018-12-19 23:03:27 +0200

    windowicon plugin pythotk

--- porcupine/plugins/windowicon.py
+++ porcupine/plugins/windowicon.py
@@ -1,7 +1,14 @@
+import pythotk as tk
+
 from porcupine import get_main_window, images
 
 
 def setup():
     window = get_main_window()
-    window.title("Porcupine")    # not related to the icon, but it's ok imo
-    window.tk.call('wm', 'iconphoto', window, images.get('logo-200x200'))
+    window.title = "Porcupine"    # not related to the icon, but it's ok imo
+
+    # TODO: add 'wm iconphoto' to pythotk
+    # fun fact: tkinter has it, but it's broken :D so this file contained a
+    # direct tcl call before i ported this to pythotk
+    tk.tcl_call(None, 'wm', 'iconphoto', window.toplevel,
+                images.get('logo-200x200'))
