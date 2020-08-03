4d2feecbab1dd146f1e32d5965294183382a353d
Akuli <akuviljanen17@gmail.com>
2020-08-03 13:59:31 +0300

    fix #43

--- docs/extensions.py
+++ docs/extensions.py
@@ -1,8 +1,10 @@
+from typing import Tuple
+
 import docutils.nodes
 import docutils.utils
 
 from sphinx.addnodes import desc_annotation, desc_name
-from sphinx.domains.python import PyClassmember, PyXRefRole
+from sphinx.domains.python import PyObject, PyXRefRole
 from sphinx.util.nodes import split_explicit_title
 
 SOURCE_URI = 'https://github.com/Akuli/porcupine/tree/master/'
@@ -18,30 +20,22 @@ def source_role(typ, rawtext, text, lineno, inliner, options={}, content=[]):
     return [refnode], []
 
 
-# this is based on the source code of sphinx.directives.ObjectDescription
-# and PyClassmember
-class TkVirtualEvent(PyClassmember):
+# this is based on the source code of sphinx.domains.python.PyObject, with some
+# trial and error
+#
+# sphinx is a badly documented documentation tool, i hate it
+class TkVirtualEvent(PyObject):
 
-    def get_signatures(self):
-        # e.g. ['Thing'], the return value is a list of sig arguments
-        # that are passed to handle_signature() one by one
-        return self.arguments
-
-    def handle_signature(self, sig, signode):
-        assert (not sig.startswith('<<')) and (not sig.endswith('>>'))
-        modname = self.options.get(
-            'module', self.env.ref_context.get('py:module'))
-        classname = self.env.ref_context.get('py:class')
-        fullname = classname + '.' + sig
-
-        signode['module'] = modname
-        signode['class'] = classname
-        signode['fullname'] = fullname
-
+    def handle_signature(self, name, signode):
         signode += desc_annotation('virtual event ', 'virtual event ')
-        signode += desc_name('<<' + sig + '>>', '<<' + sig + '>>')
+        signode += desc_name(f'<<{name}>>', f'<<{name}>>')
 
-        return fullname, ''
+        class_name = self.env.ref_context.get('py:class')
+        return (class_name + '.' + name, '')
+
+    def get_index_text(self, modname: str, name_cls: Tuple[str, str]) -> str:
+        class_name, event_name = name_cls[0].rsplit('.', 1)
+        return f'<<{event_name}>> ({modname}.{class_name} virtual event)'
 
 
 class TkVirtualEventXRefRole(PyXRefRole):
