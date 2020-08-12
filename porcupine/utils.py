Akuli/porcupine

commit 3672cc5eb996da78926779d5b6e8b14df1a4419e

bindcheck: don't complain about one-argument binds

porcupine/utils.py
@@ -422,16 +422,16 @@ class TemporaryBind:
         self._widget = widget
         self._sequence = sequence
 
-        not_bound_commands = widget.bind(sequence)  # bindcheck: ignore
+        not_bound_commands = widget.bind(sequence)
         self._tcl_command = bind_with_data(widget, sequence, func, add=True)
-        bound_commands = widget.bind(sequence)  # bindcheck: ignore
+        bound_commands = widget.bind(sequence)
         assert bound_commands.startswith(not_bound_commands)
         self._new_things = bound_commands[len(not_bound_commands):]
 
     def unbind(self) -> None:
         # other stuff might be bound too while this thing was yielding
         try:
-            bound_and_stuff = self._widget.bind(self._sequence)  # bindcheck: ignore
+            bound_and_stuff = self._widget.bind(self._sequence)
         except tkinter.TclError as e:
             if self._widget.winfo_exists():
                 raise e
@@ -440,7 +440,7 @@ class TemporaryBind:
                 return
 
         assert bound_and_stuff.count(self._new_things) == 1
-        self._widget.bind(self._sequence, bound_and_stuff.replace(self._new_things, ''))  # bindcheck: ignore
+        self._widget.bind(self._sequence, bound_and_stuff.replace(self._new_things, ''), add=False)
 
         # tkinter's unbind() does this too to avoid memory leaks
         self._widget.deletecommand(self._tcl_command)

bindcheck.py
@@ -20,6 +20,7 @@ bad_binds = [
     if isinstance(node, ast.Call)
     and isinstance(node.func, ast.Attribute)
     and node.func.attr in {'bind', 'bind_all', 'bind_class', 'bind_with_data'}
+    and len(node.args) >= 2   # widget.bind(one_argument) doesn't need add=True
     and not any(kw.arg == 'add' for kw in node.keywords)
     and '# bindcheck: ignore' not in code.splitlines()[node.lineno - 1]
 ]

porcupine/plugins/autocomplete.py
@@ -311,7 +311,7 @@ class AutoCompleter:
         # completion request or filtering, user might type more
         self._orig_cursorpos = self._tab.textwidget.index('insert')
 
-        if self._tab.bind('<<AutoCompletionRequest>>'):   # bindcheck: ignore
+        if self._tab.bind('<<AutoCompletionRequest>>'):
             # an event handler is bound, use that
             self._tab.event_generate('<<AutoCompletionRequest>>', data=Request(
                 id=the_id,

commit 7ad412cbcde2392582ec6d021d99d1bff222840e

mypy fix (broken in 3672cc5)

porcupine/utils.py
@@ -440,7 +440,7 @@ class TemporaryBind:
                 return
 
         assert bound_and_stuff.count(self._new_things) == 1
-        self._widget.bind(self._sequence, bound_and_stuff.replace(self._new_things, ''), add=False)
+        self._widget.bind(self._sequence, bound_and_stuff.replace(self._new_things, ''))  # bindcheck: ignore
 
         # tkinter's unbind() does this too to avoid memory leaks
         self._widget.deletecommand(self._tcl_command)