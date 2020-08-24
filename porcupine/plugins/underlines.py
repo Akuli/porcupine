bdaccbffd0bb5d1e80e4a7502d4ec214fda59ebb
Akuli <akuviljanen17@gmail.com>
2020-08-24 09:13:57 +0300

    use a Label inside the textwidget for underliner messages (fixes #131)

--- porcupine/plugins/underlines.py
+++ porcupine/plugins/underlines.py
@@ -37,22 +37,22 @@ class _Underliner:
     def __init__(self, textwidget: tkinter.Text) -> None:
         self.textwidget = textwidget
         self.textwidget.bind('<<CursorMoved>>', self._on_cursor_moved, add=True)
-        self.textwidget.bind('<<UnderlinerHidePopup>>', self._hide_popup, add=True)
+        self.textwidget.bind('<<UnderlinerHidePopup>>', self._hide_message_label, add=True)
         self.textwidget.tag_bind('underline_common', '<Enter>', self._on_mouse_enter)
-        self.textwidget.tag_bind('underline_common', '<Leave>', self._hide_popup)
-        utils.add_scroll_command(textwidget, 'yscrollcommand', self._hide_popup)
+        self.textwidget.tag_bind('underline_common', '<Leave>', self._hide_message_label)
+        utils.add_scroll_command(textwidget, 'yscrollcommand', self._hide_message_label)
 
-        self._popup: Optional[tkinter.Toplevel] = None
-        self._popup_tag: Optional[str] = None
+        self._message_label: Optional[tkinter.Toplevel] = None
+        self._message_tag: Optional[str] = None
         self._tag2underline: Dict[str, Underline] = {}
 
     def _show_tag_at_index(self, index: str) -> None:
         # Reversing to prefer topmost tags (i.e. tags added last)
         for tag in reversed(self.textwidget.tag_names(index)):
             if tag in self._tag2underline:
-                self._show_popup(tag)
+                self._show_message_label(tag)
                 return
-        self._hide_popup()
+        self._hide_message_label()
 
     def _on_mouse_enter(self, junk: object) -> None:
         self._show_tag_at_index('current')
@@ -99,13 +99,13 @@ class _Underliner:
         if any(tag.startswith('underline:') for tag in self.textwidget.tag_names('insert')):
             self._show_tag_at_index('insert')
         else:
-            self._hide_popup()
+            self._hide_message_label()
 
-    def _show_popup(self, tag: str) -> None:
-        if self._popup_tag == tag:
+    def _show_message_label(self, tag: str) -> None:
+        if self._message_tag == tag:
             return
 
-        self._hide_popup()
+        self._hide_message_label()
 
         if _tag_spans_multiple_lines(self.textwidget, tag):
             bbox = self.textwidget.bbox(f'{tag}.last linestart')
@@ -116,27 +116,37 @@ class _Underliner:
             # this is called even though the relevant part of text isn't visible? weird
             return
 
-        x, y, width, height = bbox
-        x += self.textwidget.winfo_rootx()
-        y += self.textwidget.winfo_rooty()
+        bbox_x, bbox_y, bbox_width, bbox_height = bbox
 
-        self._popup_tag = tag
-        self._popup = tkinter.Toplevel()
-        tkinter.Label(
-            self._popup,
+        self._message_tag = tag
+        self._message_label = tkinter.Label(
+            self.textwidget,
             text=self._tag2underline[tag].message,
             # opposite colors as in the text widget
             bg=self.textwidget.cget('fg'),
             fg=self.textwidget.cget('bg'),
-        ).pack()
-        self._popup.geometry(f'+{x}+{y + height + 5}')
-        self._popup.overrideredirect(True)
+        )
 
-    def _hide_popup(self, junk: object = None) -> None:
-        if self._popup is not None:
-            self._popup.destroy()
-            self._popup = None
-            self._popup_tag = None
+        label_width = self._message_label.winfo_reqwidth()
+        label_height = self._message_label.winfo_reqheight()
+        gap_size = 8
+
+        # don't go beyond the right edge of textwidget
+        label_x = min(bbox_x, self.textwidget.winfo_width() - gap_size - label_width)
+
+        if bbox_y + bbox_height + gap_size + label_height < self.textwidget.winfo_height():
+            # label goes below bbox
+            label_y = bbox_y + bbox_height + gap_size
+        else:
+            # would go below bottom of text widget, let's put it above instead
+            label_y = bbox_y - gap_size - label_height
+        self._message_label.place(x=label_x, y=label_y)
+
+    def _hide_message_label(self, junk: object = None) -> None:
+        if self._message_label is not None:
+            self._message_label.destroy()
+            self._message_label = None
+            self._message_tag = None
 
 
 def on_new_tab(event: utils.EventWithData) -> None:
@@ -146,7 +156,7 @@ def on_new_tab(event: utils.EventWithData) -> None:
         utils.bind_with_data(tab, '<<SetUnderlines>>', underliner.set_underlines, add=True)
 
 
-def hide_all_popups(event: 'tkinter.Event[tkinter.Misc]') -> None:
+def hide_all_message_labels(event: 'tkinter.Event[tkinter.Misc]') -> None:
     if event.widget is get_main_window():   # Tk and Toplevel events need this check
         for tab in get_tab_manager().tabs():
             if isinstance(tab, tabs.FileTab):
@@ -158,6 +168,6 @@ def setup() -> None:
     get_main_window().event_add('<<UnderlinerHidePopup>>', '<Unmap>')
 
     # and when the entire porcupine window loses input focus (binding here to avoid unbinding)
-    get_main_window().bind('<FocusOut>', hide_all_popups, add=True)
+    get_main_window().bind('<FocusOut>', hide_all_message_labels, add=True)
 
     utils.bind_with_data(get_tab_manager(), '<<NewTab>>', on_new_tab, add=True)
