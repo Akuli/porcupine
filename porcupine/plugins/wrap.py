c4c83740dbff7648e51ca1fe4ed51d55d73d9eb9
Akuli <akuviljanen17@gmail.com>
2020-12-27 12:49:48 +0200

    create word wrap plugin (fixes #155)

---
+++ porcupine/plugins/wrap.py
@@ -0,0 +1,26 @@
+"""Word Wrap option in View menu."""
+from functools import partial
+import tkinter
+
+from porcupine import get_tab_manager, menubar, tabs
+
+
+def on_menu_toggled(wrap_var: tkinter.BooleanVar, *junk: object) -> None:
+    tab = get_tab_manager().select()
+    assert tab is not None
+    tab.textwidget.config(wrap=('word' if wrap_var.get() else 'none'))
+
+
+# return value = whether to enable menu item that changes wrap_var
+def on_tab_changed(wrap_var: tkinter.BooleanVar, tab: tabs.Tab) -> bool:
+    if isinstance(tab, tabs.FileTab):
+        wrap_var.set(tab.textwidget.cget('wrap') == 'word')
+        return True
+    return False
+
+
+def setup():
+    wrap_var = tkinter.BooleanVar()
+    wrap_var.trace_add('write', partial(on_menu_toggled, wrap_var))
+    menubar.get_menu("View").add_checkbutton(label="Wrap Long Lines", variable=wrap_var)
+    menubar.set_enabled_based_on_tab("View/Wrap Long Lines", partial(on_tab_changed, wrap_var))
