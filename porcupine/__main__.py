7a1d57d6b3950e00d7dad5c3be4be8f9f7dbb819
Akuli <akuviljanen17@gmail.com>
2020-12-28 12:45:33 +0200

    Revert "tiny cleanup"
    This reverts commit 2dc5365a35239cb19f01d6b1cc5bdec4ec68f8b8.

--- porcupine/__main__.py
+++ porcupine/__main__.py
@@ -85,7 +85,8 @@ def main() -> None:
 
         bad_disables = set(disable_list) - {info.name for info in pluginloader.plugin_infos}
         if bad_disables:
-            parser.error(f"--without-plugins: no plugin named {bad_disables[0]!r}")
+            one_of_them, *the_rest = bad_disables
+            parser.error(f"--without-plugins: no plugin named {one_of_them!r}")
 
     parser.add_argument('--help', action='help', help="show this message")
     pluginloader.run_setup_argument_parser_functions(parser)
