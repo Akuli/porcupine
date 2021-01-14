1b77a423031f9117e05b94408b6162e90f0ae9da
Akuli <akuviljanen17@gmail.com>
2021-01-14 12:37:30 +0200

    tiny cleanup

--- tests/test_indent_dedent.py
+++ tests/test_indent_dedent.py
@@ -1,5 +1,3 @@
-import platform
-
 _FUNNY = '''\
 def foo(
     x,
--- tests/test_reload_plugin.py
+++ tests/test_reload_plugin.py
@@ -5,7 +5,6 @@ def test_reload(tabmanager, tmp_path):
     (tmp_path / 'foo.py').write_text("hello")
     tab = tabs.FileTab.open_file(tabmanager, tmp_path / 'foo.py')
     tabmanager.add_tab(tab, select=True)
-    assert tab.path is not None
     assert tab.textwidget.get('1.0', 'end - 1 char') == 'hello'
 
     (tmp_path / 'foo.py').write_text('lol')
