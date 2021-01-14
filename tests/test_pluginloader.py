2c132bed8f87fbefa771944c10c5bb8b2490a106
Akuli <akuviljanen17@gmail.com>
2021-01-14 11:57:45 +0200

    add pluginmanager tests

--- tests/test_pluginloader.py
+++ tests/test_pluginloader.py
@@ -1,4 +1,7 @@
-from porcupine import pluginloader
+import tkinter
+from tkinter import ttk
+
+from porcupine import get_main_window, pluginloader
 
 
 def test_all_plugins_loaded_successfully(porcusession):
@@ -6,3 +9,26 @@ def test_all_plugins_loaded_successfully(porcusession):
     assert pluginloader.plugin_infos, "if this fails, it means no plugins got loaded"
     for info in pluginloader.plugin_infos:
         assert info.status == pluginloader.Status.ACTIVE
+
+
+def get_children(widget):
+    for child in widget.winfo_children():
+        yield child
+        yield from get_children(child)
+
+
+def test_pluginmanager_plugin(porcusession, mocker):
+    mocker.patch('tkinter.Toplevel.wait_window', autospec=True)
+    get_main_window().event_generate('<<Menubar:Settings/Plugin Manager>>')
+    tkinter.Toplevel.wait_window.assert_called_once()
+
+    [dialog] = tkinter.Toplevel.wait_window.call_args[0]
+    try:
+        [treeview] = [widget for widget in get_children(dialog) if isinstance(widget, ttk.Treeview)]
+
+        # Selecting the plugins runs code that finds description of plugin (as docstring)
+        for info in pluginloader.plugin_infos:
+            treeview.selection_set(info.name)
+            treeview.update()
+    finally:
+        dialog.destroy()
