5c26c4ee243846160d9315879e5e361e3e68c548
Akuli <akuviljanen17@gmail.com>
2021-01-20 12:00:45 +0200

    use wm_iconphoto method

--- porcupine/plugins/windowicon.py
+++ porcupine/plugins/windowicon.py
@@ -5,4 +5,4 @@ from porcupine import get_main_window, images
 def setup() -> None:
     window = get_main_window()
     window.title("Porcupine")    # not related to the icon, but it's ok imo
-    window.tk.call('wm', 'iconphoto', window, images.get('logo-200x200'))
+    window.wm_iconphoto(False, images.get('logo-200x200'))
