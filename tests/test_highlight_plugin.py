26e0a7e8fe3638b30793a6512a441ec35ec325dc
Akuli <akuviljanen17@gmail.com>
2021-01-30 00:03:05 +0200

    fix test

--- tests/test_highlight_plugin.py
+++ tests/test_highlight_plugin.py
@@ -12,11 +12,13 @@ def test_deleting_bug(filetab):
     assert tag_ranges('Token.Literal.String.Double') == []
 
     filetab.textwidget.insert('1.0', '"')
+    filetab.update()
     assert tag_ranges('Token.Keyword') == []
     assert tag_ranges('Token.Keyword.Constant') == []
     assert tag_ranges('Token.Literal.String.Double') == ['1.0', '1.12']
 
     filetab.textwidget.delete('1.0')
+    filetab.update()
     assert tag_ranges('Token.Keyword') == ['1.0', '1.6']
     assert tag_ranges('Token.Keyword.Constant') == ['1.7', '1.11']
     assert tag_ranges('Token.Literal.String.Double') == []
