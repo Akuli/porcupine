d8f14c4cb18d41c40cf3d09409086f0b4e640267
Akuli <akuviljanen17@gmail.com>
2021-01-29 21:19:33 +0200

    build windows setup exes in github actions (#208)
    * create windows-build.yml
    
    * fix branch name
    
    * disable main workflow for now
    
    * simplify build script
    
    * tag experiment
    
    * go back to always running
    
    * run the script
    
    * copy 'create' from myst's config (dont know how its better)
    
    * require pillow
    
    * install nsis and pynsist
    
    * require pillow, again
    
    * comment out autopep8 for now
    
    * delete old comment
    
    * use plain 'python' in ci
    
    * debug print
    
    * try to print so that prints are visible in github actions
    
    * figure out file name with debug print
    
    * recursive debug print
    
    * try without --python
    
    * shit
    
    * try to actually make stuff happen
    
    * delete unused --python option
    
    * fixes
    
    * fix
    
    * truy get-version-action
    
    * switch to real tag trigger
    
    * Revert "disable main workflow for now"
    
    This reverts commit 877859061156597c1d2d90c5b1adeb7461c38165.
    
    * move stuff to scripts dir
    
    * update readme
    
    * lint the scripts
    
    * Revert "switch to real tag trigger"
    
    This reverts commit cf5104e07534e936e2791559d79671e67d13d3b3.
    
    * fix
    
    * requirements
    
    * requirements black fixxx
    
    * switch to real tag trigger

--- scripts/build-exe-installer.py
+++ scripts/build-exe-installer.py
@@ -1,17 +1,14 @@
-# FIXME: this contains copy/pasta from setup.py
 import configparser
 import functools
 import os
 import pathlib
 import platform
-import re
 import shutil
 import struct
 import subprocess
 import sys
 
 import PIL.Image
-
 
 assert platform.system() == 'Windows', "this script must be ran on windows"
 
@@ -23,35 +20,10 @@ assert not (struct.calcsize('P') == 4 and '64' in platform.machine()), (
     "this script can't be ran with 32-bit Python on a 64-bit Windows, "
     "install a 64-bit Python instead")
 
-
-# setup.py copy pasta
-def get_requirements():
-    with open('requirements.txt', 'r') as file:
-        for line in map(str.strip, file):
-            if (not line.startswith('#')) and line:
-                yield line
-
-
-# setup.py copy pasta
-def find_metadata():
-    with open(os.path.join('porcupine', '__init__.py')) as file:
-        content = file.read()
-
-    result = dict(re.findall(
-        r'''^__(author|copyright|license)__ = ['"](.*)['"]$''',
-        content, re.MULTILINE))
-    assert result.keys() == {'author', 'copyright', 'license'}, result
-
-    # version is defined like this: __version__ = '%d.%d.%d' % version_info
-    version_info = re.search(r'^version_info = \((\d+), (\d+), (\d+)\)',
-                             content, re.MULTILINE).groups()
-    result['version'] = '%s.%s.%s' % version_info
-
-    return result
-
+[this_script_name, porcupine_version] = sys.argv
 
 # info("asd") prints "build-exe-installer.py: asd"
-info = functools.partial(print, sys.argv[0] + ':')
+info = functools.partial(print, this_script_name + ':', file=sys.stderr, flush=True)
 
 
 def get_frozen_requirements_in_a_crazy_way():
@@ -59,59 +31,38 @@ def get_frozen_requirements_in_a_crazy_way():
          "in order to get output from 'pip freeze' to figure out which "
          "dependencies to bundle...")
     subprocess.check_call([sys.executable, '-m', 'venv', 'temp_env'])
-
     try:
-        subprocess.check_call([
-            r'temp_env\Scripts\python.exe', '-m',
-            'pip', 'install', '-r', 'requirements.txt'])
-        frozen = subprocess.check_output([
-            r'temp_env\Scripts\python.exe', '-m', 'pip', 'freeze'
-        ]).decode('utf-8').strip().splitlines()
+        subprocess.check_call([r'temp_env\Scripts\python.exe', '-m', 'pip', 'install', '-r', 'requirements.txt'])
+        frozen = subprocess.check_output([r'temp_env\Scripts\python.exe', '-m', 'pip', 'freeze'])
     finally:
         shutil.rmtree('temp_env')
-
-    return [requirement for requirement in frozen
+    return [requirement for requirement in frozen.decode('utf-8').strip().splitlines()
             if not requirement.lower().startswith('porcupine==')]
-
-
-def mkdir_empty(path):
-    try:
-        os.mkdir(path)
-    except FileExistsError:
-        shutil.rmtree(path)
-        os.mkdir(path)
 
 
 # https://pynsist.readthedocs.io/en/latest/faq.html#packaging-with-tkinter
 def copy_tkinter_files():
     info("Copying tkinter files...")
-    shutil.copytree(os.path.join(sys.prefix, 'tcl'), 'lib')
-
-    # basic pathlib... need to convert between Paths and strings a lot
-    # i'm using pathlib because sys.prefix might contain a * and it would screw
-    # up globbing, unless i use glob.escape
-    mkdir_empty('pynsist_pkgs')
-    dlldir = pathlib.Path(sys.prefix) / 'DLLs'
-    for file in list(dlldir.glob('tk*.dll')) + list(dlldir.glob('tcl*.dll')):
-        file = str(file)
+    prefix = pathlib.Path(sys.prefix)
+    shutil.copytree(prefix / 'tcl', 'lib')
+    os.mkdir('pynsist_pkgs')
+    for file in list((prefix / 'DLLs').glob('tk*.dll')) + list((prefix / 'DLLs').glob('tcl*.dll')):
         shutil.copy(file, 'pynsist_pkgs')
-
-    shutil.copy(str(dlldir / '_tkinter.pyd'), 'pynsist_pkgs')
-    shutil.copy(os.path.join(sys.prefix, 'libs', '_tkinter.lib'),
-                'pynsist_pkgs')
+    shutil.copy(prefix / 'DLLs' / '_tkinter.pyd', 'pynsist_pkgs')
+    shutil.copy(prefix / 'libs' / '_tkinter.lib', 'pynsist_pkgs')
 
 
 def create_ico_file():
-    info(r"Converting porcupine\images\logo-200x200.gif to .ico format...")
-    logo = PIL.Image.open(r'porcupine\images\logo-200x200.gif')
-    logo.save('porcupine-logo.ico')
+    info(r"Converting logo to .ico format...")
+    PIL.Image.open(r'porcupine\images\logo-200x200.gif').save('porcupine-logo.ico')
 
 
 def create_pynsist_cfg():
+    info("Creating pynsist.cfg...")
     parser = configparser.ConfigParser()
     parser['Application'] = {
         'name': 'Porcupine',
-        'version': find_metadata()['version'],
+        'version': porcupine_version,
         'entry_point': 'porcupine.__main__:main',    # setup.py copy pasta
         'icon': 'porcupine-logo.ico',
         'license_file': 'LICENSE',
@@ -124,14 +75,11 @@ def create_pynsist_cfg():
         'packages': 'tkinter\n_tkinter',
         'files': 'porcupine/images\nlib',
     }
-
-    info("Creating pynsist.cfg...")
     with open('pynsist.cfg', 'w') as file:
         parser.write(file)
 
 
 def run_pynsist():
-    info("Running pynsist...")
     subprocess.check_call([sys.executable, '-m', 'nsist', 'pynsist.cfg'])
 
 
@@ -147,6 +95,7 @@ def main():
     create_ico_file()
     create_pynsist_cfg()
     run_pynsist()
+    info("All done")
 
 
 if __name__ == '__main__':
