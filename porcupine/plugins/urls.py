85e87cfc7dfd11118aa237a03c0cbac43a4bced6
Akuli <akuviljanen17@gmail.com>
2021-01-30 22:13:38 +0200

    speed up urls plugin (fixes #222) (#232)
    * speed up urls plugin (fixes #222)
    
    * fix tests

--- porcupine/plugins/urls.py
+++ porcupine/plugins/urls.py
@@ -9,11 +9,11 @@ from porcupine import get_tab_manager, tabs, utils
 from porcupine.plugins import underlines
 
 
-def find_urls(text: tkinter.Text) -> Iterable[Tuple[str, str]]:
-    match_ends_and_search_begins = '1.0'
+def find_urls(text: tkinter.Text, start: str, end: str) -> Iterable[Tuple[str, str]]:
+    match_ends_and_search_begins = start
     while True:
         match_start = text.search(
-            r'\mhttps?://[a-z]', match_ends_and_search_begins, 'end',
+            r'\mhttps?://[a-z]', match_ends_and_search_begins, end,
             nocase=True, regexp=True)
         if not match_start:     # empty string means not found
             break
@@ -44,11 +44,14 @@ def find_urls(text: tkinter.Text) -> Iterable[Tuple[str, str]]:
 
 
 def update_url_underlines(tab: tabs.FileTab, junk: object = None) -> None:
+    view_start = tab.textwidget.index('@0,0')
+    view_end = tab.textwidget.index('@0,10000')
+
     tab.event_generate('<<SetUnderlines>>', data=underlines.Underlines(
         id='urls',
         underline_list=[
             underlines.Underline(start, end, "ctrl+click or ctrl+enter to open")
-            for start, end in find_urls(tab.textwidget)
+            for start, end in find_urls(tab.textwidget, view_start, view_end)
         ],
     ))
 
@@ -66,6 +69,7 @@ def open_the_url(tab: tabs.FileTab, index: str, junk: object) -> utils.BreakOrNo
 def on_new_tab(tab: tabs.Tab) -> None:
     if isinstance(tab, tabs.FileTab):
         tab.textwidget.bind('<<ContentChanged>>', partial(update_url_underlines, tab), add=True)
+        utils.add_scroll_command(tab.textwidget, 'yscrollcommand', partial(update_url_underlines, tab))
         update_url_underlines(tab)
 
         tab.textwidget.tag_bind('underline:urls', '<Control-Button-1>', partial(open_the_url, tab, 'current'), add=True)
