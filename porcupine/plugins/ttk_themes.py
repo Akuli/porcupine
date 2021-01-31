b082af9397c99b7fc092014b1ce3856e099607be
Akuli <akuviljanen17@gmail.com>
2021-01-31 16:23:20 +0200

    Ttk themes plugin improvements (#255)
    * Rewrite ttk_themes plugin logic (fixes #253)
    
    * improve ttk_themes plugin docstring

--- porcupine/plugins/ttk_themes.py
+++ porcupine/plugins/ttk_themes.py
@@ -1,29 +1,30 @@
-"""More Ttk themes: https://github.com/RedFantom/ttkthemes"""
+"""Change the look of Porcupine's GUI.
+
+This plugin doesn't do anything to the colors used in the main editing area.
+Those are handled by pygments_style and highlight plugins.
+"""
 # TODO: capitalize theme names in menu items?
-import functools
 import tkinter
 
 import ttkthemes  # type: ignore[import]
 
-from porcupine import menubar, settings
-
-
-def on_theme_changed(style: ttkthemes.ThemedStyle, var: tkinter.StringVar, *junk: object) -> None:
-    theme_name = var.get()
-    style.set_theme(theme_name)
-    settings.set('ttk_theme', theme_name)
+from porcupine import get_main_window, menubar, settings
 
 
 def setup() -> None:
     style = ttkthemes.ThemedStyle()
-
-    # https://github.com/RedFantom/ttkthemes/issues/6
-    # this does what style.theme_use() should do
-    default_theme = style.tk.eval('return $ttk::currentTheme')
-    settings.add_option('ttk_theme', default_theme)
+    settings.add_option('ttk_theme', style.theme_use())
 
     var = tkinter.StringVar()
-    var.trace_add('write', functools.partial(on_theme_changed, style, var))
-    var.set(settings.get('ttk_theme', str))
     for name in sorted(style.get_themes()):
         menubar.get_menu("Ttk Themes").add_radiobutton(label=name, value=name, variable=var)
+
+    # Connect style and var
+    var.trace_add('write', lambda *junk: style.set_theme(var.get()))
+
+    # Connect var and settings
+    get_main_window().bind('<<SettingChanged:ttk_theme>>', (
+        lambda event: var.set(settings.get('ttk_theme', str))
+    ), add=True)
+    var.set(settings.get('ttk_theme', str))
+    var.trace_add('write', lambda *junk: settings.set('ttk_theme', var.get()))
