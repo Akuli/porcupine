27f659947ec7dafbb4d2dc94ec3f8f58dd9e0921
Akuli <akuviljanen17@gmail.com>
2021-01-31 15:46:44 +0200

    Rewrite ttk_themes plugin logic (fixes #253)

--- porcupine/plugins/ttk_themes.py
+++ porcupine/plugins/ttk_themes.py
@@ -1,29 +1,26 @@
 """More Ttk themes: https://github.com/RedFantom/ttkthemes"""
 # TODO: capitalize theme names in menu items?
-import functools
 import tkinter
 
 import ttkthemes  # type: ignore[import]
 
-from porcupine import menubar, settings
-
-
-def on_theme_changed(style: ttkthemes.ThemedStyle, var: tkinter.StringVar, *junk: object) -> None:
-    theme_name = var.get()
-    style.set_theme(theme_name)
-    settings.set('ttk_theme', theme_name)
+from porcupine import menubar, settings, get_main_window
 
 
 def setup() -> None:
     style = ttkthemes.ThemedStyle()
-
-    # https://github.com/RedFantom/ttkthemes/issues/6
-    # this does what style.theme_use() should do
-    default_theme = style.tk.eval('return $ttk::currentTheme')
-    settings.add_option('ttk_theme', default_theme)
+    settings.add_option('ttk_theme', style.theme_use())
 
     var = tkinter.StringVar()
-    var.trace_add('write', functools.partial(on_theme_changed, style, var))
-    var.set(settings.get('ttk_theme', str))
     for name in sorted(style.get_themes()):
         menubar.get_menu("Ttk Themes").add_radiobutton(label=name, value=name, variable=var)
+
+    # Connect style and var
+    var.trace_add('write', lambda *junk: style.set_theme(var.get()))
+
+    # Connect var and settings
+    get_main_window().bind('<<SettingChanged:ttk_theme>>', (
+        lambda event: var.set(settings.get('ttk_theme', str))
+    ), add=True)
+    var.set(settings.get('ttk_theme', str))
+    var.trace_add('write', lambda *junk: settings.set('ttk_theme', var.get()))
