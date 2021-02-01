8e14d3a41357b9955320a8eb95db1593c131e2fd
Akuli <akuviljanen17@gmail.com>
2021-02-01 19:16:47 +0200

    don't reload plugins while porcupine is running (fixes #262)

--- tests/test_pluginloader.py
+++ tests/test_pluginloader.py
@@ -6,3 +6,9 @@ def test_all_plugins_loaded_successfully():
     assert pluginloader.plugin_infos, "if this fails, it means no plugins got loaded"
     for info in pluginloader.plugin_infos:
         assert info.status == pluginloader.Status.ACTIVE
+
+
+# filetypes plugin adds custom command line arguments
+def test_filetypes_plugin_cant_be_loaded_while_running():
+    [info] = [info for info in pluginloader.plugin_infos if info.name == 'filetypes']
+    assert not pluginloader.can_setup_while_running(info)
