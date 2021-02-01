44736c3844633a596d7a4a4b88c5528275c08eb3
Akuli <akuviljanen17@gmail.com>
2021-02-01 17:28:06 +0200

    allow selecting multiple plugins in plugin manager

--- tests/test_pluginloader.py
+++ tests/test_pluginloader.py
@@ -1,7 +1,4 @@
-import tkinter
-from tkinter import ttk
-
-from porcupine import get_main_window, pluginloader, utils
+from porcupine import pluginloader
 
 
 def test_all_plugins_loaded_successfully():
@@ -9,21 +6,3 @@ def test_all_plugins_loaded_successfully():
     assert pluginloader.plugin_infos, "if this fails, it means no plugins got loaded"
     for info in pluginloader.plugin_infos:
         assert info.status == pluginloader.Status.ACTIVE
-
-
-def test_pluginmanager_plugin(mocker):
-    mocker.patch('tkinter.Toplevel.wait_window', autospec=True)
-    get_main_window().event_generate('<<Menubar:Settings/Plugin Manager>>')
-    tkinter.Toplevel.wait_window.assert_called_once()
-
-    [dialog] = tkinter.Toplevel.wait_window.call_args[0]
-    try:
-        [treeview] = [widget for widget in utils.get_children_recursively(dialog)
-                      if isinstance(widget, ttk.Treeview)]
-
-        # Selecting the plugins runs code that finds description of plugin (as docstring)
-        for info in pluginloader.plugin_infos:
-            treeview.selection_set(info.name)
-            treeview.update()
-    finally:
-        dialog.destroy()
