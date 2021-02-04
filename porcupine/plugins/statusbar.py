77f1c25ff9019c305717f6f05858e59dc24d0e71
Akuli <akuviljanen17@gmail.com>
2021-02-04 14:18:01 +0200

    simplify statusbar plugin a lot

--- porcupine/plugins/statusbar.py
+++ porcupine/plugins/statusbar.py
@@ -1,63 +1,36 @@
 """Display a status bar in each file tab."""
-import tkinter
 from tkinter import ttk
-from functools import partial
 
 from porcupine import get_tab_manager, tabs
 
 
-# this widget is kind of weird
-class LabelWithEmptySpaceAtLeft(ttk.Label):
-
-    def __init__(self, master: tkinter.BaseWidget) -> None:
-        self._spacer = ttk.Frame(master)
-        self._spacer.pack(side='left', expand=True)
-        super().__init__(master)
-        self.pack(side='left')
-
-    def destroy(self) -> None:
-        self._spacer.destroy()
-        super().destroy()
-
-
 class StatusBar(ttk.Frame):
 
-    def __init__(self, master: tkinter.BaseWidget):
-        super().__init__(master)
-        # one label for each tab-separated thing
-        self.labels = [ttk.Label(self)]
-        self.labels[0].pack(side='left')
+    def __init__(self, tab: tabs.FileTab):
+        super().__init__(tab.bottom_frame)
+        self.tab = tab
+        self.left_label = ttk.Label(self)
+        self.right_label = ttk.Label(self)
+        self.left_label.pack(side='left')
+        self.right_label.pack(side='right')
 
-    def set_status(self, status: str) -> None:
-        parts = status.split('\t')
+    def on_path_changed(self, junk: object = None) -> None:
+        self.left_label.config(text=("New file" if self.tab.path is None else str(self.tab.path)))
 
-        # there's always at least one part, the label added in
-        # __init__ is not destroyed here
-        while len(self.labels) > len(parts):
-            self.labels.pop().destroy()
-        while len(self.labels) < len(parts):
-            self.labels.append(LabelWithEmptySpaceAtLeft(self))
-
-        for label, text in zip(self.labels, parts):
-            label.config(text=text)
-
-
-def update_status(tab: tabs.FileTab, statusbar: StatusBar, junk: object = None) -> None:
-    if tab.path is None:
-        path_string = "New file"
-    else:
-        path_string = str(tab.path)
-    line, column = tab.textwidget.index('insert').split('.')
-    statusbar.set_status(f"{path_string}\tLine {line}, column {column}")
+    def on_cursor_moved(self, junk: object = None) -> None:
+        line, column = self.tab.textwidget.index('insert').split('.')
+        self.right_label.config(text=f"Line {line}, column {column}")
 
 
 def on_new_tab(tab: tabs.Tab) -> None:
     if isinstance(tab, tabs.FileTab):
-        statusbar = StatusBar(tab.bottom_frame)
+        statusbar = StatusBar(tab)
         statusbar.pack(side='bottom', fill='x')
-        tab.bind('<<PathChanged>>', partial(update_status, tab, statusbar), add=True)
-        tab.textwidget.bind('<<CursorMoved>>', partial(update_status, tab, statusbar), add=True)
-        update_status(tab, statusbar)
+        tab.bind('<<PathChanged>>', statusbar.on_path_changed, add=True)
+        tab.textwidget.bind('<<CursorMoved>>', statusbar.on_cursor_moved, add=True)
+
+        statusbar.on_path_changed()
+        statusbar.on_cursor_moved()
 
 
 def setup() -> None:
