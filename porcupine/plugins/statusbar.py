51e0cff7cced2016702e01d7e062fb5e47c3d170
Akuli <akuviljanen17@gmail.com>
2021-02-04 14:21:44 +0200

    add reload warning

--- porcupine/plugins/statusbar.py
+++ porcupine/plugins/statusbar.py
@@ -14,23 +14,36 @@ class StatusBar(ttk.Frame):
         self.left_label.pack(side='left')
         self.right_label.pack(side='right')
 
-    def on_path_changed(self, junk: object = None) -> None:
+    def show_path(self, junk: object = None) -> None:
         self.left_label.config(text=("New file" if self.tab.path is None else str(self.tab.path)))
 
-    def on_cursor_moved(self, junk: object = None) -> None:
+    def show_cursor_pos(self, junk: object = None) -> None:
         line, column = self.tab.textwidget.index('insert').split('.')
         self.right_label.config(text=f"Line {line}, column {column}")
+
+    def show_reload_warning(self, junk: object) -> None:
+        self.left_label.config(
+            foreground='red',
+            text="File was reloaded. Press Ctrl+Z to get your changes back.",
+        )
+
+    def clear_reload_warning(self, junk: object) -> None:
+        if self.left_label['foreground']:
+            self.left_label.config(foreground='')
+            self.show_path()
 
 
 def on_new_tab(tab: tabs.Tab) -> None:
     if isinstance(tab, tabs.FileTab):
         statusbar = StatusBar(tab)
         statusbar.pack(side='bottom', fill='x')
-        tab.bind('<<PathChanged>>', statusbar.on_path_changed, add=True)
-        tab.textwidget.bind('<<CursorMoved>>', statusbar.on_cursor_moved, add=True)
+        tab.bind('<<PathChanged>>', statusbar.show_path, add=True)
+        tab.bind('<<Reloaded>>', statusbar.show_reload_warning)
+        tab.textwidget.bind('<<CursorMoved>>', statusbar.show_cursor_pos, add=True)
+        tab.textwidget.bind('<<ContentChanged>>', statusbar.clear_reload_warning, add=True)
 
-        statusbar.on_path_changed()
-        statusbar.on_cursor_moved()
+        statusbar.show_path()
+        statusbar.show_cursor_pos()
 
 
 def setup() -> None:
