10e1d39beed09aece6e7fceac1b2027a67f2a92d
Akuli <akuviljanen17@gmail.com>
2021-02-06 18:53:12 +0200

    prevent freezing, no matter what

--- porcupine/plugins/directory_tree.py
+++ porcupine/plugins/directory_tree.py
@@ -139,6 +139,7 @@ class DirectoryTree(ttk.Treeview):
                     stdout=subprocess.PIPE,
                     stderr=subprocess.PIPE,
                     encoding=sys.getfilesystemencoding(),
+                    timeout=0.1,
                 )
                 if run_result.returncode != 0:
                     log.info(f"git failed: {run_result}")
