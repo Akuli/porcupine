a6b7f96e7e5b994c1e6cd7da968f9f08e62b52f3
Akuli <akuviljanen17@gmail.com>
2021-02-07 11:45:41 +0200

    get things working again, debugging the slowness

--- porcupine/plugins/directory_tree.py
+++ porcupine/plugins/directory_tree.py
@@ -1,3 +1,4 @@
+import contextlib
 import time
 import logging
 import os
@@ -7,7 +8,7 @@ import sys
 import tkinter
 from functools import partial
 from tkinter import ttk
-from typing import Dict, Iterator, List, Tuple, Any
+from typing import Dict, Iterator, List, Tuple, Any, Optional
 
 from porcupine import get_paned_window, get_tab_manager, settings, tabs, utils
 
@@ -16,6 +17,68 @@ log = logging.getLogger(__name__)
 # If more than this many projects are opened, then the least recently opened
 # project will be closed, unless a file has been opened from that project.
 PROJECT_AUTOCLOSE_COUNT = 3
+
+
+@contextlib.contextmanager
+def timer(what):
+    start = time.time()
+    yield
+    end = time.time()
+    print(f"{what} ran for {round((end-start)*1000)}ms")
+
+
+class Timer:
+
+    def __init__(self):
+        self.times = {}
+
+    @contextlib.contextmanager
+    def add(self, what):
+        start = time.perf_counter()
+        yield
+        self.times.setdefault(what, 0)
+        self.times[what] += time.perf_counter() - start
+
+    def show(self):
+        for what, da_time in sorted(self.times.items()):
+            print(f'{what.ljust(40)}: {round(da_time*1000)}ms')
+
+
+def run_git_status(project_root):
+    try:
+        start = time.time()
+        run_result = subprocess.run(
+            ['git', 'status', '--ignored', '--porcelain'],
+            cwd=project_root,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,   # for logging error message
+            encoding=sys.getfilesystemencoding(),
+        )
+        print(f"git ran for {round((time.time() - start)*1000)}ms")
+
+        if run_result.returncode != 0:
+            log.info(f"git failed: {run_result}")
+            return {}
+
+    except (OSError, UnicodeError):
+        log.warning("can't run git", exc_info=True)
+        return {}
+
+    # Show .git as ignored, even though it actually isn't
+    result = {project_root / '.git': 'git_ignored'}
+    for line in run_result.stdout.splitlines():
+        path = project_root / line[3:]
+        if line[1] == 'M':
+            result[path] = 'git_modified'
+        elif line[1] == ' ':
+            result[path] = 'git_added'
+        elif line[:2] == '??':
+            result[path] = 'git_untracked'
+        elif line[:2] == '!!':
+            result[path] = 'git_ignored'
+        else:
+            log.warning(f"unknown git status line: {repr(line)}")
+    return result
 
 
 # TODO: handle files being deleted, copied, renamed, etc
@@ -28,6 +91,7 @@ class DirectoryTree(ttk.Treeview):
         self.bind('<<TreeviewOpen>>', self.on_click, add=True)
         self.bind('<<ThemeChanged>>', self._config_tags, add=True)
         self._config_tags()
+        self.git_statuses = {}
 
     def _config_tags(self, junk: object = None) -> None:
         fg = self.tk.eval('ttk::style look Treeview -foreground')
@@ -49,11 +113,13 @@ class DirectoryTree(ttk.Treeview):
         self.tag_configure('git_ignored', foreground=gray)
 
     def add_project(self, root_path: pathlib.Path) -> None:
+        print("adding project:", root_path)
         for project_item_id in self.get_children():
             path = self.get_path(project_item_id)
             if path == root_path or path in root_path.parents:
                 # Project or parent project added already. Move it first to
                 # avoid hiding it soon.
+                print("   found already")
                 self.move(project_item_id, '', 0)
                 return
             if root_path in path.parents:
@@ -66,36 +132,14 @@ class DirectoryTree(ttk.Treeview):
         else:
             text = str(root_path)
 
-        project_item_id = self.insert('', 'end', text=text, values=[root_path], tags='dir', open=False)
+        # Add project to beginning so it won't be hidden soon
+        project_item_id = self.insert('', 0, text=text, values=[root_path], tags=['dir', 'project'], open=False)
         self._insert_dummy(project_item_id)
         self.hide_old_projects()
-        self.save_project_list()
-
-    def refresh_directory(self, dir_path: pathlib.Path, dir_id: str) -> None:
-        for child in self.get_children(dir_id):
-            self.delete(child)
-
-        paths = dir_path.iterdir()
-        if paths:
-            for path in paths:
-                tag = 'dir' if path.is_dir() else 'file'
-                n = self.insert(dir_id, 'end', text=path.name, values=[path], tags=tag, open=False)
-                if path.is_dir():
-                    self._insert_dummy(n)
-        else:
-            self._insert_dummy(dir_id)
-
-#        self.update_git_tags()
-
-    def _insert_dummy(self, parent: str) -> None:
-        self.insert(parent, 'end', text='(empty)', tags='dummy')
-
-    def _contains_dummy(self, parent: str) -> None:
-        children = self.get_children(parent)
-        return (len(children) == 1 and 'dummy' in self.item(children[0], 'tags'))
+        self.refresh_everything()
 
     def hide_old_projects(self, junk: object = None) -> None:
-        for project_id in self.get_children(''):
+        for project_id in reversed(self.get_children('')):
             project_path = self.get_path(project_id)
 
             if (
@@ -109,104 +153,131 @@ class DirectoryTree(ttk.Treeview):
                     for tab in get_tab_manager().tabs()
                 )
             ):
+                print("Hiding old project:", project_path)
                 self.delete(project_id)
 
-        self.save_project_list()
-
-    def save_project_list(self):
         # Settings is a weird place for this, but easier than e.g. using a cache file.
         settings.set('directory_tree_projects', [str(self.get_path(id)) for id in self.get_children()])
 
+    def open_and_refresh_directory(
+        self,
+        dir_path: Optional[pathlib.Path],
+        dir_id: str,
+        timer: Timer,
+    ) -> None:
+        if self._contains_dummy(dir_id):
+            self.delete(self.get_children(dir_id)[0])
+
+        path2id = {self.get_path(id): id for id in self.get_children(dir_id)}
+        if dir_path is None:
+            # refreshing an entire project
+            assert not dir_id
+            new_paths = set(path2id.keys())
+        else:
+            new_paths = set(dir_path.iterdir())
+            if not new_paths:
+                self._insert_dummy(dir_id)
+                return
+
+        # TODO: handle changing directory to file
+        for path in (path2id.keys() - new_paths):
+            self.delete(path2id[path])
+        for path in list(new_paths - path2id.keys()):
+            tag = 'dir' if path.is_dir() else 'file'
+            path2id[path] = self.insert(dir_id, 'end', text=path.name, values=[path], tags=tag, open=False)
+            if path.is_dir():
+                assert dir_path is not None
+                self._insert_dummy(path2id[path])
+
+        for child_path, child_id in path2id.items():
+            with timer.add("A"):
+                old_tags = set(self.item(child_id, 'tags'))
+                new_tags = {tag for tag in old_tags if not tag.startswith('git_')}
+
+                [git_status] = [
+                    self.git_statuses[parent]
+                    for parent in [child_path] + list(child_path.parents)
+                    if parent in self.git_statuses
+                ]
+
+            with timer.add("B"):
+                if child_path in git_status:
+                    new_tags.add(git_status[child_path])
+                else:
+                    with timer.add("B2"):
+                        for status_path, tag in git_status.items():
+                            if status_path == child_path or status_path in child_path.parents:
+                                new_tags.add(tag)
+                                break
+                        else:
+                            with timer.add("B2 inner"):
+                                # Handle directories containing files with different statuses
+                                new_tags |= {
+                                    status
+                                    for subpath, status in git_status.items()
+                                    if status in {'git_added', 'git_modified'}
+                                    and child_path in subpath.parents
+                                }
+
+            with timer.add("C"):
+                if old_tags != new_tags:
+                    self.item(child_id, tags=list(new_tags))
+
+            if 'dir' in new_tags and not self._contains_dummy(child_id):
+                self.open_and_refresh_directory(child_path, child_id, timer)
+
+        if dir_path is not None:
+            children = sorted(self.get_children(dir_id), key=self._sorting_key)
+            for index, child_id in enumerate(children):
+                self.move(child_id, dir_id, index)
+
+    # TODO: does this run too often?
+    def refresh_everything(self, junk: object = None):
+        print("refresh_everything START")
+        da_timer = Timer()
+        with timer("refreshing everything"):
+            with timer("hiding old projects"):
+                self.hide_old_projects()
+            with timer("calling git status for each project"):
+                start = time.time()
+                self.git_statuses = {
+                    path: run_git_status(path)
+                    for path in map(self.get_path, self.get_children())
+                }
+            with timer("open_and_refresh_directory"):
+                self.open_and_refresh_directory(None, '', da_timer)
+        print("refresh_everything DONE")
+        da_timer.show()
+
+    def _insert_dummy(self, parent: str) -> None:
+        assert parent
+        self.insert(parent, 'end', text='(empty)', tags='dummy')
+
+    def _contains_dummy(self, parent: str) -> None:
+        children = self.get_children(parent)
+        return (len(children) == 1 and 'dummy' in self.item(children[0], 'tags'))
+
     def on_click(self, event: tkinter.Event) -> None:
-        [selected_id] = self.selection()
+        try:
+            [selected_id] = self.selection()
+        except ValueError:
+            # nothing selected, can happen when double-clicking something else than one of the items
+            return
+
         tags = self.item(selected_id, 'tags')
-        if 'file' in tags or 'dir' in tags:
-            path = self.get_path(selected_id)
-            if 'dir' in tags:
-                self.refresh_directory(path, selected_id)
-            else:
-                get_tab_manager().add_tab(tabs.FileTab.open_file(get_tab_manager(), path))
+        if 'dir' in tags:
+            self.open_and_refresh_directory(self.get_path(selected_id), selected_id, Timer())
+        elif 'file' in tags:
+            get_tab_manager().add_tab(tabs.FileTab.open_file(get_tab_manager(), self.get_path(selected_id)))
 
     def get_path(self, item_id: str) -> pathlib.Path:
+        assert 'dummy' not in self.item(item_id, 'tags')
         return pathlib.Path(self.item(item_id, 'values')[0])
 
     def _get_children_recursively(self, item_id: str) -> Iterator[str]:
         for child in self.get_children(item_id):
             yield child
             yield from self._get_children_recursively(child)
-
-    # FIXME: this runs too often, clicking text widget --> slow response
-    def update_git_tags(self, junk: object = None) -> None:
-        total_start = time.time()
-
-        for project_id in self.get_children(''):
-            project_path = self.get_path(project_id)
-
-            start = time.time()
-            try:
-                run_result = subprocess.run(
-                    ['git', 'status', '--ignored', '--porcelain'],
-                    cwd=project_path,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                    encoding=sys.getfilesystemencoding(),
-                )
-                if run_result.returncode != 0:
-                    log.info(f"git failed: {run_result}")
-                    continue
-            except (OSError, UnicodeError):
-                log.warning("can't run git", exc_info=True)
-                continue
-            print(f"git ran for {round((time.time() - start)*1000)}ms")
-
-            # Show .git as ignored, even though it actually isn't
-            parsed_git_status = {project_path / '.git': 'git_ignored'}
-            for line in run_result.stdout.splitlines():
-                path = project_path / line[3:]
-                if line[1] == 'M':
-                    parsed_git_status[path] = 'git_modified'
-                elif line[1] == ' ':
-                    parsed_git_status[path] = 'git_added'
-                elif line[:2] == '??':
-                    parsed_git_status[path] = 'git_untracked'
-                elif line[:2] == '!!':
-                    parsed_git_status[path] = 'git_ignored'
-                else:
-                    log.warning(f"unknown git status line: {repr(line)}")
-
-            self._update_git_tags_and_sort_dir(project_id, parsed_git_status)
-            for item_id in self._get_children_recursively(project_id):
-                if 'dir' in self.item(item_id, 'tags') and not self._contains_dummy(item_id):
-                    self._update_git_tags_and_sort_dir(item_id, parsed_git_status)
-
-        print(f"total time {round((time.time() - total_start)*1000)}ms")
-
-    # TODO: use git tags when sorting
-    def _update_git_tags_and_sort_dir(self, dir_id: str, parsed_git_status: Dict[pathlib.Path, str]) -> None:
-        for child_id in self.get_children(dir_id):
-            child_path = self.get_path(child_id)
-            old_tags = set(self.item(child_id, 'tags'))
-            new_tags = {tag for tag in old_tags if not tag.startswith('git_')}
-
-            for status_path, tag in parsed_git_status.items():
-                if status_path == child_path or status_path in child_path.parents:
-                    new_tags.add(tag)
-                    break
-            else:
-                # Handle directories containing files with different statuses
-                new_tags |= {
-                    status
-                    for subpath, status in parsed_git_status.items()
-                    if status in {'git_added', 'git_modified'}
-                    and child_path in subpath.parents
-                }
-
-            if old_tags != new_tags:
-                self.item(child_id, tags=list(new_tags))
-
-        children = sorted(self.get_children(dir_id), key=self._sorting_key)
-        for index, child_id in enumerate(children):
-            self.move(child_id, dir_id, index)
 
     def _sorting_key(self, item_id) -> Tuple[Any, ...]:
         tags = self.item(item_id, 'tags')
@@ -234,8 +305,10 @@ def on_new_tab(tree: DirectoryTree, tab: tabs.Tab) -> None:
         tab.bind('<<PathChanged>>', path_callback, add=True)
         tab.bind('<<PathChanged>>', tree.hide_old_projects, add=True)
         tab.bind('<Destroy>', tree.hide_old_projects, add=True)
-#        tab.bind('<<Save>>', tree.update_git_tags, add=True)
-#        tab.textwidget.bind('<FocusIn>', tree.update_git_tags, add=True)
+
+        # TODO: don't always refresh everything?
+        tab.bind('<<Save>>', tree.refresh_everything, add=True)
+        tab.textwidget.bind('<FocusIn>', tree.refresh_everything, add=True)
 
 
 def setup() -> None:
@@ -243,6 +316,7 @@ def setup() -> None:
     container = ttk.Frame(get_paned_window())
     tree = DirectoryTree(container)
     tree.pack(side='left', fill='both', expand=True)
+    tree.bind('<FocusIn>', tree.refresh_everything, add=True)
     scrollbar = ttk.Scrollbar(container)
     scrollbar.pack(side='right', fill='y')
 
