2b533032d48b050e3d45546a033ad5163a89836f
Akuli <akuviljanen17@gmail.com>
2021-02-24 14:53:57 +0200

    Search speedup (#378)
    * optimize matching_paren plugin with copy/pasta
    
    * add pseudo-optimization
    
    * make the two cases more similar
    
    * more pseudo -optimizations
    
    * simplify code
    
    * shorten slightly
    
    * simplify line number counting
    
    * speed up find plugin similarly
    
    * simplify slightly

--- porcupine/plugins/matching_paren.py
+++ porcupine/plugins/matching_paren.py
@@ -3,6 +3,7 @@ When you put the cursor next to ')', this plugin highlights the matching '('.
 """
 from __future__ import annotations
 
+import re
 import tkinter
 from functools import partial
 
@@ -13,8 +14,7 @@ OPEN_TO_CLOSE = {
     '[': ']',
     '(': ')',
 }
-OPEN = OPEN_TO_CLOSE.keys()
-CLOSE = OPEN_TO_CLOSE.values()
+CLOSE_TO_OPEN = {close: open_ for open_, close in OPEN_TO_CLOSE.items()}
 
 
 def on_cursor_moved(event: tkinter.Event[tkinter.Text]) -> None:
@@ -25,39 +25,49 @@ def on_cursor_moved(event: tkinter.Event[tkinter.Text]) -> None:
         return
 
     last_char = event.widget.get('insert - 1 char')
-    if last_char in OPEN:
-        search_backwards = False
-        search_start = 'insert'
-    elif last_char in CLOSE:
-        search_backwards = True
-        search_start = 'insert - 1 char'
+    cursor_line, cursor_column = map(int, event.widget.index('insert').split('.'))
+    stack = [last_char]
+
+    # Tkinter's .search() is slow when there are lots of tags from highlight plugin.
+    # See "PERFORMANCE ISSUES" in text widget manual page
+    if last_char in OPEN_TO_CLOSE.keys():
+        backwards = False
+        text = event.widget.get('insert', 'end - 1 char')
+        regex = r'(?<!\\)[()\[\]{}]'
+        mapping = CLOSE_TO_OPEN
+    elif last_char in OPEN_TO_CLOSE.values():
+        backwards = True
+        text = event.widget.get('1.0', 'insert - 1 char')[::-1]
+        regex = r'[()\[\]{}](?!\\)'
+        mapping = OPEN_TO_CLOSE
     else:
         return
 
-    stack = [last_char]
-    while stack:
-        match = event.widget.search(
-            r'[()\[\]{}]', search_start, ('1.0' if search_backwards else 'end'),
-            regexp=True, backwards=search_backwards)
-        if not match:
-            return   # unclosed parentheses
+    for match in re.finditer(regex, text):
+        char = match.group()
+        if char not in mapping:
+            assert char in mapping.values()
+            stack.append(char)
+            continue
 
-        # ignore backslash-escaped parens
-        if event.widget.get(f'{match} - 1 char') != '\\':
-            paren = event.widget.get(match)
-            if (paren in OPEN and not search_backwards) or (paren in CLOSE and search_backwards):
-                stack.append(paren)
-            elif (paren in CLOSE and not search_backwards) or (paren in OPEN and search_backwards):
-                pair = (stack.pop(), paren)
-                if pair not in OPEN_TO_CLOSE.items() and pair[::-1] not in OPEN_TO_CLOSE.items():
-                    # foo([) does not highlight its () because you forgot to close square bracket
-                    return
+        if stack.pop() != mapping[char]:
+            return
+        if not stack:
+            if backwards:
+                lineno = 1 + text.count('\n', match.end())
+                if lineno == 1:
+                    column = len(text) - match.end()
+                else:
+                    column = text.index('\n', match.end()) - match.end()
             else:
-                raise NotImplementedError(paren)
-        search_start = match if search_backwards else f'{match} + 1 char'
-
-    event.widget.tag_add('matching_paren', 'insert - 1 char')
-    event.widget.tag_add('matching_paren', match)
+                lineno = cursor_line + text.count('\n', 0, match.start())
+                if lineno == cursor_line:
+                    column = cursor_column + match.start()
+                else:
+                    column = match.start() - text.rindex('\n', 0, match.start()) - 1
+            event.widget.tag_add('matching_paren', 'insert - 1 char')
+            event.widget.tag_add('matching_paren', f'{lineno}.{column}')
+            break
 
 
 def on_pygments_theme_changed(text: tkinter.Text, fg: str, bg: str) -> None:
