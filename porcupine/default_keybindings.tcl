c2b6ab15ad2eeb494f65c3b3dff73ac5ab056b22
Akuli <akuviljanen17@gmail.com>
2021-03-08 10:40:21 +0200

    add key binding for scrolling with keyboard (fixes #405)

--- porcupine/default_keybindings.tcl
+++ porcupine/default_keybindings.tcl
@@ -139,3 +139,14 @@ bind Text <Shift-$contmand-BackSpace> {
 bind Text <<Paste>> {+
     catch {%W delete sel.first sel.last}
 }
+
+# Alt and arrow keys to scroll
+set scroll_amount 2
+bind Text <Alt-Up> {
+    %W yview scroll -$scroll_amount units
+    %W mark set insert "@0,[expr [winfo height %W] / 2]"
+}
+bind Text <Alt-Down> {
+    %W yview scroll $scroll_amount units
+    %W mark set insert "@0,[expr [winfo height %W] / 2]"
+}
