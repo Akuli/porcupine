aac2940ed257542d35e348aa88c1ecc71c1e3e3e
Akuli <akuviljanen17@gmail.com>
2021-06-25 21:26:27 +0300

    simplify comment_block logic

--- tests/test_comment_block_plugin.py
+++ tests/test_comment_block_plugin.py
@@ -13,3 +13,38 @@ def test_comment_block_and_undo(filetab, tmp_path):
     assert filetab.textwidget.get("1.0", "end - 1 char") == "foo\nbar\nbaz\n"
     filetab.textwidget.edit_undo()
     assert filetab.textwidget.get("1.0", "end - 1 char") == ""
+
+
+def test_partially_commented(filetab, tmp_path):
+    filetab.textwidget.insert(
+        "1.0",
+        """\
+#We select starting from this line
+#
+
+To start of this line, so that the plugin shouldn't see this line as selected
+""",
+    )
+    filetab.textwidget.tag_add("sel", "1.0", "4.0")
+
+    filetab.textwidget.event_generate("<numbersign>")
+    assert (
+        filetab.textwidget.get("1.0", "end - 1 char")
+        == """\
+We select starting from this line
+
+
+To start of this line, so that the plugin shouldn't see this line as selected
+"""
+    )
+
+    filetab.textwidget.event_generate("<numbersign>")
+    assert (
+        filetab.textwidget.get("1.0", "end - 1 char")
+        == """\
+#We select starting from this line
+#
+#
+To start of this line, so that the plugin shouldn't see this line as selected
+"""
+    )
