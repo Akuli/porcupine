f90c830dc3334553c797e9acac0c9c478715f8f4
Akuli <akuviljanen17@gmail.com>
2021-06-26 17:06:29 +0300

    restart plugin improvements

--- porcupine/plugins/restart.py
+++ porcupine/plugins/restart.py
@@ -1,23 +1,29 @@
-"""Save and restore Porcupine's state when Porcupine is restarted."""
-# TODO: remember which tab was selected
+"""Save and restore opened tabs when Porcupine is restarted."""
+import logging
 import pathlib
 import pickle
 
 from porcupine import dirs, get_main_window, get_tab_manager
+
+log = logging.getLogger(__name__)
 
 # https://fileinfo.com/extension/pkl
 STATE_FILE = pathlib.Path(dirs.user_cache_dir) / "restart_state.pkl"
 
 
 def save_states(junk: object) -> None:
-    states = []
+    file_contents = []
+    selected_tab = get_tab_manager().select()
+
     for tab in get_tab_manager().tabs():
         state = tab.get_state()
         if state is not None:
-            states.append((type(tab), state))
+            file_contents.append(
+                {"tab_type": type(tab), "tab_state": state, "selected": (tab == selected_tab)}
+            )
 
     with STATE_FILE.open("wb") as file:
-        pickle.dump(states, file)
+        pickle.dump(file_contents, file)
 
 
 def setup() -> None:
@@ -26,10 +32,25 @@ def setup() -> None:
 
     try:
         with STATE_FILE.open("rb") as file:
-            states = pickle.load(file)
+            file_contents = pickle.load(file)
     except FileNotFoundError:
-        states = []
+        file_contents = []
 
-    for tab_class, state in states:
-        tab = tab_class.from_state(get_tab_manager(), state)
-        get_tab_manager().add_tab(tab)
+    for state_dict in file_contents:
+        if isinstance(state_dict, tuple):
+            log.info(
+                "state file contains a tab saved by Porcupine 0.93.x or older: " + repr(state_dict)
+            )
+            tab_type, tab_state = state_dict
+            state_dict = {"tab_type": tab_type, "tab_state": tab_state, "selected": True}
+
+        try:
+            tab = state_dict["tab_type"].from_state(get_tab_manager(), state_dict["tab_state"])
+        except FileNotFoundError:
+            log.debug(
+                "file has been deleted, can't reopen tab: " + repr(state_dict["tab_state"]),
+                exc_info=True,
+            )
+            continue
+
+        get_tab_manager().add_tab(tab, select=state_dict["selected"])
