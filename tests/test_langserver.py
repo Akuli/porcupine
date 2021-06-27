60a7a27e3ca84a0c75089fc9b062fe8342980b78
Akuli <akuviljanen17@gmail.com>
2021-06-27 22:03:24 +0300

    add menu test

--- tests/test_langserver.py
+++ tests/test_langserver.py
@@ -1,5 +1,5 @@
-# TODO: create much more tests
-
+# TODO: create much more tests for langserver
+import tkinter
 import time
 
 from sansio_lsp_client import ClientState
@@ -26,23 +26,63 @@ def wait_for_langserver_to_start(filetab):
     )
 
 
-def test_jump_to_definition(filetab, tmp_path):
+def test_basic(filetab, tmp_path):
     filetab.textwidget.insert(
         "1.0",
         """\
 def foo():
     print("Lul")
 
-def bar():
-    foo()
+foo()
 """,
     )
     filetab.save_as(tmp_path / "foo.py")  # starts lang server
     wait_for_langserver_to_start(filetab)
 
-    filetab.textwidget.mark_set("insert", "5.5")  # in middle of calling foo()
+    # Put cursor to middle of calling foo()
+    filetab.textwidget.mark_set("insert", "end - 1 char")
+    filetab.textwidget.mark_set("insert", "insert - 1 line")
+    filetab.textwidget.mark_set("insert", "insert + 2 chars")
+
     filetab.textwidget.event_generate("<<JumpToDefinition>>")
     wait_until(lambda: bool(filetab.textwidget.tag_ranges("sel")))
 
     assert filetab.textwidget.get("sel.first", "sel.last") == "foo"
     assert filetab.textwidget.get("sel.first linestart", "sel.last lineend") == "def foo():"
+
+
+def test_two_definitions(filetab, tmp_path, mocker):
+    filetab.textwidget.insert(
+        "1.0",
+        """\
+if lolwat:
+    def foo():  # first
+        print("Lul")
+else:
+    def foo():  # second
+        print("Lul2")
+
+foo()
+""",
+    )
+    filetab.save_as(tmp_path / "foo.py")  # starts lang server
+    wait_for_langserver_to_start(filetab)
+
+    # Put cursor to middle of calling foo()
+    filetab.textwidget.mark_set("insert", "end - 1 char")
+    filetab.textwidget.mark_set("insert", "insert - 1 line")
+    filetab.textwidget.mark_set("insert", "insert + 2 chars")
+
+    mocker.patch("tkinter.Menu")
+    filetab.textwidget.event_generate("<<JumpToDefinition>>")
+    wait_until(lambda: tkinter.Menu.call_args is not None)
+
+    # It should add two menu items pointing at 2 different lines
+    [first_call, second_call] = tkinter.Menu.return_value.add_command.call_args_list
+    assert "Line 2" in str(first_call)
+    assert "Line 5" in str(second_call)
+
+    # Click first menu item
+    first_call[1]['command']()
+    assert filetab.textwidget.get("sel.first", "sel.last") == "foo"
+    assert filetab.textwidget.get("sel.first linestart", "sel.last lineend") == "    def foo():  # first"
