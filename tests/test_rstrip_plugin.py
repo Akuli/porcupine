378503b2769a7996daabc0962ead9211f663b460
Akuli <akuviljanen17@gmail.com>
2021-06-27 22:26:37 +0300

    ctrl+enter no longer means "leave trailing whitespace untouched"

--- tests/test_rstrip_plugin.py
+++ tests/test_rstrip_plugin.py
@@ -4,14 +4,3 @@ def test_rstrip(filetab):
     filetab.event_generate("<Return>")
     filetab.update()
     assert filetab.textwidget.get("1.0", "end - 1 char") == 'print("hello")\n'
-
-    # ctrl+enter should not wipe trailing whitespace
-    filetab.textwidget.delete("1.0", "end")
-    filetab.textwidget.insert("end", 'print("hello")  ')
-    filetab.update()
-    if filetab.tk.eval("tk windowingsystem") == "aqua":
-        filetab.event_generate("<Command-Return>")
-    else:
-        filetab.event_generate("<Control-Return>")
-    filetab.update()
-    assert filetab.textwidget.get("1.0", "end - 1 char") == 'print("hello")  \n'
