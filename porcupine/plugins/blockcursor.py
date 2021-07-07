c0d0cdfecbfaa6bd673fd6b89d218b1f268dd899
rdbende <rdbende@gmail.com>
2021-07-07 11:58:36 +0200

    fix mypy fail

--- porcupine/plugins/blockcursor.py
+++ porcupine/plugins/blockcursor.py
@@ -6,7 +6,7 @@ import tkinter
 from porcupine import get_tab_manager, tabs, settings
 
 
-def on_insert_key(event: tkinter.Event = None) -> None:
+def on_insert_key(event: tkinter.Event) -> str:
     event.widget["blockcursor"] = False if event.widget["blockcursor"] else True
     settings.set_("blockcursor", bool(event.widget["blockcursor"]))
     return "break"  # sometimes the insert key also inserts the content of the cb, we abort it
