0097caa613af8042cbaeece73e1f15e8c9549e91
rdbende <rdbende@gmail.com>
2021-07-07 19:41:38 +0200

    commit suggested changes

--- porcupine/plugins/blockcursor.py
+++ porcupine/plugins/blockcursor.py
@@ -1,27 +1,24 @@
 """Toggles between the ❚ and | cursor state of the textwidgets."""
 from __future__ import annotations
 
-import tkinter
-
 from porcupine import get_tab_manager, settings, tabs
 
 
-def do_toggle(event: tkinter.Event[tkinter.Text]) -> None:
-    if settings.get("blockcursor", object):
-        event.widget.configure(blockcursor=True, insertwidth=0)  # minimize the cursor thickness
+def do_toggle(tab: tabs.FileTab) -> None:
+    if settings.get("blockcursor", bool):
+        tab.textwidget.configure(blockcursor=True, insertwidth=0)  # minimize the cursor thickness
     else:
-        event.widget.configure(
+        tab.textwidget.configure(
             blockcursor=False, insertwidth=2
         )  # set insertwidth back to tk default
 
 
 def on_new_filetab(tab: tabs.FileTab) -> None:
-    is_block = settings.get("blockcursor", object)
-    tab.textwidget.config(blockcursor=is_block, insertwidth=0 if is_block else 2)
-    tab.textwidget.bind("<<SettingChanged:blockcursor>>", do_toggle, add=True)
+    tab.bind("<<SettingChanged:blockcursor>>", lambda event: do_toggle(tab), add=True)
+    do_toggle(tab)
 
 
 def setup() -> None:
     settings.add_option("blockcursor", False)
-    settings.add_checkbutton("blockcursor", text="Use blockcursor")
+    settings.add_checkbutton("blockcursor", text="Show cursor as a block instead of a thin line")
     get_tab_manager().add_filetab_callback(on_new_filetab)
