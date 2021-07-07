979e0ecb61708e5e51e6ff1e2be5d2e266a2fb6b
rdbende <rdbende@gmail.com>
2021-07-07 02:03:48 +0200

    create blockcursor plugin

---
+++ porcupine/plugins/blockcursor.py
@@ -0,0 +1,24 @@
+"""Toggles between the ❚ and | cursor state when the insert key is pressed."""
+from __future__ import annotations
+
+import tkinter
+
+from porcupine import get_tab_manager, settings
+
+
+def on_insert_key(event: tkinter.Event = None) -> None:
+    event.widget["blockcursor"] = False if event.widget["blockcursor"] else True
+    settings.set_("blockcursor", bool(event.widget["blockcursor"]))
+    return "break"  # sometimes the insert key also inserts the content of the cb, we abort it
+
+
+def on_filetab(tab: tabs.FileTab) -> None:
+    tab.textwidget.config(blockcursor=settings.get("blockcursor", object))
+    tab.textwidget.bind("<Insert>", on_insert_key, add=True)
+    tab.textwidget.bind("<KP_Insert>", on_insert_key, add=True)
+
+
+def setup() -> None:
+    settings.add_option("blockcursor", False)
+    get_tab_manager().add_filetab_callback(on_filetab)
+
