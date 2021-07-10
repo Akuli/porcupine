96de33f4611c54c8d9705e8dff9271f3644ca77d
Akuli <akuviljanen17@gmail.com>
2021-07-10 14:19:04 +0300

    black

--- porcupine/plugins/find.py
+++ porcupine/plugins/find.py
@@ -186,11 +186,15 @@ class Finder(ttk.Frame):
         # "find_highlight_selected". Both have to match.
         locations = [str(loc) for loc in self._textwidget.tag_ranges("sel")]
         locations2 = [str(loc) for loc in self._textwidget.tag_ranges("find_highlight_selected")]
-        replace_this_state = "normal" if (
-            locations == locations2
-            and len(locations) == 2
-            and tuple(locations) in self.get_match_ranges()
-        ) else "disabled"
+        replace_this_state = (
+            "normal"
+            if (
+                locations == locations2
+                and len(locations) == 2
+                and tuple(locations) in self.get_match_ranges()
+            )
+            else "disabled"
+        )
 
         self.previous_button.config(state=matches_something_state)
         self.next_button.config(state=matches_something_state)
