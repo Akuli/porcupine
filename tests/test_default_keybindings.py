74b71e6b3e8fcae9aaa08fc49a9ae7c250ebad35
Akuli <akuviljanen17@gmail.com>
2021-07-12 20:40:03 +0300

    fix backspace in beginning of file

---
+++ tests/test_default_keybindings.py
@@ -0,0 +1,9 @@
+def test_backspace_in_beginning_of_file(filetab):
+    filetab.textwidget.insert('end', 'a')
+    filetab.textwidget.mark_set('insert', '1.0')
+    filetab.textwidget.event_generate('<BackSpace>')
+    assert filetab.textwidget.get('1.0', 'end - 1 char') == 'a'
+
+    filetab.textwidget.tag_add('sel', '1.0', '1.1')
+    filetab.textwidget.event_generate('<BackSpace>')
+    assert filetab.textwidget.get('1.0', 'end - 1 char') == ''
