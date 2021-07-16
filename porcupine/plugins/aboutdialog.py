0a2d6763f8e55153f34514b926efbe09b1100d44
Arrinao <62777744+Arrinao@users.noreply.github.com>
2021-07-16 23:44:40 +0200

    Deleted tooltipmanager class and the function that was using it (#610)

--- porcupine/plugins/aboutdialog.py
+++ porcupine/plugins/aboutdialog.py
@@ -19,7 +19,7 @@ else:
     from typing import Match
 
 from porcupine import __version__ as porcupine_version
-from porcupine import get_main_window, images, menubar, plugins, textutils, utils
+from porcupine import get_main_window, images, menubar, plugins, textutils
 
 _BORING_TEXT = """
 Porcupine is a simple but powerful and configurable text editor written in \
@@ -84,7 +84,6 @@ class _AboutDialogContent(ttk.Frame):
 
         label = ttk.Label(self, image=images.get("logo-200x200"), cursor="hand2")
         label.pack(anchor="e")
-        utils.set_tooltip(label, "Click to view in full size")
         label.bind("<Button-1>", show_huge_logo, add=True)
 
     def _add_minimal_markdown(self, text: str) -> None:
