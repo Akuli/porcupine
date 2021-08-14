37a1e33edd980cd455d85ed0e0d6b08a0a0f01c4
Akuli <akuviljanen17@gmail.com>
2021-08-15 00:57:08 +0300

    Revert "attempt sys.executable fix"
    This reverts commit 1dae8656ba15a96752cf76fec6837e6d8da5f485.

--- launcher/main.c
+++ launcher/main.c
@@ -16,20 +16,17 @@ typedef int(WINAPI *PyMainProc)(int argc, wchar_t **argv);
 
 int wmain(int argc, wchar_t **argv)
 {
-	wchar_t exepath[PATH_MAX];
+	wchar_t *launcherpath = calloc(sizeof(launcherpath[0]), MAX_PATH);
+	if (!launcherpath)
+		fatal_error(L"allocating memory failed");
+
 	if (GetModuleFileNameW(NULL, launcherpath, MAX_PATH-1) <= 0)
 		fatal_error(L"GetModuleFileNameW(NULL, ...) failed");
 
 	// ...\Porcupine\Python\Porcupine.exe --> ...\Porcupine\launch.pyw
-	wchar_t *launcherpath = wcsdup(exepath);
 	*wcsrchr(launcherpath, L'\\') = L'\0';
 	*wcsrchr(launcherpath, L'\\') = L'\0';
 	wcscat(launcherpath, L"\\launch.pyw");
-
-	// ...\Porcupine\Python\Porcupine.exe --> ...\Porcupine\Python\python.exe
-	wchar_t *pypath = wcsdup(exepath);
-	*wcsrchr(pypath, L'\\') = L'\0';
-	wcscat(pypath, L"\\python.exe");
 
 	HMODULE pydll = LoadLibraryW(L"python3.dll");
 	if (!pydll)
@@ -41,7 +38,7 @@ int wmain(int argc, wchar_t **argv)
 
 	// argv[argc] is NULL
 	wchar_t **myargv = malloc(sizeof(myargv[0]) * (argc+2));
-	myargv[0] = pypath;  // so that sys.executable does the right thing
+	myargv[0] = argv[0];
 	myargv[1] = launcherpath;
 	memcpy(&myargv[2], &argv[1], sizeof(myargv[0]) * argc);
 
