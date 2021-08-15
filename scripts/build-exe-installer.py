c2812c98b502ac8247ddac9a83ac2d9ac7e4e0e9
Akuli <akuviljanen17@gmail.com>
2021-08-15 18:05:00 +0300

    black

--- scripts/build-exe-installer.py
+++ scripts/build-exe-installer.py
@@ -109,7 +109,9 @@ else:
 
 print("Installing Porcupine into build/Python with pip")
 # TODO: delete --use-feature=in-tree-build when pip is new enough to not make warning without it
-subprocess.check_call(["pip", "install", "--use-feature=in-tree-build", "--target=build/Python", "."])
+subprocess.check_call(
+    ["pip", "install", "--use-feature=in-tree-build", "--target=build/Python", "."]
+)
 
 print("Moving files")
 shutil.move("build/launcher/Porcupine.exe", "build/Python/Porcupine.exe")
