fc8e783f8b9a59612d68bca4d00a1c6c94e3df79
Akuli <akuviljanen17@gmail.com>
2021-08-17 00:34:09 +0300

    geometry plugin: do not place window outside screen (#664)

--- porcupine/plugins/geometry.py
+++ porcupine/plugins/geometry.py
@@ -1,9 +1,25 @@
 """Remember the size and location of the Porcupine window."""
 from __future__ import annotations
 
+import re
 import tkinter
 
 from porcupine import get_main_window, settings
+
+
+def geometry_is_within_screen(geometry: str) -> bool:
+    match = re.fullmatch(r"(.*)x(.*)\+(.*)\+(.*)", geometry)
+    if match is None:
+        # Does not specify location, size only
+        return True
+
+    width, height, x, y = map(int, match.groups())
+    return (
+        x >= 0
+        and x + width <= get_main_window().winfo_screenwidth()
+        and y >= 0
+        and y + height <= get_main_window().winfo_screenheight()
+    )
 
 
 def save_geometry(event: tkinter.Event[tkinter.Misc]) -> None:
@@ -13,5 +29,9 @@ def save_geometry(event: tkinter.Event[tkinter.Misc]) -> None:
 
 def setup() -> None:
     settings.add_option("default_geometry", "650x600")
-    get_main_window().geometry(settings.get("default_geometry", str))
     get_main_window().bind("<<PorcupineQuit>>", save_geometry, add=True)
+
+    geometry = settings.get("default_geometry", str)
+    if not geometry_is_within_screen(geometry):
+        geometry = geometry.split("+")[0]  # size only, discard location
+    get_main_window().geometry(geometry)
