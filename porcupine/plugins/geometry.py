c87fdfcc31c520685b46e018c8f78bc44b5c2bff
Akuli <akuviljanen17@gmail.com>
2021-08-17 00:02:59 +0300

    Update porcupine/plugins/geometry.py

--- porcupine/plugins/geometry.py
+++ porcupine/plugins/geometry.py
@@ -15,7 +15,7 @@ def geometry_is_within_screen(geometry: str) -> bool:
 
     width, height, x, y = map(int, match.groups())
     return (
-        0 <= x
+        x >= 0
         and x + width <= get_main_window().winfo_screenwidth()
         and 0 <= y
         and y <= get_main_window().winfo_screenheight()
