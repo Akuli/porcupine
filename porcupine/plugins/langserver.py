51faad32c008e7ef0c68b895c82e727024e02278
Akuli <akuviljanen17@gmail.com>
2021-08-21 17:57:51 +0300

    langserver: don't use nonblock flag for writing

--- porcupine/plugins/langserver.py
+++ porcupine/plugins/langserver.py
@@ -49,39 +49,39 @@ class SubprocessStdIO:
         self._process = process
 
         # Reads can obviously block, but flushing can block too, see #635
+        # Nonblock flags don't help with writing, it just raises error if it would block
+        self._write_queue: queue.Queue[bytes] = queue.Queue()
+        threading.Thread(target=self._write_queue_to_stdin, daemon=True).start()
+
         if sys.platform == "win32":
             self._read_queue: queue.Queue[bytes] = queue.Queue()
-            self._write_queue: queue.Queue[bytes] = queue.Queue()
             self._reader_thread = threading.Thread(target=self._stdout_to_read_queue, daemon=True)
             self._reader_thread.start()
-            threading.Thread(target=self._write_queue_to_stdin, daemon=True).start()
         else:
-            for stream in [process.stdin, process.stdout]:
-                # this works because we don't use .readline()
-                # https://stackoverflow.com/a/1810703
-                assert stream is not None
-                fileno = stream.fileno()
-                old_flags = fcntl.fcntl(fileno, fcntl.F_GETFL)
-                new_flags = old_flags | os.O_NONBLOCK
-                fcntl.fcntl(fileno, fcntl.F_SETFL, new_flags)
+            # this works because we don't use .readline()
+            # https://stackoverflow.com/a/1810703
+            assert process.stdout is not None
+            fileno = process.stdout.fileno()
+            old_flags = fcntl.fcntl(fileno, fcntl.F_GETFL)
+            new_flags = old_flags | os.O_NONBLOCK
+            fcntl.fcntl(fileno, fcntl.F_SETFL, new_flags)
+
+    def _write_queue_to_stdin(self) -> None:
+        while self._process.poll() is None:
+            # Why timeout: if process dies and no more to write, stop soon
+            try:
+                chunk = self._write_queue.get(timeout=5)
+            except queue.Empty:  # timed out
+                continue
+
+            # Process can exit while waiting, but clean shutdown involves
+            # writing messages before the process exits, so here it should
+            # be still alive
+            assert self._process.stdin is not None
+            self._process.stdin.write(chunk)
+            self._process.stdin.flush()
 
     if sys.platform == "win32":
-
-        def _write_queue_to_stdin(self) -> None:
-            while self._process.poll() is None:
-                # Why timeout: if process dies and no more to write, stop soon
-                try:
-                    chunk = self._write_queue.get(timeout=5)
-                except queue.Empty:  # timed out
-                    continue
-
-                # Process can exit while waiting, but clean shutdown involves
-                # writing messages before the process exits, so here it should
-                # be still alive
-                assert self._process.stdin is not None
-                self._process.stdin.write(chunk)
-                self._process.stdin.flush()
-
         def _stdout_to_read_queue(self) -> None:
             while True:
                 # for whatever reason, nothing works unless i go ONE BYTE at a
@@ -98,7 +98,6 @@ class SubprocessStdIO:
     #   - None: no data to read
     def read(self) -> bytes | None:
         if sys.platform == "win32":
-            # shitty windows code
             buf = bytearray()
             while True:
                 try:
@@ -115,12 +114,7 @@ class SubprocessStdIO:
             return self._process.stdout.read(CHUNK_SIZE)
 
     def write(self, bytez: bytes) -> None:
-        if sys.platform == "win32":
-            self._write_queue.put(bytez)
-        else:
-            assert self._process.stdin is not None
-            self._process.stdin.write(bytez)
-            self._process.stdin.flush()
+        self._write_queue.put(bytez)
 
 
 def error_says_socket_not_connected(error: OSError) -> bool:
