4ce63bdba37c69c407e798e5f36cb942242bb4b7
Akuli <akuviljanen17@gmail.com>
2021-09-27 12:12:55 +0300

    move stuff

--- porcupine/default_keybindings.tcl
+++ porcupine/default_keybindings.tcl
@@ -77,15 +77,15 @@ event add "<<Menubar:View/Pop Tab>>" <$contmand-P>
 # directory tree plugin (don't use <Alt-t>, see #425)
 event add "<<Menubar:View/Focus directory tree>>" <Alt-T>
 
-# jump_to_definition plugin
-# cursor moves between button press and release, don't bind to press
-event add "<<JumpToDefinition>>" <$contmand-ButtonRelease-1>
-event add "<<JumpToDefinition>>" <$contmand-Return>
-
 # filemanager plugin
 event add "<<FileManager:Rename>>" <F2>
 event add "<<FileManager:Trash>>" <Delete>
 event add "<<FileManager:Delete>>" <Shift-Delete>
+
+# jump_to_definition plugin
+# cursor moves between button press and release, don't bind to press
+event add "<<JumpToDefinition>>" <$contmand-ButtonRelease-1>
+event add "<<JumpToDefinition>>" <$contmand-Return>
 
 # more_plugins/terminal.py
 # upper-case T means Ctrl+Shift+T or Command+Shift+T
