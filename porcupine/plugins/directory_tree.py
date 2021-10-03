f085c10207ba343e2764922f053876f1a7f95b9c
1anakin20 <33395451+1anakin20@users.noreply.github.com>
2021-10-03 14:48:27 -0400

    fix MacOS right clicking (#792)

--- porcupine/plugins/directory_tree.py
+++ porcupine/plugins/directory_tree.py
@@ -461,9 +461,8 @@ def setup() -> None:
             tree.add_project(path, refresh=False)
     tree.refresh()
 
-    # TODO: mac right click = button 2?
     # TODO: invoking context menu from keyboard
-    tree.bind("<Button-3>", tree._on_right_click, add=True)
+    tree.bind("<<RightClick>>", tree._on_right_click, add=True)
 
     tree.bind("<Key>", tree._cycle_through_items, add=True)
 
