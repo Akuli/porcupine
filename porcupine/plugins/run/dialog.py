24c4cd767a16bb0ebf762b11d4c53b28c675572b
rdbende <rdbende@gmail.com>
2021-10-04 20:13:42 +0200

    fix some padding, make substitutions selectable

--- porcupine/plugins/run/dialog.py
+++ porcupine/plugins/run/dialog.py
@@ -33,7 +33,7 @@ class FormattingEntryAndLabels(Generic[T]):
 
         self.format_var = tkinter.StringVar()
         self.entry = ttk.Entry(entry_area, font="TkFixedFont", textvariable=self.format_var)
-        self.entry.grid(row=grid_y, column=1, sticky="we")
+        self.entry.grid(row=grid_y, column=1, sticky="we", padx=(5, 0))
         self.entry.selection_range(0, "end")
 
         grid_y += 1
@@ -105,8 +105,14 @@ class CommandAsker:
             validated_callback=self.update_run_button,
         )
 
-        sub_text = "\n".join("{%s} = %s" % pair for pair in substitutions.items())
-        ttk.Label(content_frame, text=f"Substitutions:\n{sub_text}\n").pack(fill="x")
+        sub_text = "\n".join("\t•  {%s} = %s" % pair for pair in substitutions.items())
+        sub_textbox = textutils.create_passive_text_widget(
+            content_frame, height=len(substitutions) + 2, tabs=10
+        )
+        sub_textbox.pack(fill="x")
+        sub_textbox.config(state="normal")
+        sub_textbox.insert("1.0", f"Substitutions:\n{sub_text}\n")
+        sub_textbox.config(state="disabled")
 
         # TODO: remember value with settings
         self.terminal_var = tkinter.BooleanVar()
@@ -137,10 +143,12 @@ class CommandAsker:
 
         button_frame = ttk.Frame(content_frame)
         button_frame.pack(fill="x")
-        cancel_button = ttk.Button(button_frame, text="Cancel", command=self.window.destroy)
-        cancel_button.pack(side="left", fill="x", expand=True)
-        self.run_button = ttk.Button(button_frame, text="Run", command=self.on_run_clicked)
-        self.run_button.pack(side="left", fill="x", expand=True)
+        cancel_button = ttk.Button(
+            button_frame, text="Cancel", command=self.window.destroy, width=1
+        )
+        cancel_button.pack(side="left", fill="x", expand=True, padx=(0, 5))
+        self.run_button = ttk.Button(button_frame, text="Run", command=self.on_run_clicked, width=1)
+        self.run_button.pack(side="left", fill="x", expand=True, padx=(5, 0))
         self.run_clicked = False
 
         for entry in [self.command.entry, self.cwd.entry]:
