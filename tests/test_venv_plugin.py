dbb6c5a94436637151cc95f0c33ff4af350d3d7a
Akuli <akuviljanen17@gmail.com>
2021-10-06 16:42:12 +0300

    add test

--- tests/test_venv_plugin.py
+++ tests/test_venv_plugin.py
@@ -9,10 +9,11 @@ from porcupine.plugins import python_venv
 def test_venv_setting(tmp_path):
     assert python_venv.get_venv(tmp_path) is None
 
+    # auto-detect venv
     subprocess.run([sys.executable, "-m", "venv", "env2"], cwd=tmp_path, check=True)
     assert python_venv.get_venv(tmp_path) == tmp_path / "env2"
 
-    # Never change venv implicitly, as new venvs are created
+    # Never change venv implicitly as new venvs are created
     subprocess.run([sys.executable, "-m", "venv", "env1"], cwd=tmp_path, check=True)
     assert python_venv.get_venv(tmp_path) == tmp_path / "env2"
 
@@ -22,6 +23,11 @@ def test_venv_setting(tmp_path):
     for env in ["env1", "env2", "env1", "env1", "env2", "env2"]:
         python_venv.set_venv(tmp_path, tmp_path / env)
         assert python_venv.get_venv(tmp_path) == tmp_path / env
+
+    # Explicitly say no to using venvs, prevent auto-detection from running
+    python_venv.set_venv(tmp_path, None)
+    for lel in range(10):
+        assert python_venv.get_venv(tmp_path) is None
 
 
 def test_venv_becomes_invalid(tmp_path, caplog):
