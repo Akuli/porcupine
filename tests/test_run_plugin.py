f39cebf9ec2eff39617a9d03ae396a06ee458f6a
Akuli <akuviljanen17@gmail.com>
2021-10-11 20:53:16 +0300

    add failing test

--- tests/test_run_plugin.py
+++ tests/test_run_plugin.py
@@ -209,3 +209,12 @@ while True:
     no_terminal.run_command(f"{utils.quote(sys.executable)} hello.py", tmp_path)
     wait_until(lambda: "Hello" in get_output())
     assert not size_is_changing(tmp_path / "out.txt")
+
+
+def test_smashing_f5(tmp_path, wait_until):
+    (tmp_path / "hello.py").write_text("print('Hello')")
+    no_terminal.run_command(f"{utils.quote(sys.executable)} hello.py", tmp_path)
+    no_terminal.run_command(f"{utils.quote(sys.executable)} hello.py", tmp_path)
+    no_terminal.run_command(f"{utils.quote(sys.executable)} hello.py", tmp_path)
+    wait_until(lambda: "The process completed successfully." in get_output())
+    assert not get_output()
