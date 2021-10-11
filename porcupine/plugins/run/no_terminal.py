f65a34d2d2f5b57a8db20e0e8e1bf70ee8acd881
Akuli <akuviljanen17@gmail.com>
2021-10-11 12:43:00 +0000

    Revert "simplify some stuff and mess up others"
    This reverts commit 6ad011dfa29142217ebdadf821ba482d5af18093.

--- porcupine/plugins/run/no_terminal.py
+++ porcupine/plugins/run/no_terminal.py
@@ -83,6 +83,7 @@ class NoTerminalRunner:
                 return
             self._output_queue.put(msg)
 
+        emit_message(("start", ""))
         emit_message(("info", command + "\n"))
 
         env = dict(os.environ)
@@ -128,7 +129,11 @@ class NoTerminalRunner:
         if messages:
             self.textwidget.config(state="normal")
             for tag, output_line in messages:
-                if tag == "end":
+                if tag == "start":
+                    assert not output_line
+                    self.textwidget.delete("1.0", "end")
+                    self._link_manager.delete_all_links()  # prevent memory leak
+                elif tag == "end":
                     assert not output_line
                     get_tab_manager().event_generate("<<FileSystemChanged>>")
                 else:
@@ -143,11 +148,6 @@ class NoTerminalRunner:
         process = self._running_process
         if process is not None:
             process.kill()
-
-    def clear(self) -> None:
-        self.kill_process()
-        self._running_process = None
-        self.textwidget.delete("1.0", "end")
 
 
 runner: NoTerminalRunner | None = None
@@ -177,5 +177,5 @@ def run_command(command: str, cwd: Path) -> None:
         closebutton.bind("<Button-1>", on_close, add=True)
         closebutton.place(relx=1, rely=0, anchor="ne")
 
-    runner.clear()
+    runner.textwidget.delete("1.0", "end")
     runner.run_command(cwd, command)
