1f929e249df4c4d8ddfacb211edda38d8ddcc8b5
rdbende <rdbende@gmail.com>
2021-10-12 15:32:02 +0200

    File is huge warning (#894)

--- porcupine/tabs.py
+++ porcupine/tabs.py
@@ -162,6 +162,20 @@ class TabManager(ttk.Notebook):
         If the file can't be opened, this method displays an error to the user
         and returns ``None``.
         """
+
+        try:
+            if path.stat().st_size > 1_000_000 and not messagebox.askyesno(
+                "Open big file",
+                "Uhh, this file is huge!\nAre you sure you want to open it? ",
+                detail=(
+                    "This file is larger than 1MB. If you open it, Porcupine may be unusably slow"
+                    " or require an huge amount of RAM."
+                ),
+            ):
+                return None
+        except OSError:
+            pass  # no problem, reload() will handle the error
+
         # Add tab before loading content, so that editorconfig plugin gets a
         # chance to set the encoding into tab.settings
         tab = FileTab(self, path=path)
