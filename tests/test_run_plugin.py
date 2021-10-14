e42b51ae7b3256e105327ee015d6d75cf0f7fcdf
Akuli <akuviljanen17@gmail.com>
2021-10-15 01:48:26 +0300

    param

--- tests/test_run_plugin.py
+++ tests/test_run_plugin.py
@@ -291,27 +291,21 @@ while True:
     assert not size_is_changing(tmp_path / "out.txt")
 
 
-def test_smashing_f5(tmp_path, wait_until):
+@pytest.mark.parametrize("use_after_idle", [True, False])
+def test_smashing_f5(tmp_path, wait_until, use_after_idle):
     (tmp_path / "hello.py").write_text("print('Hello')")
-    no_terminal.run_command(f"{utils.quote(sys.executable)} hello.py", tmp_path)
-    no_terminal.run_command(f"{utils.quote(sys.executable)} hello.py", tmp_path)
-    no_terminal.run_command(f"{utils.quote(sys.executable)} hello.py", tmp_path)
-    wait_until(lambda: "The process completed successfully." in get_output())
 
-    first_line, rest = get_output().split("\n", 1)
-    assert first_line.endswith("hello.py")
-    assert rest == "Hello\nThe process completed successfully."
-
-
-def test_smashing_f5_again_lol(tmp_path, wait_until):
-    (tmp_path / "hello.py").write_text("print('Hello')")
     run = lambda: no_terminal.run_command(f"{utils.quote(sys.executable)} hello.py", tmp_path)
-    get_main_window().after_idle(run)
-    get_main_window().after_idle(run)
-    get_main_window().after_idle(run)
+    if use_after_idle:
+        get_main_window().after_idle(run)
+        get_main_window().after_idle(run)
+        get_main_window().after_idle(run)
+    else:
+        run()
+        run()
+        run()
+
     wait_until(lambda: "The process completed successfully." in get_output())
-    
     first_line, rest = get_output().split("\n", 1)
     assert first_line.endswith("hello.py")
     assert rest == "Hello\nThe process completed successfully."
-
