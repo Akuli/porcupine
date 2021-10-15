9d37f95e4590b9e88876c881e98b07486689b3cd
Akuli <akuviljanen17@gmail.com>
2021-10-15 22:39:54 +0300

    fix test

--- tests/test_run_plugin.py
+++ tests/test_run_plugin.py
@@ -315,5 +315,5 @@ def test_stop_button(tmp_path, wait_until):
     (tmp_path / "sleeper.py").write_text("import time; print('started'); time.sleep(10)")
     no_terminal.run_command(f"{utils.quote(sys.executable)} sleeper.py", tmp_path)
     wait_until(lambda: "started" in get_output())
-    no_terminal.runner.stop_button.invoke()
+    no_terminal.runner.stop_button.event_generate("<Button-1>")
     wait_until(lambda: "started\nKilled.")
