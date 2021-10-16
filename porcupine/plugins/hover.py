3f32d0d2714888434dff36484f1c09f01e4db77d
rdbende <rdbende@gmail.com>
2021-10-16 15:13:12 +0200

    Make popups have similar background as the main text widget (#935)
    Co-authored-by: Akuli <akuviljanen17@gmail.com>

--- porcupine/plugins/hover.py
+++ porcupine/plugins/hover.py
@@ -30,9 +30,9 @@ class HoverManager:
             self._textwidget,
             text=message,
             justify="left",
-            # opposite colors as in the text widget
-            bg=self._textwidget["fg"],
-            fg=self._textwidget["bg"],
+            # some hack to get a fitting popup bg
+            bg=utils.mix_colors(self._textwidget["bg"], self._textwidget["fg"], 0.8),
+            fg=self._textwidget["fg"],
         )
         textutils.place_popup(
             self._textwidget,
