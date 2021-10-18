5e7cef2538121018652855d285f85c2dc677df49
Akuli <akuviljanen17@gmail.com>
2021-10-18 13:00:28 +0300

    more tests

--- tests/test_git_right_click.py
+++ tests/test_git_right_click.py
@@ -1,27 +1,41 @@
+from functools import partial
 import shutil
 import subprocess
 
 import pytest
 
+from porcupine.plugins.directory_tree import get_directory_tree
 
-# FIXME: copy/pasted from test_directory_tree_plugin.py
-def open_as_if_user_clicked(tree, item):
-    tree.selection_set(item)
-    tree.item(item, open=True)
+
+def add_project_and_select_file(project_path, file_path):
+    tree = get_directory_tree()
+    tree.add_project(project_path)
+    [project_id] = tree.get_children("")
+    tree.selection_set(project_id)
+    tree.item(project_id, open=True)
     tree.event_generate("<<TreeviewOpen>>")
     tree.update()
+    tree.select_file(file_path)
 
 
 @pytest.mark.skipif(shutil.which("git") is None, reason="git not found")
-def test_working_on_untracked_file(tree, tmp_path):
-    subprocess.check_call("git init", cwd=tmp_path, shell=True)
+def test_working_on_new_file(tree, tmp_path):
+    subprocess.check_call("git init -q", cwd=tmp_path, shell=True)
     (tmp_path / "a.py").write_text("lol")
 
-    tree.add_project(tmp_path)
-    [project_id] = tree.get_children("")
-    open_as_if_user_clicked(tree, project_id)
-    tree.select_file(tmp_path / "a.py")
+    add_project_and_select_file(tmp_path, tmp_path / "a.py")
+    tree._populate_contextmenu()
+    assert tree.contextmenu.entrycget("git add", "state") == "normal"
+    assert tree.contextmenu.entrycget("* (undo add)", "state") == "disabled"
+    assert tree.contextmenu.entrycget("* (discard non-added changes)", "state") == "disabled"
 
+    tree.contextmenu.invoke("git add")
+    tree._populate_contextmenu()
+    assert tree.contextmenu.entrycget("git add", "state") == "disabled"
+    assert tree.contextmenu.entrycget("* (undo add)", "state") == "normal"
+    assert tree.contextmenu.entrycget("* (discard non-added changes)", "state") == "disabled"
+
+    tree.contextmenu.invoke("* (undo add)")
     tree._populate_contextmenu()
     assert tree.contextmenu.entrycget("git add", "state") == "normal"
     assert tree.contextmenu.entrycget("* (undo add)", "state") == "disabled"
@@ -44,3 +58,50 @@ def test_working_on_untracked_file(tree, tmp_path):
     assert tree.contextmenu.entrycget("git add", "state") == "disabled"
     assert tree.contextmenu.entrycget("* (undo add)", "state") == "normal"
     assert tree.contextmenu.entrycget("* (discard non-added changes)", "state") == "disabled"
+
+
+@pytest.mark.skipif(shutil.which("git") is None, reason="git not found")
+def test_working_on_committed_file(tree, tmp_path):
+    run = partial(subprocess.run, stdout=subprocess.DEVNULL, shell=True, check=True, cwd=tmp_path)
+    run("git init")
+    run("git config user.name foo")  # not --global, will stay inside test repo
+    run("git config user.email foo@bar.baz")
+    (tmp_path / "a.py").write_text("lol")
+    run("git add a.py")
+    run("git commit -m asdf")
+
+    add_project_and_select_file(tmp_path, tmp_path / "a.py")
+    tree._populate_contextmenu()
+    assert tree.contextmenu.entrycget("git add", "state") == "disabled"
+    assert tree.contextmenu.entrycget("* (undo add)", "state") == "disabled"
+    assert tree.contextmenu.entrycget("* (discard non-added changes)", "state") == "disabled"
+
+    (tmp_path / "a.py").write_text("lolwat new text")
+    tree._populate_contextmenu()
+    assert tree.contextmenu.entrycget("git add", "state") == "normal"
+    assert tree.contextmenu.entrycget("* (undo add)", "state") == "disabled"
+    assert tree.contextmenu.entrycget("* (discard non-added changes)", "state") == "normal"
+
+    tree.contextmenu.invoke("git add")
+    tree._populate_contextmenu()
+    assert tree.contextmenu.entrycget("git add", "state") == "disabled"
+    assert tree.contextmenu.entrycget("* (undo add)", "state") == "normal"
+    assert tree.contextmenu.entrycget("* (discard non-added changes)", "state") == "disabled"
+
+    tree.contextmenu.invoke("* (undo add)")
+    tree._populate_contextmenu()
+    assert tree.contextmenu.entrycget("git add", "state") == "normal"
+    assert tree.contextmenu.entrycget("* (undo add)", "state") == "disabled"
+    assert tree.contextmenu.entrycget("* (discard non-added changes)", "state") == "normal"
+
+    tree.contextmenu.invoke("git add")
+    tree._populate_contextmenu()
+    assert tree.contextmenu.entrycget("git add", "state") == "disabled"
+    assert tree.contextmenu.entrycget("* (undo add)", "state") == "normal"
+    assert tree.contextmenu.entrycget("* (discard non-added changes)", "state") == "disabled"
+
+    (tmp_path / "a.py").write_text("this is even newer text")
+    tree._populate_contextmenu()
+    assert tree.contextmenu.entrycget("git add", "state") == "normal"
+    assert tree.contextmenu.entrycget("* (undo add)", "state") == "normal"
+    assert tree.contextmenu.entrycget("* (discard non-added changes)", "state") == "normal"
