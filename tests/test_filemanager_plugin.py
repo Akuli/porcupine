2f189203e14927d765c12cbc7248e43aa4e0dd83
rdbende <rdbende@gmail.com>
2021-10-25 18:23:48 +0200

    fix a test

--- tests/test_filemanager_plugin.py
+++ tests/test_filemanager_plugin.py
@@ -41,7 +41,7 @@ def test_cutpasting_or_copypasting_to_same_dir(tree, tmp_path, mocker, event):
     tree.event_generate(event)
     tree.event_generate("<<Paste>>")
     ask_file_name.assert_called_once_with(
-        tmp_path / "foo", is_paste=True, show_overwriting_option=False
+        tmp_path / "foo", mode="paste", overwrite=False
     )
 
     if event == "<<Copy>>":
