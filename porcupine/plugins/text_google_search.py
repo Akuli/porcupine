b7105b4ff877b0fef23a4df394a50a879c1c701e
okan kamil sen <okankamilsen@gmail.com>
2022-01-16 14:23:31 +0100

    multiline, space, except fixed

--- porcupine/plugins/text_google_search.py
+++ porcupine/plugins/text_google_search.py
@@ -8,10 +8,15 @@ import urllib.parse
 def google_search(tab: tabs.FileTab) -> None:
     try:
         selected_text = tab.textwidget.get("sel.first", "sel.last")
+    except tk.TclError:
+        # nothing selected
+        return
+    
+    # Check multi line or text with only spaces
+    if selected_text.strip() and "\n" not in selected_text and not selected_text.isspace():
         url = "https://www.google.com/search?q={}".format(urllib.parse.quote_plus(selected_text))
         webbrowser.open_new_tab(url)
-    except:
-        pass
+    
     
 def setup() -> None:
     menubar.add_filetab_command("Tools/Search selected text on Google", google_search)
