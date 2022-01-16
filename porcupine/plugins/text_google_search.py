1eda1802f1d113f3e9270c55a442eaa2e1aeaffc
okan kamil sen <okankamilsen@gmail.com>
2022-01-16 14:04:12 +0100

    ctrl+g error resolved

--- porcupine/plugins/text_google_search.py
+++ porcupine/plugins/text_google_search.py
@@ -6,9 +6,12 @@ import webbrowser
 import urllib.parse
 
 def google_search(tab: tabs.FileTab) -> None:
-    selected_text = tab.textwidget.get("sel.first", "sel.last")
-    url = "https://www.google.com/search?q={}".format(urllib.parse.quote_plus(selected_text))
-    webbrowser.open_new_tab(url)
-
+    try:
+        selected_text = tab.textwidget.get("sel.first", "sel.last")
+        url = "https://www.google.com/search?q={}".format(urllib.parse.quote_plus(selected_text))
+        webbrowser.open_new_tab(url)
+    except:
+        pass
+    
 def setup() -> None:
     menubar.add_filetab_command("Tools/Search selected text on Google", google_search)
