3734582906ebb0803fc98be8e72026c263cd6a7c
okan kamil sen <okankamilsen@gmail.com>
2022-01-16 15:31:08 +0100

    str strip and test fails fixed

--- porcupine/plugins/text_google_search.py
+++ porcupine/plugins/text_google_search.py
@@ -1,10 +1,11 @@
 """Search selected text on Google."""
 
 import tkinter as tk
-import webbrowser
 import urllib.parse
+import webbrowser
 
 from porcupine import menubar, tabs
+
 
 def google_search(tab: tabs.FileTab) -> None:
     try:
@@ -12,12 +13,12 @@ def google_search(tab: tabs.FileTab) -> None:
     except tk.TclError:
         # nothing selected
         return
-    
+
     # Check multi line or text with only spaces
-    if selected_text.strip() and "\n" not in selected_text and not selected_text.isspace():
+    if selected_text.strip() and "\n" not in selected_text:
         url = "https://www.google.com/search?q={}".format(urllib.parse.quote_plus(selected_text))
         webbrowser.open_new_tab(url)
-    
-    
+
+
 def setup() -> None:
     menubar.add_filetab_command("Tools/Search selected text on Google", google_search)
