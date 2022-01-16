0869a0e826a7438a3864e9cb4fff3b3346577310
okan kamil sen <okankamilsen@gmail.com>
2022-01-16 14:00:11 +0100

    urllib added for selected text

--- porcupine/plugins/text_google_search.py
+++ porcupine/plugins/text_google_search.py
@@ -3,10 +3,11 @@
 import tkinter as tk
 from porcupine import menubar, tabs
 import webbrowser
+import urllib.parse
 
 def google_search(tab: tabs.FileTab) -> None:
     selected_text = tab.textwidget.get("sel.first", "sel.last")
-    url = "https://www.google.com/search?q={}".format(selected_text)
+    url = "https://www.google.com/search?q={}".format(urllib.parse.quote_plus(selected_text))
     webbrowser.open_new_tab(url)
 
 def setup() -> None:
