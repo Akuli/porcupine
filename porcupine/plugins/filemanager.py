5fe11200e18ed2beeb200a4ba80c39f7d828e526
Hammad Asghar <ihammadasghar@gmail.com>
2022-04-09 17:50:51 +0100

    Add 'Copy full path to clipboard' option

--- porcupine/plugins/filemanager.py
+++ porcupine/plugins/filemanager.py
@@ -316,6 +316,12 @@ def open_in_file_manager(path: Path) -> None:
     subprocess.Popen([opener_command, str(path)])
 
 
+def copy_full_path_to_clipboard(path: Path) -> None:
+    main_window = get_main_window()
+    main_window.clipboard_clear()
+    main_window.clipboard_append(str(path))
+
+
 def get_selected_path(tree: DirectoryTree) -> Path | None:
     try:
         [item] = tree.selection()
@@ -371,6 +377,7 @@ commands = [
     Command(f"Move to {trash_name}", "<<FileManager:Trash>>", is_NOT_project_root, trash),
     Command("Delete", "<<FileManager:Delete>>", is_NOT_project_root, delete),
     Command("Open in file manager", None, (lambda p: p.is_dir()), open_in_file_manager),
+    Command("Copy full path to clipboard", None, (lambda p: p.is_dir()), copy_full_path_to_clipboard),
 ]
 
 
