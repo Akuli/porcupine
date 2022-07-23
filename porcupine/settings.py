747975b6010fa2bd69c08154707752c046595075
Akuli <akuviljanen17@gmail.com>
2022-07-24 01:14:55 +0300

    black

--- porcupine/settings.py
+++ porcupine/settings.py
@@ -924,8 +924,7 @@ def _fill_dialog_content_with_defaults() -> None:
         "default_line_ending", "Default line ending:", values=[ending.name for ending in LineEnding]
     )
     add_checkbutton(
-        "remember_tabs_on_restart",
-        text="Remember open tabs when Porcupine is closed and reopened",
+        "remember_tabs_on_restart", text="Remember open tabs when Porcupine is closed and reopened"
     )
     add_pygments_style_button("pygments_style", "Pygments style for editing:")
 
