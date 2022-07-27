a0a06a3b758c234d1f7b0e6b3859e0e697cf299f
Akuli <akuviljanen17@gmail.com>
2022-07-28 00:09:19 +0300

    better test

--- tests/test_images.py
+++ tests/test_images.py
@@ -1,9 +1,8 @@
 import os
-from pathlib import Path
 
 
 def test_sources_txt_is_up_to_date():
-    sources_file_content = Path("porcupine/images/sources.txt").read_text()
-    for filename in os.listdir("porcupine/images"):
-        if filename not in {"__init__.py", "__pycache__", "sources.txt"}:
-            assert filename in sources_file_content
+    with open("porcupine/images/sources.txt") as file:
+        filenames_mentioned = [line.split()[0] for line in file]
+    non_image_files = {"__init__.py", "__pycache__", "sources.txt"}
+    assert set(filenames_mentioned) == set(os.listdir("porcupine/images")) - non_image_files
