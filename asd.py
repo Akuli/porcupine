7243632a9fd0a9257bdc696f07d7f700db48e512
Akuli <akuviljanen17@gmail.com>
2022-08-08 22:42:45 +0300

    how about now

--- asd.py
+++ asd.py
@@ -1,5 +1,5 @@
 import os
-from ctypes import windll
+import ctypes
 import tempfile
 import tree_sitter
 from zipfile import ZipFile
@@ -8,6 +8,6 @@ z = ZipFile("porcupine/plugins/highlight/tree-sitter-data/tree-sitter-binaries.z
 with tempfile.TemporaryDirectory() as d:
     z.extract("tree-sitter-binary-win32-AMD64.dll", d)
     x = tree_sitter.Language(os.path.join(d, "tree-sitter-binary-win32-AMD64.dll"), "python")
-    windll.kernel32.FreeLibrary(x.lib._handle)
+    ctypes.windll.kernel32.FreeLibrary(ctypes.c_void_p(x.lib._handle))
     del x
     import gc; gc.collect()
