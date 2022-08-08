3937b9933987004b7a9393400767bcce64a7939f
Akuli <akuviljanen17@gmail.com>
2022-08-08 22:05:42 +0300

    try dis

--- asd.py
+++ asd.py
@@ -1,7 +1,9 @@
 import os
+import tempfile
 import tree_sitter
 from zipfile import ZipFile
 
 z = ZipFile("porcupine/plugins/highlight/tree-sitter-data/tree-sitter-binaries.zip")
-z.extract("tree-sitter-binary-win32-AMD64.dll")
-x = tree_sitter.Language(os.path.abspath("tree-sitter-binary-win32-AMD64.dll"), "python")
+with tempfile.TemporaryDirectory() as d:
+    z.extract("tree-sitter-binary-win32-AMD64.dll", d)
+    x = tree_sitter.Language(os.path.join(d, "tree-sitter-binary-win32-AMD64.dll"), "python")
