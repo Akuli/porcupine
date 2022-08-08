de5f82fd36a2c727c48c89151bc574f81500c18c
Akuli <akuviljanen17@gmail.com>
2022-08-08 22:22:04 +0300

    FreeLibrary?

--- asd.py
+++ asd.py
@@ -7,4 +7,5 @@ z = ZipFile("porcupine/plugins/highlight/tree-sitter-data/tree-sitter-binaries.z
 with tempfile.TemporaryDirectory() as d:
     z.extract("tree-sitter-binary-win32-AMD64.dll", d)
     x = tree_sitter.Language(os.path.join(d, "tree-sitter-binary-win32-AMD64.dll"), "python")
+    windll.kernel32.FreeLibrary(x.lib._handle)
     del x
