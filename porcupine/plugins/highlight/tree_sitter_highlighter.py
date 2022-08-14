2f3c32c27e6eae0f2b829707c7be80badd745c11
Akuli <akuviljanen17@gmail.com>
2022-08-14 17:45:40 +0300

    tree-sitter highlighting for toml (#1151)

--- porcupine/plugins/highlight/tree_sitter_highlighter.py
+++ porcupine/plugins/highlight/tree_sitter_highlighter.py
@@ -115,37 +115,12 @@ class TreeSitterHighlighter(BaseHighlighter):
         return self.textwidget.get("1.0", "end - 1 char").encode("ascii", errors="replace")
 
     def _decide_tag(self, node: tree_sitter.Node) -> str:
-        # A hack for TOML. This:
-        #
-        #   [foo]
-        #   x=1
-        #   y=2
-        #
-        # parses as:
-        #
-        #    type='[' text=b'['
-        #    type='bare_key' text=b'foo'
-        #    type=']' text=b']'
-        #    type='pair' text=b'x=1'
-        #    type='pair' text=b'y=2'
-        #
-        # I want the whole section header [foo] to have the same tag.
-        if (
-            self._language.name == "toml"
-            and node.type not in ("pair", "comment")
-            and node.parent is not None
-            and node.parent.type in ("table", "table_array_element")
-        ):
-            type_name = node.parent.type
-        else:
-            type_name = node.type
-
-        if set(type_name) <= set("+-*/%~&|^!?<>=@.,:;()[]{}"):
+        if set(node.type) <= set("+-*/%~&|^!?<>=@.,:;()[]{}"):
             default = "Token.Operator"
         else:
             default = "Token.Text"
 
-        config_value = self._config.token_mapping.get(type_name, default)
+        config_value = self._config.token_mapping.get(node.type, default)
         if isinstance(config_value, dict):
             return config_value.get(node.text.decode("utf-8"), default)
         return config_value
