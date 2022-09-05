15f6ee23b8dbcc552f5e26137ae544ab1e3665f0
Akuli <akuviljanen17@gmail.com>
2022-09-05 17:18:22 +0300

    Make restart plugin stateless

--- porcupine/plugins/fullscreen.py
+++ porcupine/plugins/fullscreen.py
@@ -1,10 +1,17 @@
 """Full Screen button in the View menu."""
-import tkinter
 
 from porcupine import get_main_window, menubar
 
 
+def set_fullscreened(value: bool) -> None:
+    get_main_window().attributes("-fullscreen", value)
+
+
+def get_fullscreened() -> bool:
+    return bool(get_main_window().attributes("-fullscreen"))
+
+
 def setup() -> None:
-    var = tkinter.BooleanVar()
-    var.trace_add("write", (lambda *junk: get_main_window().attributes("-fullscreen", var.get())))
-    menubar.get_menu("View").add_checkbutton(label="Full Screen", variable=var)
+    menubar.get_menu("View").add_command(
+        label="Toggle Full Screen", command=lambda: set_fullscreened(not get_fullscreened())
+    )
