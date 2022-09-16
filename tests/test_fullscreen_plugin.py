b10c9d48c5da4a0ca86f5b29b3f961a5e7ae8886
Akuli <akuviljanen17@gmail.com>
2022-09-16 20:11:07 +0300

    Split test

--- tests/test_fullscreen_plugin.py
+++ tests/test_fullscreen_plugin.py
@@ -2,7 +2,7 @@ from porcupine import get_main_window
 from porcupine.menubar import get_menu
 
 
-def test_fullscreen(wait_until):
+def test_basic(wait_until):
     assert not get_main_window().attributes("-fullscreen")
 
     get_menu("View").invoke("Toggle Full Screen")
@@ -11,7 +11,9 @@ def test_fullscreen(wait_until):
     get_menu("View").invoke("Toggle Full Screen")
     wait_until(lambda: not get_main_window().attributes("-fullscreen"))
 
-    # Window managers can toggle full-screen-ness without going through the menubar
+
+# Window managers can toggle full-screen-ness without going through our menubar
+def test_toggled_without_menu_bar(wait_until):
     get_main_window().attributes("-fullscreen", 1)
     wait_until(lambda: bool(get_main_window().attributes("-fullscreen")))
 
