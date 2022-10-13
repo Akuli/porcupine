f29c2afd09da550225df79532ef5d6db8cc5ce00
Akuli <akuviljanen17@gmail.com>
2022-10-13 21:28:45 +0300

    initial version of debug prompt

---
+++ porcupine/plugins/porcupine_debug_prompt.py
@@ -0,0 +1,77 @@
+import contextlib
+import io
+import queue
+import traceback
+import tkinter
+from tkinter import ttk
+from typing import Any
+
+from porcupine import get_tab_manager, menubar, tabs, textutils
+
+
+# In "Run" menu, get the important stuff first
+setup_after = ["run"]
+
+
+class PromptTab(tabs.Tab):
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.title_choices = ["Porcupine Debug Prompt"]
+        self.namespace = {}
+        exec('from porcupine import *', self.namespace)
+
+        self.textwidget = tkinter.Text(self, width=1, height=1)
+        self.textwidget.pack(side='left', fill='both', expand=True)
+        self.textwidget.mark_set('output_end', 'end')
+        self.textwidget.mark_gravity('output_end', 'left')
+        self.show('>>> ')
+        try:
+            textutils.use_pygments_theme(self.textwidget)
+        except AttributeError:
+            textutils.use_pygments_tags(self.textwidget)
+
+        self.scrollbar = ttk.Scrollbar(self)
+        self.scrollbar.pack(side='left', fill='y')
+        self.textwidget.config(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.textwidget.yview)
+
+        self.to_interpreter_queue = queue.Queue()
+        self.from_interpreter_queue = queue.Queue()
+        self.bind('<Destroy>', (lambda event: self.to_interpreter_queue.put(None)), add=True)
+        self.bind('<<TabSelected>>', (lambda event: self.textwidget.focus()), add=True)
+        self.textwidget.bind('<Return>', self.on_enter_key, add=True)
+
+    def show(self, string):
+        self.textwidget.insert('end - 1 char', string)
+        self.textwidget.mark_set('output_end', 'end - 1 char')
+        self.textwidget.mark_set('insert', 'end - 1 char')
+        self.textwidget.see('insert')
+
+    def on_focus(self) -> None:
+        self.textwidget.focus_set()
+
+    def on_enter_key(self, event=None) -> None:
+        code_string = self.textwidget.get('output_end', 'end - 1 char')
+        out = io.StringIO()
+        with contextlib.redirect_stdout(out), contextlib.redirect_stderr(out):
+            try:
+                code = compile(code_string, '<prompt>', 'single')
+                exec(code, self.namespace)
+            except Exception:
+                traceback.print_exc()
+        self.show(f'\n{out.getvalue()}>>> ')
+        return 'break'
+
+
+def start_prompt() -> None:
+    for tab in get_tab_manager().tabs():
+        if isinstance(tab, PromptTab):
+            get_tab_manager().select(tab)
+            return
+    get_tab_manager().add_tab(PromptTab(get_tab_manager()))
+
+
+def setup() -> None:
+    menubar.get_menu("Run").add_separator()
+    menubar.get_menu("Run").add_command(label="Porcupine debug prompt", command=start_prompt)
