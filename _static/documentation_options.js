a36b669d985b7764a3af6ff392f297ca96370e57
Akuli <akuviljanen17@gmail.com>
2023-01-19 21:14:57 +0000

    Deploying to gh-pages from @ Akuli/porcupine@235f8e67782ccf1a43de5f9cd08c5d022142adb5 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2022.11.25',
+    VERSION: '2023.01.19',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
