a025b3e2d5f8faa3a9341823be99a6db04c8c533
Akuli <akuviljanen17@gmail.com>
2023-01-21 14:33:25 +0200

    Fix various bugs.

--- porcupine/plugins/desktop_menu.py
+++ porcupine/plugins/desktop_menu.py
@@ -4,60 +4,81 @@ You can enable it in Porcupine's settings.
 """
 
 import os
+import shlex
 import shutil
 import subprocess
+import tkinter
 from pathlib import Path
-from tkinter import messagebox
+from tkinter import messagebox, ttk
 
-from porcupine import dirs, get_tab_manager, images, settings
-from porcupine.settings import global_settings
+from porcupine import dirs, images, settings
 
 setup_after = ["filetypes"]  # To group the checkbutton on the bottom
 
 XDG_DESKTOP_MENU = "xdg-desktop-menu"
+DESKTOP_FILE_PATH = Path(dirs.user_config_dir) / "Porcupine.desktop"
 
 
-def create_desktop_file(venv_path: str) -> Path:
-    file_path = Path(dirs.user_cache_dir) / "Porcupine.desktop"
+def create_desktop_file(venv_path: Path) -> None:
+    activate_path = venv_path / "bin" / "activate"
+    assert activate_path.is_file()
 
-    with file_path.open("w") as file:
+    with DESKTOP_FILE_PATH.open("w") as file:
         file.write("[Desktop Entry]\n")
         file.write("Name=Porcupine\n")
         file.write("GenericName=Text Editor\n")
-        file.write(f"Exec={venv_path}/bin/porcu\n")
+        # Must activate the venv, otherwise various things don't work
+        # (e.g. os.environ.get("VIRTUAL_ENV") in this plugin)
+        bash_command = f"source {shlex.quote(str(activate_path))} && porcu"
+        file.write(f"Exec=bash -c {bash_command}\n")
         file.write("Terminal=false\n")
         file.write("Type=Application\n")
         file.write("Categories=TextEditor;Development;\n")
         file.write(f"Icon={images.images_dir}/logo-200x200.gif\n")
 
-    return file_path
 
-
-def install_uninstall_desktop_file(junk_event: object) -> None:
-    if global_settings.get("has_desktop_file", bool):
-        venv = os.environ.get("VIRTUAL_ENV")
-        if not venv or not (Path(venv) / "bin" / "porcu").is_file():
-            messagebox.showerror(
-                "Creating menu entry failed",
-                "Porcupine must be installed in a virtual environment in order to create a desktop menu entry.",
-            )
-            global_settings.set("has_desktop_file", False)
-            return None
-
-        launcher_path = create_desktop_file(venv)
-        subprocess.call(
-            [XDG_DESKTOP_MENU, "install", "--mode", "user", "--novendor", launcher_path]
+def install_desktop_file() -> None:
+    venv = os.environ.get("VIRTUAL_ENV")
+    if not venv or not (Path(venv) / "bin" / "porcu").is_file():
+        messagebox.showerror(
+            "Creating menu entry failed",
+            "Porcupine must be installed in a virtual environment in order to create a desktop menu entry.",
         )
-    else:
-        subprocess.call([XDG_DESKTOP_MENU, "uninstall", "--mode", "user", "Porcupine.desktop"])
+        return
+
+    create_desktop_file(Path(venv))
+    subprocess.call(
+        [XDG_DESKTOP_MENU, "install", "--mode", "user", "--novendor", DESKTOP_FILE_PATH]
+    )
+
+
+def uninstall_desktop_file() -> None:
+    subprocess.call([XDG_DESKTOP_MENU, "uninstall", "--mode", "user", "Porcupine.desktop"])
+    DESKTOP_FILE_PATH.unlink()
+
+
+# Can't use settings.add_checkbutton() because it makes a checkbox that
+# gets unchecked when you reset all settings. This leaves the user in a
+# weird situation where Porcupine is installed but can't be launched
+# from the menu.
+def add_checkbox_to_settings() -> None:
+    checkbutton = ttk.Checkbutton(
+        settings.get_dialog_content(), text="Show Porcupine in the desktop menu system"
+    )
+    checkbutton.grid(column=0, columnspan=2, sticky="w", pady=2)
+
+    var = tkinter.BooleanVar(value=DESKTOP_FILE_PATH.exists())
+    checkbutton.config(variable=var)
+
+    def var_changed(*junk: object) -> None:
+        if var.get():
+            install_desktop_file()
+        else:
+            uninstall_desktop_file()
+
+    var.trace_add("write", var_changed)
 
 
 def setup() -> None:
-    if not shutil.which("xdg-desktop-menu"):
-        return
-
-    global_settings.add_option("has_desktop_file", False)
-    settings.add_checkbutton("has_desktop_file", text="Show Porcupine in the desktop menu system")
-    get_tab_manager().bind(
-        "<<GlobalSettingChanged:has_desktop_file>>", install_uninstall_desktop_file, add=True
-    )
+    if shutil.which(XDG_DESKTOP_MENU):
+        add_checkbox_to_settings()
