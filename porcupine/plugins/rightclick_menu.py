f6c04ab64a445ac4b2aa0c49553ae6c6ad67e07b
ArchKats <ckatsika@csd.auth.gr>
2023-02-16 16:49:55 +0200

    Various fixes

--- porcupine/plugins/rightclick_menu.py
+++ porcupine/plugins/rightclick_menu.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 
-from typing import Callable, Any
+import tkinter
+
 from porcupine import get_main_window, tabs
 from porcupine.menubar import get_filetab
 
-import tkinter
-
-_ftab_indexes_ = []
-_menu_items_ = {}
+_ftab_indexes = []
+_menu_items = {}
 
 
 def text_is_selected(tab: tabs.FileTab) -> bool:
@@ -18,40 +17,38 @@ def text_is_selected(tab: tabs.FileTab) -> bool:
         return False
     return True
 
-
 def show_menu(event) -> None:
     rm = tkinter.Menu(get_main_window(), tearoff=0)
 
-    for path, func in _menu_items_.items():
+    for path, func in _menu_items.items():
         rm.add_command(label=path, command=func)
 
 
-    if rm.index("end") is not None:
-        flag = bool
-        try:
-            flag=text_is_selected(get_filetab())
-        except AssertionError:
-            flag=False
-        finally:
-            if flag:
-                for i in _ftab_indexes_:
-                    rm.entryconfigure(i, state=tkinter.NORMAL)
-            else:
-                for i in _ftab_indexes_:
-                    rm.entryconfigure(i, state=tkinter.DISABLED)
-        rm.tk_popup(event.x_root, event.y_root)
-        rm.bind("<Unmap>", (lambda event: rm.after_idle(rm.update)), add=True)
+    flag = bool
+    try:
+        flag=text_is_selected(get_filetab())
+    except AssertionError:
+        flag=False
+    finally:
+        if not flag:
+            for i in _ftab_indexes:
+                rm.entryconfigure(i, state=tkinter.DISABLED)
 
-def add_rightclick_option(path: str, func):
-    _menu_items_[path] = func
-def add_filetab_command(path: str, func: Callable[[tabs.FileTab], object] | None = None, **kwargs: Any) -> None:
-    if func is None:
-        command = lambda: get_filetab().event_generate(f"<<FiletabCommand:{path}>>")
+    rm.tk_popup(event.x_root, event.y_root)
+    rm.bind("<Unmap>", (lambda event: rm.after_idle(rm.destroy)), add=True)
+
+def add_rightclick_option(path: str, func, needs_selected_text: bool = False):
+    if not needs_selected_text:
+        _menu_items[path] = func
     else:
-        command = lambda: func(get_filetab())  # type: ignore
+        if func is None:
+            command = lambda: get_filetab().event_generate(f"<<FiletabCommand:{path}>>")
+        else:
+            command = lambda: func(get_filetab())  # type: ignore
 
-    _menu_items_[path] = command
-    _ftab_indexes_.append(len(_menu_items_) - 1)
+        _menu_items[path] = command
+        _ftab_indexes.append(len(_menu_items) - 1)
+
 
 
 def setup()-> None:
--- porcupine/plugins/rightclick_menu_options.py
+++
@@ -1,6 +0,0 @@
-from porcupine.plugins import rightclick_menu, text_google_search
-
-def setup()->None:
-    rightclick_menu.add_filetab_command("Search Google for text", text_google_search.google_search)
-
-
