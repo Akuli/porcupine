74a4a0e801ef5adaa466f8b52fc768c0438499d1
ArchKats <ckatsika@csd.auth.gr>
2023-02-17 20:04:50 +0200

    Fixed it so the menu binds only to the text widget

--- porcupine/plugins/rightclick_menu.py
+++ porcupine/plugins/rightclick_menu.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import tkinter
+from typing import Callable
 
 from porcupine import get_main_window, get_tab_manager, tabs
 from porcupine.menubar import get_filetab
@@ -33,12 +34,13 @@ def show_menu(event) -> None:
     rm.bind("<Unmap>", (lambda event: rm.after_idle(rm.destroy)), add=True)
 
 
-def add_rightclick_option(path: str, func, needs_selected_text: bool = False) -> None:
-    if not needs_selected_text:
-        _menu_items[path] = func
-    else:
+def add_rightclick_option(path: str, func: Callable[[tabs.FileTab], object], needs_selected_text: bool = False) -> None:
+
+    if needs_selected_text:
         _menu_items[path] = lambda: func(get_filetab())
         _ftab_indexes.append(len(_menu_items) - 1)
+    else:
+        _menu_items[path] = func
 
 
 def on_new_filetab(tab: tabs.FileTab) -> None:
