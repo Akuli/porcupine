b5de9eb46b8c2b3aad418148577686ebc4972c29
benjamin-kirkbride <benjamin-kirkbride@users.noreply.github.com>
2023-06-30 23:10:47 +0000

    Run pycln, black and isort

--- tests/test_markdown_plugin.py
+++ tests/test_markdown_plugin.py
@@ -24,9 +24,7 @@ IS_LIST_ITEM_CASES = [
     IsListItemCase(id="# bad separator \\", line="#\\ item 1", expected=False),
     IsListItemCase(id="ol bad separator |", line="8| item 1", expected=False),
     IsListItemCase(id="ol bad separator /", line="8/ item 1", expected=False),
-    IsListItemCase(
-        id="ol bad separator \\", line="8\\ item 1", expected=False
-    ),
+    IsListItemCase(id="ol bad separator \\", line="8\\ item 1", expected=False),
     IsListItemCase(id="not a list 1", line="item 1", expected=False),
     IsListItemCase(id="not a list 2", line="   item 1", expected=False),
     IsListItemCase(id="not a list 3", line="          item 1", expected=False),
@@ -35,27 +33,17 @@ IS_LIST_ITEM_CASES = [
     IsListItemCase(id="duplicate token 1", line="-- item 1", expected=True),
     IsListItemCase(id="duplicate token 2", line="--- item 1", expected=True),
     IsListItemCase(id="duplicate token 3", line="- - - item 1", expected=True),
-    IsListItemCase(
-        id="duplicate token 4", line="  - item -- 1 -", expected=True
-    ),
-    IsListItemCase(
-        id="duplicate token 5", line="  -#) item -- 1 -", expected=True
-    ),
-    IsListItemCase(
-        id="duplicate token 6", line="  *-#)1. item -- 1 -", expected=True
-    ),
+    IsListItemCase(id="duplicate token 4", line="  - item -- 1 -", expected=True),
+    IsListItemCase(id="duplicate token 5", line="  -#) item -- 1 -", expected=True),
+    IsListItemCase(id="duplicate token 6", line="  *-#)1. item -- 1 -", expected=True),
 ]
 
 # test `#` and 0 to 99 numbered lists
 # tests ol with `.` and `)`
 IS_LIST_ITEM_CASES.extend(
     [
-        IsListItemCase(
-            id=f"numbered {i}", line=f"{i}{sep} item 1", expected=True
-        )
-        for i, sep in itertools.product(
-            itertools.chain(range(100), "#"), (".", ")")
-        )
+        IsListItemCase(id=f"numbered {i}", line=f"{i}{sep} item 1", expected=True)
+        for i, sep in itertools.product(itertools.chain(range(100), "#"), (".", ")"))
     ]
 )
 
@@ -81,9 +69,7 @@ IS_LIST_ITEM_CASES.extend(
             line=f"{' ' * preceding}{bullet} {' ' * following} item 1",
             expected=True,
         )
-        for bullet, preceding, following in itertools.product(
-            ("-", "*", "+"), range(11), range(11)
-        )
+        for bullet, preceding, following in itertools.product(("-", "*", "+"), range(11), range(11))
     ]
 )
 
@@ -138,14 +124,10 @@ def test_filetype_switching(li: str, filetab, tmp_path):
 
     filetab.textwidget.event_generate("<Tab>")
     filetab.update()
-    assert (
-        filetab.textwidget.get("1.0", "end - 1 char") == f"    {li}\n"
-    ), "should indent"
+    assert filetab.textwidget.get("1.0", "end - 1 char") == f"    {li}\n", "should indent"
     filetab.textwidget.event_generate("<Shift-Tab>")
     filetab.update()
-    assert (
-        filetab.textwidget.get("1.0", "end - 1 char") == f"{li}\n"
-    ), "should dedent"
+    assert filetab.textwidget.get("1.0", "end - 1 char") == f"{li}\n", "should dedent"
 
 
 @pytest.mark.parametrize(
@@ -167,8 +149,6 @@ def test_filetype_switching(li: str, filetab, tmp_path):
     ],
 )
 def test_non_list(line: str, filetab, tmp_path):
-    import time
-
     # switch to Markdown filetype format
     filetab.save_as(tmp_path / "asdf.md")
     assert filetab.settings.get("filetype_name", object) == "Markdown"
