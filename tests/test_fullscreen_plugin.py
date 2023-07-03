1c41dbe378e2242592a94feeb162ec12c39e905d
Benjamin Kirkbride <benjaminkirkbride@gmail.com>
2023-07-03 19:50:12 -0400

    fix headless detection

--- tests/test_fullscreen_plugin.py
+++ tests/test_fullscreen_plugin.py
@@ -5,13 +5,20 @@ import pytest
 from porcupine import get_main_window
 from porcupine.menubar import get_menu
 
-try:
-    xvfb_status = "xvfb" in os.environ["XAUTHORITY"]
-except KeyError:
-    xvfb = False
+github_actions = os.getenv("GITHUB_ACTIONS")
+xauthority = os.getenv("XAUTHORITY")
+
+headless = False
+
+if github_actions is not None:
+    headless = github_actions == "true"
+elif xauthority is not None:
+    headless = "xvfb" in xauthority
 
 
-@pytest.mark.skipif(xvfb_status, reason="fails CI on all platforms")
+pytestmark = pytest.mark.skipif(headless, reason="Does not work in headless environments")
+
+
 def test_basic(wait_until):
     assert not get_main_window().attributes("-fullscreen")
 
@@ -23,7 +30,6 @@ def test_basic(wait_until):
 
 
 # Window managers can toggle full-screen-ness without going through our menubar
-@pytest.mark.skipif(xvfb_status, reason="fails CI on all platforms")
 def test_toggled_without_menu_bar(wait_until):
     get_main_window().attributes("-fullscreen", 1)
     wait_until(lambda: bool(get_main_window().attributes("-fullscreen")))
