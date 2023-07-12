68dab923572cc5e28b401c410cc5c37373d118a4
Akuli <akuviljanen17@gmail.com>
2023-07-12 17:27:07 +0300

    tests for issue

--- tests/test_anchors_plugin.py
+++ tests/test_anchors_plugin.py
@@ -54,3 +54,31 @@ def test_cyclic_jumping(filetab, cyclic_setting_enabled):
     filetab.textwidget.mark_set("insert", "1.0")
     assert jump_5_times(filetab, "Jump to next") == ["2.0", "4.0", "7.0", "2.0", "4.0"]
     assert jump_5_times(filetab, "Jump to previous") == ["2.0", "7.0", "4.0", "2.0", "7.0"]
+
+
+def test_single_anchor_bug(filetab, cyclic_setting_enabled):
+    # Setup is exactly as in #1353
+    filetab.textwidget.insert("end", "first row\nsecond row")
+
+    # Set an anchor point on row 1 & one anchor point on row 2.
+    filetab.textwidget.mark_set("insert", "1.0")
+    get_menu("Edit/Anchors").invoke("Add or remove on this line")
+    filetab.textwidget.mark_set("insert", "2.0")
+    get_menu("Edit/Anchors").invoke("Add or remove on this line")
+
+    # Set cursor on row 1. Remove the anchor point on row 1.
+    filetab.textwidget.mark_set("insert", "1.0")
+    get_menu("Edit/Anchors").invoke("Add or remove on this line")
+
+    # Use keyboard shortcut to jump to next anchor (down).
+    get_menu("Edit/Anchors").invoke("Jump to next")
+    assert filetab.textwidget.index("insert") == "2.0"
+
+    # Recreate anchor point on row 1. Make sure cursor is on row 1, then remove anchor point on row 1.
+    filetab.textwidget.mark_set("insert", "1.0")
+    get_menu("Edit/Anchors").invoke("Add or remove on this line")
+    get_menu("Edit/Anchors").invoke("Add or remove on this line")
+
+    # Use keyboard shortcut to jump to previous anchor (up).
+    get_menu("Edit/Anchors").invoke("Jump to previous")
+    assert filetab.textwidget.index("insert") == "2.0"
