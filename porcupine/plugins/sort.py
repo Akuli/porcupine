0d1390ff979a27143099b757568879cbd392b68c
Akuli <akuviljanen17@gmail.com>
2023-07-12 16:17:25 +0300

    Sort plugin: use indentation when deciding lines to sort

--- porcupine/plugins/sort.py
+++ porcupine/plugins/sort.py
@@ -1,12 +1,37 @@
 """
-Sort the selected lines alphabetically, or if there's no selection, sort between surrounding blank lines.
+Sort the selected lines alphabetically. Available in menubar → Edit → Sort Lines.
 
-Available in Edit/Sort Lines.
+To sort lines that are indented or surrounded by blank lines, you can simply
+place the cursor somewhere in the middle without selecting anything and run the
+sort. In other words, if there is nothing selected, this plugin will select
+lines above and below the cursor location, excluding blank lines or any lines
+with less indentation.
 """
 
+from __future__ import annotations
 import tkinter
-
 from porcupine import menubar, tabs, textutils
+
+
+def can_extend_default_selection(text: tkinter.Text, required_prefix: str, new_lineno: int) -> bool:
+    line = text.get(f"{new_lineno}.0", f"{new_lineno + 1}.0")
+    return bool(line.strip()) and line.startswith(required_prefix)
+
+
+def find_chunk_around_cursor(text: tkinter.Text) -> tuple[int, int]:
+    cursor_lineno = int(text.index("insert").split(".")[0])
+    cursor_line = text.get("insert linestart", "insert lineend")
+    cursor_line_indentation = cursor_line[:(len(cursor_line) - len(cursor_line.lstrip()))]
+
+    start = cursor_lineno
+    while can_extend_default_selection(text, cursor_line_indentation, start - 1):
+        start -= 1
+
+    end = cursor_lineno
+    while can_extend_default_selection(text, cursor_line_indentation, end + 1):
+        end += 1
+
+    return (start, end)
 
 
 def sort(tab: tabs.FileTab) -> None:
@@ -15,12 +40,7 @@ def sort(tab: tabs.FileTab) -> None:
         # If last selected character is newline, ignore it
         last_line = int(tab.textwidget.index("sel.last - 1 char").split(".")[0])
     except tkinter.TclError:
-        # Nothing selected, find blank line separated part
-        first_line = last_line = int(tab.textwidget.index("insert").split(".")[0])
-        while tab.textwidget.get(f"{first_line - 1}.0", f"{first_line}.0").strip():
-            first_line -= 1
-        while tab.textwidget.get(f"{last_line + 1}.0", f"{last_line + 2}.0").strip():
-            last_line += 1
+        first_line, last_line = find_chunk_around_cursor(tab.textwidget)
 
     old_lines = tab.textwidget.get(f"{first_line}.0", f"{last_line}.0 lineend").splitlines()
     new_lines = sorted(old_lines)
