f24106d21513d812612c0cb8be2fbbde90fd48af
ThePhilgrim <ThePhilgrim@users.noreply.github.com>
2023-07-13 22:02:50 +0000

    Run pycln, black and isort

--- porcupine/plugins/comment_selected_lines.py
+++ porcupine/plugins/comment_selected_lines.py
@@ -36,7 +36,7 @@ def comment_or_uncomment(tab: tabs.FileTab, pressed_key: str | None = None) -> s
         line_no = tab.textwidget.index("insert").split(".")[0]
         line_start = line_no + ".0"
         line_text = tab.textwidget.get(line_start, f"{line_start} lineend")
-        
+
         if pressed_key is None:
             if already_commented(line_text, comment_prefix):
                 tab.textwidget.delete(line_start, f"{line_no}.{len(comment_prefix)}")
