8eeb905c3b55c731411a881ed55d6fc77794181d
rdbende <rdbende@proton.me>
2023-07-17 19:41:08 +0200

    hey, i just learned how to do a partial revert

--- porcupine/plugins/jump_to_definition.py
+++ porcupine/plugins/jump_to_definition.py
@@ -9,6 +9,7 @@ import logging
 import tkinter
 from functools import partial
 from pathlib import Path
+from typing import List
 
 from porcupine import get_tab_manager, menubar, tabs, utils
 from porcupine.plugins import rightclick_menu
