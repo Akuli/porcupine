2f705481ef7bd9ed4516743ad3fd6469c5f2b276
Benjamin Kirkbride <benjaminkirkbride@gmail.com>
2023-08-05 06:28:03 -0400

    Mark `menubar.add_filetab_command` as deprecated (#1399)

--- porcupine/menubar.py
+++ porcupine/menubar.py
@@ -311,6 +311,7 @@ def get_filetab() -> tabs.FileTab:
     return tab
 
 
+# FIXME(#1398): this function is deprecated
 def add_filetab_command(path: str, func: Callable[[tabs.FileTab], object], **kwargs: Any) -> None:
     """
     This is a convenience function that does several things:
