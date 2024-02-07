bf378a77b65a9a66d2a343d2be5cc5578299638e
Akuli <akuviljanen17@gmail.com>
2024-02-07 13:10:05 +0200

    Delete dumb test

--- tests/test_porcupine_files.py
+++ tests/test_porcupine_files.py
@@ -15,24 +15,3 @@ def test_license_file():
 
 def test_copyright():
     assert str(datetime.datetime.now().year) in porcupine.__copyright__
-
-
-# Porcupine dependencies are specified in pyproject.toml, but also kept in
-# requirements-dev.txt for a convenient and familiar workflow. You just
-# "pip install -r requirements-dev.txt" when you start developing Porcupine.
-def test_requirements_and_pyproject_toml_in_sync():
-    reqs_dev_content = []
-    with open("requirements-dev.txt") as file:
-        for line in file:
-            requirement = line.split("#")[0].strip()
-            if requirement:
-                reqs_dev_content.append(requirement)
-
-    with open("pyproject.toml", "rb") as file:
-        toml_content = tomli.load(file)
-
-    assert (
-        toml_content["project"]["dependencies"]
-        + toml_content["project"]["optional-dependencies"]["dev"]
-        == reqs_dev_content
-    )
