a2231b9235eab598b2ce78a9faa7002d5284f6eb
Akuli <akuviljanen17@gmail.com>
2024-02-07 12:07:57 +0200

    Get rid of requirements.txt

--- tests/test_porcupine_files.py
+++ tests/test_porcupine_files.py
@@ -17,27 +17,22 @@ def test_copyright():
     assert str(datetime.datetime.now().year) in porcupine.__copyright__
 
 
-# The intended way to store dependency info is that setup.py or pyproject.toml contains version
-# ranges, and requirements.txt contains specific versions in those ranges so that each deployment
-# gets the same dependencies from there.
-#
-# A downside is that if your deployment also gets dependencies from other places, they could specify
-# conflicting versions. This could be a problem for Porcupine: even though users usually install
-# Porcupine into a virtualenv that doesn't contain anything else, I don't want to assume that they
-# do.
-#
-# Another downside with pinning for Porcupine would be dependencies getting outdated easily.
-#
-# Solution: pin only the packages where updating will likely break things, and keep the same
-# dependencies in requirements.txt (for easy "pip install -r requirements.txt" during development)
-# and in pyproject.toml (for installing released versions).
+# Porcupine dependencies are specified in pyproject.toml, but also kept in
+# requirements-dev.txt for a convenient and familiar workflow. You just
+# "pip install -r requirements-dev.txt" when you start developing Porcupine.
 def test_requirements_and_pyproject_toml_in_sync():
-    requirements = []
-    with open("requirements.txt") as file:
+    reqs_dev_content = []
+    with open("requirements-dev.txt") as file:
         for line in file:
             requirement = line.split("#")[0].strip()
             if requirement:
-                requirements.append(requirement)
+                reqs_dev_content.append(requirement)
 
     with open("pyproject.toml", "rb") as file:
-        assert tomli.load(file)["project"]["dependencies"] == requirements
+        toml_content = tomli.load(file)
+
+    assert (
+        toml_content["project"]["dependencies"]
+        + toml_content["project"]["optional-dependencies"]["dev"]
+        == reqs_dev_content
+    )
