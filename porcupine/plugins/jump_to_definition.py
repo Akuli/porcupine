a84f73b749ca5d76c1cc0548b5c5b22f2ac8c13b
ethical_haquer <141518185+ethical-haquer@users.noreply.github.com>
2024-03-10 13:05:11 -0700

    Correct right-click menu offset
    Added the missing implementation, and changed the offset to 1 instead of 10.

--- porcupine/plugins/jump_to_definition.py
+++ porcupine/plugins/jump_to_definition.py
@@ -67,9 +67,10 @@ def find_cursor_xy(textwidget: tkinter.Text) -> tuple[int, int]:
 
     # Make coords relative to top left corner of screen, not text widget
     left += textwidget.winfo_rootx()
+    
     top += textwidget.winfo_rooty()
 
-    return (left, top + height)
+    return (left + 1, top + height + 1)
 
 
 def receive_jump(event: utils.EventWithData) -> None:
