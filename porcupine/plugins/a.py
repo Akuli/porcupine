544b26bf12fc1c45b853d395a505974134d28a2a
Akuli <Akuli@users.noreply.github.com>
2024-03-31 21:34:11 +0000

    Run pycln, pyupgrade, black and isort

--- porcupine/plugins/a.py
+++ porcupine/plugins/a.py
@@ -1,8 +1,11 @@
 from tkinter import messagebox
+
 from porcupine import menubar
+
 
 def hello():
     messagebox.showinfo("Hello", "Hello World!")
+
 
 def setup():
     menubar.get_menu("Run/Greetings").add_command(label="Hello World", command=hello)
