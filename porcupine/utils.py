5e69862e22f3b3f910d5b976555ea2ba0dbbaed3
Akuli <akuviljanen17@gmail.com>
2024-03-31 13:30:31 +0300

    Update flake8-tkinter config

--- porcupine/utils.py
+++ porcupine/utils.py
@@ -548,7 +548,7 @@ def bind_tab_key(
         shift_tab = "<Shift-Tab>"
 
     widget.bind("<Tab>", functools.partial(callback, False), **bind_kwargs)  # noqa: TK231
-    widget.bind(shift_tab, functools.partial(callback, True), **bind_kwargs)  # noqa: TK231
+    widget.bind(shift_tab, functools.partial(callback, True), **bind_kwargs)  # noqa: TK131
 
 
 # Must be in a function because lambdas and local variables are ... inconvenient
