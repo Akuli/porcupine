14176eb98ee93a081a309b11eb836388a7653cb2
jhi2 <jhi2@users.noreply.github.com>
2024-05-16 22:20:35 +0000

    Run pycln, pyupgrade, black and isort

--- porcupine/plugins/windowicon.py
+++ porcupine/plugins/windowicon.py
@@ -1,14 +1,13 @@
 """Set the title and icon of the Porcupine window."""
-from porcupine import __version__ as porcupine_version
-from porcupine import get_main_window, images
 import Titleman as ttle
 
+from porcupine import __version__ as porcupine_version
+from porcupine import get_main_window
+
+
 def setup() -> None:
-    #Bitmap for the icon? Not with windowmanager because that's how titleman works.
+    # Bitmap for the icon? Not with windowmanager because that's how titleman works.
     window = get_main_window()
     window.title(f"Porcupine {porcupine_version}")  # not related to the icon, but it's ok imo
     ttle.initCustom(window)
-    ttle.startTitlebar(window,"black")
-    
-    
-    
+    ttle.startTitlebar(window, "black")
