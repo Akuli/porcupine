5b9b248b5cf287d235428706d8dddc7f8f6decf2
jhi2 <johndrostan@outlook.com>
2024-05-16 15:19:17 -0700

    Make sure that titlebar is black on all platforms

--- porcupine/plugins/windowicon.py
+++ porcupine/plugins/windowicon.py
@@ -1,9 +1,14 @@
 """Set the title and icon of the Porcupine window."""
 from porcupine import __version__ as porcupine_version
 from porcupine import get_main_window, images
-
+import Titleman as ttle
 
 def setup() -> None:
+    #Bitmap for the icon? Not with windowmanager because that's how titleman works.
     window = get_main_window()
     window.title(f"Porcupine {porcupine_version}")  # not related to the icon, but it's ok imo
-    window.wm_iconphoto(False, images.get("logo-200x200"))
+    ttle.initCustom(window)
+    ttle.startTitlebar(window,"black")
+    
+    
+    
