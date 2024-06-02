0602fa2b62f08e236b1cb649db87a9bee6c5723d
Akuli <akuviljanen17@gmail.com>
2024-06-02 15:48:28 +0300

    bruh

--- porcupine/pluginloader.py
+++ porcupine/pluginloader.py
@@ -26,6 +26,7 @@ import time
 import traceback
 from collections.abc import Iterator, Sequence
 from typing import Any, TypeVar, cast
+from typing import Callable
 
 from porcupine import get_main_window
 from porcupine.plugins import __path__ as plugin_paths
